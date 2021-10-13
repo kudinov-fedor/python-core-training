f3c5b483d85ee982d3f6fa9aa07090e99a2a8f43
Anton Kaiafiuk <akaiafiuk@gmail.com>
2021-10-14 02:07:40 +0300

    add few more tasks

---
+++ akaiafiuk/MaxDigit.py
@@ -0,0 +1,35 @@
+"""
+You have a number and you need to determine which digit in this number is the biggest.
+
+Input: A positive int.
+Output: An Int (0-9).
+
+Example:
+max_digit(0) == 0
+max_digit(52) == 5
+max_digit(634) == 6
+max_digit(1) == 1
+max_digit(10000) == 1
+"""
+
+
+def max_digit(number: int) -> int:
+    max_d = 0
+    x = list(str(number))
+    for digit in x:
+        if int(digit) > max_d:
+            max_d = int(digit)
+    return max_d
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(max_digit(0))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert max_digit(0) == 0
+    assert max_digit(52) == 5
+    assert max_digit(634) == 6
+    assert max_digit(1) == 1
+    assert max_digit(10000) == 1
+    print("Coding complete? Click 'Check' to earn cool rewards!")
---
+++ akaiafiuk/ReplaceFirst.py
@@ -0,0 +1,31 @@
+"""
+In a given list the first element should become the last one. An empty list or list with only one element should stay
+the same.
+Input: List.
+Output: Iterable.
+
+Example:
+replace_first([1, 2, 3, 4]) == [2, 3, 4, 1]
+replace_first([1]) == [1]
+"""
+
+
+from typing import Iterable
+
+
+def replace_first(items: list) -> Iterable:
+    if not items:
+        return items
+    items.append(items.pop(0))
+    return items
+
+
+if __name__ == "__main__":
+    print("Example:")
+    print(list(replace_first([1, 2, 3, 4])))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert list(replace_first([1, 2, 3, 4])) == [2, 3, 4, 1]
+    assert list(replace_first([1])) == [1]
+    assert list(replace_first([])) == []
+    print("Coding complete? Click 'Check' to earn cool rewards!")
