9f44fb1f1d26fca9557227668adfe10b63636063
fkudinov-mn <fkudinov@modeln.com>
2021-10-18 13:56:41 +0300

    add test examples

--- fkudi/demo_task.py
+++ fkudi/demo_task.py
@@ -1,2 +1,12 @@
-def demo_feature():
+def demo_feature() -> int:
+    """
+    Simply return 1
+    """
     return 1
+
+
+def demo_division(a:  int, b: int) -> float:
+    """
+    divide a by b
+    """
+    return a / b
--- tests/fkudi/test_demo_feature.py
+++ tests/fkudi/test_demo_feature.py
@@ -1,6 +1,32 @@
-from fkudi.demo_task import demo_feature
+import pytest
+
+from fkudi.demo_task import demo_feature, demo_division
 
 
 def test_demo_feature():
+    """
+    Simple test
+    """
     res = demo_feature()
     assert res == 1
+
+
+@pytest.mark.parametrize("a, b, expected", [
+    (10, 5, 2),
+    (5, 2, 2.5),
+    (-4, 2, -2),
+    (0, 5, 0)
+])
+def test_division_success(a, b, expected):
+    """
+    data driven test
+    """
+    assert demo_division(a, b) == expected
+
+
+def test_division_error():
+    """
+    test that error raises
+    """
+    with pytest.raises(ZeroDivisionError):
+        demo_division(5, 0)
