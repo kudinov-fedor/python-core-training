f3ea818565a4e071a8755c5c9d6fa3710d4323be
RDruzhko <rdruz@softserveinc.com>
2021-10-24 22:18:22 +0300

    Added hometasks: 	new file:   additional_tasks_01.py 	new file:   checkio_elementary_02-06.py

---
+++ checkio_elementary_02-06.py
@@ -0,0 +1,105 @@
+#2. In this mission, you need to create a password verification function.
+#The verification condition is: the length should be bigger than 6.
+
+def is_acceptable_password(password: str) -> bool:
+    return len(password) > 6 # my code here
+    return True
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(is_acceptable_password('more_than_six'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_acceptable_password('short') == False
+    assert is_acceptable_password('muchlonger') == True
+    assert is_acceptable_password('ashort') == False
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
+
+#3. You have a positive integer. Try to find out how many digits it has?
+
+def number_length(a: int) -> int:
+    return len(str(a)) # my code here
+    #todo solution with return int(math.log10(a))
+    return None
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(number_length(10))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert number_length(10) == 2
+    assert number_length(0) == 1
+    assert number_length(4) == 1
+    assert number_length(44) == 2
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
+
+#4. You have a positive integer. Try to find out how many digits it has?
+
+def end_zeros(num: int) -> int:
+    find = str(num) # my code here
+    return len(find)-len(find.strip('0'))
+    return None
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(end_zeros(0))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert end_zeros(0) == 1
+    assert end_zeros(1) == 0
+    assert end_zeros(10) == 1
+    assert end_zeros(101) == 0
+    assert end_zeros(245) == 0
+    assert end_zeros(100100) == 2
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
+
+#5. You should return a given string in reverse order.
+
+def backward_string(val: str) -> str:
+    return val[::-1]   # my code here
+    return None
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(backward_string('val'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert backward_string('val') == 'lav'
+    assert backward_string('') == ''
+    assert backward_string('ohho') == 'ohho'
+    assert backward_string('123456789') == '987654321'
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
+
+#6. Not all of the elements are important. What you need to do here is to remove from the list all of the elements before the given one.
+#For the illustration we have a list [1, 2, 3, 4, 5] and we need to remove all elements that go before 3 - which is 1 and 2.
+#We have two edge cases here: (1) if a cutting element cannot be found, then the list shoudn't be changed. (2) if the list is empty, then it should remain empty.
+#from typing import Iterable
+
+def remove_all_before(items: list, border: int) -> Iterable:
+    #items = [e for e in items if e not in (1, 2)]
+    for i in range(0, len(items)):
+        if (items[i] == border):
+            return items[i:]
+    return items
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(list(remove_all_before([1, 2, 3, 4, 5], 3)))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert list(remove_all_before([1, 2, 3, 4, 5], 3)) == [3, 4, 5]
+    assert list(remove_all_before([1, 1, 2, 2, 3, 3], 2)) == [2, 2, 3, 3]
+    assert list(remove_all_before([1, 1, 2, 4, 2, 3, 4], 2)) == [2, 4, 2, 3, 4]
+    assert list(remove_all_before([1, 1, 5, 6, 7], 2)) == [1, 1, 5, 6, 7]
+    assert list(remove_all_before([], 0)) == []
+    assert list(remove_all_before([7, 7, 7, 7, 7, 7, 7, 7, 7], 7)) == [7, 7, 7, 7, 7, 7, 7, 7, 7]
+    print("Coding complete? Click 'Check' to earn cool rewards!")
