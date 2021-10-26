efd3c45072f8fc5bc22e9edf6155d33c4db3a9d9
Anton Kaiafiuk <akaiafiuk@gmail.com>
2021-10-26 19:19:53 +0300

    added solution two for the nearest value

--- tests/akaiafiuk/test_lesson_one.py
+++ tests/akaiafiuk/test_lesson_one.py
@@ -6,7 +6,7 @@ from akaiafiuk.end_zeros import end_zeros, end_zeros_using_split, end_zeros_usin
 from akaiafiuk.first_word_simplified import first_word
 from akaiafiuk.max_digit import max_digit
 from akaiafiuk.multiply import multiply_two
-from akaiafiuk.nearest_value import nearest_value
+from akaiafiuk.nearest_value import nearest_value, nearest_value_solution_two
 from akaiafiuk.number_length import number_length
 from akaiafiuk.remove_all_before import remove_all_before
 from akaiafiuk.replace_first import replace_first
@@ -117,6 +117,19 @@ def test_nearest_value():
     assert nearest_value({4, 7, 10, 11, 12, 17}, 100) == 17
     assert nearest_value({5, 10, 8, 12, 89, 100}, 7) == 8
     assert nearest_value({-1, 2, 3}, 0) == -1
+    assert nearest_value([12, 10], 11) == 10
+
+
+def test_nearest_value_solution_two():
+    assert nearest_value_solution_two({4, 7, 10, 11, 12, 17}, 9) == 10
+    assert nearest_value_solution_two({4, 7, 10, 11, 12, 17}, 8) == 7
+    assert nearest_value_solution_two({4, 8, 10, 11, 12, 17}, 9) == 8
+    assert nearest_value_solution_two({4, 9, 10, 11, 12, 17}, 9) == 9
+    assert nearest_value_solution_two({4, 7, 10, 11, 12, 17}, 0) == 4
+    assert nearest_value_solution_two({4, 7, 10, 11, 12, 17}, 100) == 17
+    assert nearest_value_solution_two({5, 10, 8, 12, 89, 100}, 7) == 8
+    assert nearest_value_solution_two({-1, 2, 3}, 0) == -1
+    assert nearest_value([12, 10], 11) == 10
 
 
 @pytest.mark.parametrize("number, result", [
