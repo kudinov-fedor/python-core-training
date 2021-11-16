6b19a308acf100cf9a947e5e712b08656526cf5c
Anton Kaiafiuk <akaiafiuk@gmail.com>
2021-11-17 00:50:21 +0200

    fixes for descriptor and solution using property

--- akaiafiuk/descriptor_test_drive.py
+++ akaiafiuk/descriptor_test_drive.py
@@ -6,34 +6,29 @@ object creation and update
 
 
 class Descriptor:
-    def __init__(self):
-        self._max_speed = 0
 
-    def __get__(self, obj, objtype=None):
-        return self._max_speed
+    def __get__(self, instance, objtype=None):
+        return instance._max_speed
 
-    def __set__(self, obj, value):
-        if isinstance(value, int):
-            if value > 0:
-                self._max_speed = value
-            else:
-                raise ValueError("max_speed cannot be negative")
-        else:
+    def __set__(self, instance, value):
+        if not isinstance(value, int):
             raise TypeError("max_speed must be integer")
+        if value < 0:
+            raise ValueError("max_speed cannot be negative")
+        instance._max_speed = value
 
-    def __delete__(self, obj):
-        del self._max_speed
+    def __delete__(self, instance):
+        del instance._max_speed
 
 
 class Car:
+
     max_speed = Descriptor()
 
     def __init__(self, make, model, max_speed):
         self.make = make
         self.model = model
         self.max_speed = max_speed
-        if self.max_speed < 0:
-            raise ValueError("Max speed cannot be negative")
 
     def __str__(self):
         return "Make: {0}, Model: {1}, Maximum Speed: {2}".format(self.make, self.model, self.max_speed)
