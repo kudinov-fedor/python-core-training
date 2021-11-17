420bf307c500b3268da425202e89f9dac9ff3005
fkudinov-mn <fkudinov@modeln.com>
2021-11-17 13:05:08 +0200

    add trello testing

---
+++ api_testing/helpers/constants.py
@@ -0,0 +1,12 @@
+import os
+
+
+API_KEY = os.environ["API_KEY"]
+API_TOKEN = os.environ["API_TOKEN"]
+
+
+BOARD_NAME = "TEST_BOARD"
+VAULT_NAME = "TEST_VAULT_BOARD"
+LIST_NAME = "TEST_LIST"
+
+SAMPLES_PATH = os.path.join(os.getcwd(), "api_testing/sample_data")
