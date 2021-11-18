b3d543d0aa3fb1b58f8b9ba9338b8f5b4b281114
Anton Kaiafiuk <akaiafiuk@gmail.com>
2021-11-18 14:20:25 +0200

    made tests for descriptor and property parametrized

--- tests/akaiafiuk/descriptor_test.py
+++ tests/akaiafiuk/descriptor_test.py
@@ -1,24 +1,46 @@
 import pytest
 
-from akaiafiuk.descriptor_test_drive import Car
+from akaiafiuk.descriptor_test_drive import Car as DescriptorCar
+from akaiafiuk.property_test_drive import Car as PropertyCar
+from akaiafiuk.property_outside_class import Car as PropertyOutsideCar
 
 
-def test_valid_car():
-    bmw = Car("BMW", "M3", 360)
+@pytest.mark.parametrize("func", [
+    DescriptorCar,
+    PropertyCar,
+    PropertyOutsideCar
+])
+def test_valid_car(func):
+    bmw = func("BMW", "M3", 360)
     assert str(bmw) == "Make: BMW, Model: M3, Maximum Speed: 360"
 
 
-def test_car_after_speed_update():
-    bmw = Car("BMW", "M3", 360)
+@pytest.mark.parametrize("func", [
+    DescriptorCar,
+    PropertyCar,
+    PropertyOutsideCar
+])
+def test_car_after_speed_update(func):
+    bmw = func("BMW", "M3", 360)
     bmw.max_speed = 400
     assert str(bmw) == "Make: BMW, Model: M3, Maximum Speed: 400"
 
 
-def test_value_error():
+@pytest.mark.parametrize("func", [
+    DescriptorCar,
+    PropertyCar,
+    PropertyOutsideCar
+])
+def test_value_error(func):
     with pytest.raises(ValueError):
-        Car("BMW", "M3", -360)
+        func("BMW", "M3", -360)
 
 
-def test_type_error():
+@pytest.mark.parametrize("func", [
+    DescriptorCar,
+    PropertyCar,
+    PropertyOutsideCar
+])
+def test_type_error(func):
     with pytest.raises(TypeError):
-        Car("BMW", "M3", "360")
+        func("BMW", "M3", "360")
