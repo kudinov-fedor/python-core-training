e7c13638b2e7b7d0fa591fc67f64f4383c13d298
Anton Kaiafiuk <akaiafiuk@gmail.com>
2021-12-05 16:57:20 +0200

    The field is now dynamic. Splitted Battlefield into model and controller, added some validations for data input

--- akaiafiuk/mines/mines_simplified.py
+++ akaiafiuk/mines/mines_simplified.py
@@ -4,17 +4,13 @@ from itertools import product
 
 class BattleField:
 
-    # Create a battlefield with field to print to a user and field to assemble mines
-    def __init__(self):
-        self.field_to_print = [['*'] * 5 for _ in range(5)]
-        self.field_mines = [[False] * 5 for _ in range(5)]
-
-    # Draw current state of the battlefield
-    def get_field(self):
-        print('-----1----2----3----4----5--')
-
-        for i in range(len(self.field_to_print)):
-            print('-' + str(i + 1) + '-' + str(self.field_to_print[i]))
+    # Create a battlefield and assemble mines
+    def __init__(self, width=5, height=5, mines_count=3):
+        self.width = width
+        self.height = height
+        self.field_mines = [[False] * width for _ in range(height)]
+        for _ in range(mines_count):
+            self.assemble_mine()
 
     # Draw mines. Debug purposes.
     def get_mines(self):
@@ -28,38 +24,52 @@ class BattleField:
     # Open the cell on the battlefield
     def pick_cell(self, coordinates):
         x_coord, y_coord = coordinates
+        if x_coord not in range(self.width) or y_coord not in range(self.height):
+            return -1
+
         if self.field_mines[y_coord][x_coord]:
-            print('@@@ -- YOU LOOSE! -- @@@')
-            self.field_to_print[y_coord][x_coord] = '@'
-            self.get_field()
-            exit(0)
+            cell_value = '@'
 
         else:
-            print('XX -- I"m alive! Continue -- XX')
-
             # Verify how many mines borders with the opened cell
             mines_count = 0
             x_inspect_cells = [0, +1, -1]
             y_inspect_cells = [0, +1, -1]
-            if coordinates[1] == 0:
+            if x_coord == 0:
                 x_inspect_cells.pop(-1)
-            if coordinates[1] == 4:
+            if x_coord == self.width - 1:
                 x_inspect_cells.pop(+1)
-            if coordinates[0] == 0:
+
+            if y_coord == 0:
                 y_inspect_cells.pop(-1)
-            if coordinates[0] == 4:
+            if y_coord == self.height - 1:
                 y_inspect_cells.pop(+1)
 
             for x, y in product(x_inspect_cells, y_inspect_cells):
                 try:
-                    if self.field_mines[coordinates[1] + x][coordinates[0] + y]:
+                    if self.field_mines[x_coord + x][y_coord + y]:
                         mines_count += 1
                 except IndexError:
                     continue
+                cell_value = mines_count
 
-            # Open the cell and draw the current battlefield state
-            if mines_count == 0:
-                self.field_to_print[coordinates[1]][coordinates[0]] = '-'
-            else:
-                self.field_to_print[coordinates[1]][coordinates[0]] = str(mines_count)
-            self.get_field()
+        return cell_value
+
+
+class BattlefieldView:
+    def __init__(self, width=5, height=5):
+        self.field_to_print = [['*'] * width for _ in range(height)]
+
+    def get_field(self):
+        print('-----1----2----3----4----5--')
+        for i in range(len(self.field_to_print)):
+            print('-' + str(i + 1) + '-' + str(self.field_to_print[i]))
+
+    def renew_field(self, coordinates, cell_value):
+        if cell_value == -1:
+            print("Index outside boundaries. Please repeat.")
+        elif cell_value == 0:
+            self.field_to_print[coordinates[1]][coordinates[0]] = '-'
+        else:
+            self.field_to_print[coordinates[1]][coordinates[0]] = str(cell_value)
+        self.get_field()
--- akaiafiuk/mines/run_the_game.py
+++ akaiafiuk/mines/run_the_game.py
@@ -1,4 +1,4 @@
-from akaiafiuk.mines.mines_simplified import BattleField
+from akaiafiuk.mines.mines_simplified import BattleField, BattlefieldView
 
 DEBUG = False
 
@@ -17,10 +17,9 @@ class TheGame:
     print('Hit "Enter" to continue')
     print('-*-' * 15)
     input()
-    field = BattleField()
-    for _ in range(3):
-        field.assemble_mine()
-    field.get_field()
+    field = BattleField(7, 7, 3)
+    field_view = BattlefieldView(7, 7)
+    field_view.get_field()
 
     # Following line can be used for debug purposes to verify mines position
     if DEBUG:
@@ -30,7 +29,7 @@ class TheGame:
 
         # End the game in case when all cells are open
         star_count = 0
-        for row in field.field_to_print:
+        for row in field_view.field_to_print:
             star_count += row.count('*')
         if star_count == 3:
             print('$' * 30)
@@ -43,10 +42,11 @@ class TheGame:
             exit(0)
 
         # Get coordinates from user input and open the cell
-        x = input("Input x coordinates and hit Enter: ")
-        y = input("Input y coordinates and hit Enter: ")
-        coordinates = (int(x) - 1, int(y) - 1)
-        field.pick_cell(coordinates)
+        x = int(input("Input x coordinates and hit Enter: ") or 0)
+        y = int(input("Input y coordinates and hit Enter: ") or 0)
+        coordinates = (x - 1, y - 1)
+        cell_value = field.pick_cell(coordinates)
+        field_view.renew_field(coordinates, cell_value)
 
 
 if __name__ == "__main__":
