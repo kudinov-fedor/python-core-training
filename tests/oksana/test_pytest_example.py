b1c64e0e24f0f8ab49dd5e0db33d5f986b2b080d
Oksana Khaibullina <okhaibullina@gmail.com>
2021-12-07 18:33:58 +0200

    added more tests; removed pipfile; moved pytest.ini to the root

--- tests/oksana/test_pytest_example.py
+++ tests/oksana/test_pytest_example.py
@@ -1,11 +1,29 @@
 import pytest
+import random
+
+LIST_LEN = 10
 
 @pytest.mark.smoke
-def test_assert():
+def test_assert_in():
     assert 5 in [5,6,7]
 
-@pytest.mark.parametrize("test_data, expected",[('test','testtesttest')])
-def test_data(test_data, expected):
-    assert test_data*3 == expected
+@pytest.mark.parametrize("test_data, expected",[
+    ([1,2,3]+['h'],[1,2,3,'h'])
+])
+def test_append_list(test_data, expected):
+    assert test_data == expected
+
+@pytest.fixture
+def pop_list():
+    r = random.sample(range(0,100),LIST_LEN)
+    return r
+
+@pytest.mark.smoke
+def test_len_list(pop_list):
+    assert len(pop_list) == LIST_LEN
+
+@pytest.mark.xfail
+def test_append_list_fail():
+    assert [1,2]+'f'
 
 
