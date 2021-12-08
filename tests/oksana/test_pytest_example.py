22c2abbb1cf1a90e672c597e57d5611235a8c088
Oksana Khaibullina <okhaibullina@gmail.com>
2021-12-08 15:30:52 +0200

    fixed review comments

--- tests/oksana/test_pytest_example.py
+++ tests/oksana/test_pytest_example.py
@@ -3,27 +3,33 @@ import random
 
 LIST_LEN = 10
 
+
 @pytest.mark.smoke
 def test_assert_in():
-    assert 5 in [5,6,7]
+    assert 5 in [5, 6, 7]
 
-@pytest.mark.parametrize("test_data, expected",[
-    ([1,2,3]+['h'],[1,2,3,'h'])
+
+@pytest.mark.parametrize("test_data1, test_data2, expected", [
+    ([1, 2, 3], [56], [1, 2, 3, 56]),
+    (1, 7, 8),
+    ("test_", "data", "test_data")
 ])
-def test_append_list(test_data, expected):
-    assert test_data == expected
+def test_sum(test_data1, test_data2, expected):
+    result = test_data1 + test_data2
+    assert result == expected
+
 
 @pytest.fixture
 def pop_list():
-    r = random.sample(range(0,100),LIST_LEN)
+    r = random.sample(range(0, 100), LIST_LEN)
     return r
+
 
 @pytest.mark.smoke
 def test_len_list(pop_list):
     assert len(pop_list) == LIST_LEN
 
+
 @pytest.mark.xfail
 def test_append_list_fail():
-    assert [1,2]+'f'
-
-
+    assert [1, 2] + 'f'
