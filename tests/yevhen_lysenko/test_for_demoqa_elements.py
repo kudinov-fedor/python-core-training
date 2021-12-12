513582e33108328980469faa6c0c719445d706de
Yevhen <87.evgeniy@gmail.com>
2021-12-12 02:48:57 +0200

    'Demoqa' site/'Elements' block/'Check Box' task

--- tests/yevhen_lysenko/test_for_demoqa_elements.py
+++ tests/yevhen_lysenko/test_for_demoqa_elements.py
@@ -7,3 +7,17 @@ def test_elements_block(driver):
     driver.find_element(By.ID, 'submit').click()
     final_text = driver.find_element(By.ID, 'name').text
     assert 'Test message' in final_text
+
+
+def test_check_box(driver):
+    driver.find_element(By.ID, 'item-1').click()
+    element_first = driver.find_element(By.CSS_SELECTOR,
+                                        '#tree-node > ol > li'
+                                        ' > span > button > svg')
+    element_first.click()
+    selected_tree = driver.find_element(By.CSS_SELECTOR,
+                                        '#tree-node > ol > li'
+                                        ' > span > label > span.rct-title')
+    selected_tree.click()
+    displayed_result = driver.find_element(By.ID, 'result')
+    assert 'home' in displayed_result.text
