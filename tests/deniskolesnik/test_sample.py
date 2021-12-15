3ae8a1f0a25bc1d68eddfa0444e69369b9be90c1
Denis Kolesnik <denys.kolesnyk@n-able.com>
2021-12-15 14:51:45 +0200

    tests for len and list functions added

--- tests/deniskolesnik/test_sample.py
+++ tests/deniskolesnik/test_sample.py
@@ -1,4 +1,48 @@
+import pytest
+
+"""
+Tests for len() python built-in function
+Tests for list() python built-in function
+"""
 
 
-def test_sample():
-    assert 1 == 1
+def len_func(x):
+    return len(x)
+
+
+def list_func(x):
+    return list(x)
+
+
+@pytest.mark.parametrize("x, expected", [
+    (5, TypeError),
+    (5.0, TypeError),
+    (0, TypeError),
+    (None, TypeError),
+    ('', 0),
+    ('hello', 5),
+])
+def test_len(x, expected):
+    try:
+        assert len_func(x) == expected
+    except TypeError:
+        pass
+    else:
+        return AssertionError
+
+
+@pytest.mark.parametrize("x, expected", [
+    (5, TypeError),
+    (5.0, TypeError),
+    (0, TypeError),
+    (None, TypeError),
+    ('', []),
+    ('hello', ['h', 'e', 'l', 'l', 'o']),
+])
+def test_list(x, expected):
+    try:
+        assert list_func(x) == expected
+    except TypeError:
+        pass
+    else:
+        return AssertionError
