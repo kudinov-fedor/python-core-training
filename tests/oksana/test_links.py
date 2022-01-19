d07c13013ce1f69589d41c7f017359ba072f3cda
Oksana Khaibullina <okhaibullina@gmail.com>
2022-01-19 21:33:22 +0200

    changed tests according to comments

--- tests/oksana/test_links.py
+++ tests/oksana/test_links.py
@@ -1,11 +1,11 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import requests
+import pytest
+from urllib.parse import urljoin
 
 WEBSITE = "https://demoqa.com/"
-CREATED_LINK = "created"
-NO_CONTENT_LINK = "no-content"
-BAD_REQUEST = "bad-request"
 
 
 def test_opening_new_tab_from_link(driver):
@@ -13,44 +13,26 @@ def test_opening_new_tab_from_link(driver):
     Click on link and check that a new tab is open with the home page.
     """
     driver.get(WEBSITE + "links")
-    first_tab_handle = driver.window_handles
     link = driver.find_element(By.ID, "simpleLink")
     link.click()
-    WebDriverWait(driver, timeout=5).until(
-        lambda driver: len(first_tab_handle) != len(driver.window_handles))
-    new_tab_handle = driver.window_handles[1]
+    WebDriverWait(driver, 5).until(EC.number_of_windows_to_be(2))
+    new_tab_handle = driver.window_handles[-1]  # latest opened window
     driver.switch_to.window(new_tab_handle)
     assert driver.current_url == WEBSITE
 
 
-def test_link_status_created(driver):
+@pytest.mark.parametrize("text, status_code", [
+    ("Created", 201),
+    ("No Content", 204),
+    ("Bad Request", 400)
+])
+def test_link_status_response(driver, text, status_code):
     """
-    Click on the link and check the response of the request is 201.
-    """
-    driver.get(WEBSITE + "links")
-    link = driver.find_element(By.ID, CREATED_LINK)
-    link.click()
-    res = requests.head(WEBSITE + CREATED_LINK)
-    assert res.status_code == 201
-
-
-def test_link_status_no_content(driver):
-    """
-    Click on the link and check the response of the request is 204.
+    Click on the link and check the response of the request.
     """
     driver.get(WEBSITE + "links")
-    link = driver.find_element(By.ID, NO_CONTENT_LINK)
+    link = driver.find_element(By.XPATH, "//a[text()='{}']".format(text))
+    path = link.get_attribute("id")
     link.click()
-    res = requests.head(WEBSITE + NO_CONTENT_LINK)
-    assert res.status_code == 204
-
-
-def test_link_status_bad_request(driver):
-    """
-    Click on the link and check the response of the request is 400.
-    """
-    driver.get(WEBSITE + "links")
-    link = driver.find_element(By.ID, BAD_REQUEST)
-    link.click()
-    res = requests.head(WEBSITE + BAD_REQUEST)
-    assert res.status_code == 400
+    res = requests.head(urljoin(WEBSITE, path))
+    assert res.status_code == status_code
