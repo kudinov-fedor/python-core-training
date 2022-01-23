8162939190e9ee9a97fc55d1e5e6a70be8434612
SOFTSERVE\odyshlevyi <oleksandrdyshlevyi@interbrew.net>
2022-01-23 15:01:35 +0200

    Added TC's for books application

---
+++ tests/odysh/books_app/test_books_app.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from .constants import USER
+from .books_page import BooksPage
+from .login_page import LoginPage
+from .profile_page import ProfilePage
+
+
+def test_login(driver: WebDriver):
+    login_page = LoginPage(driver)
+    login_page.navigate_to_page()
+    login_page.login()
+    assert login_page.header == ProfilePage.HEADER
+
+def test_user_name(driver):
+    login_page = LoginPage(driver)
+    login_page.navigate_to_page()
+    login_page.login()
+    assert ProfilePage(driver).logged_in_user_name() == USER
+
+def test_search_book(driver):
+    books_page = BooksPage(driver)
+    books_page.navigate_to_page()
+    assert books_page.search_book() == BooksPage.GIT_BOOK
