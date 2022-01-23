22e99d308ecaeca4948073a49fae9dd813426ce9
SOFTSERVE\odyshlevyi <oleksandrdyshlevyi@interbrew.net>
2022-01-23 15:03:06 +0200

    Added file with constans and locators

---
+++ tests/odysh/books_app/constants.py
@@ -0,0 +1,16 @@
+HOST = "https://demoqa.com"
+
+USER = "test_user_1"
+PASSWORD = "QWEqwe123!@#"
+
+#login page locators
+user_name_field = 'userName'
+pwd_name_field = 'password'
+login_btn = 'login'
+
+#profile page locators
+user_name_value = 'userName-value'
+
+#books page locators
+search_book_field = 'searchBox'
+git_book = 'see-book-Git Pocket Guide'
