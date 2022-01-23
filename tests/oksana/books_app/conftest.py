f586656793ebe78d540bb209ac8df3d64caac018
Oksana Khaibullina <okhaibullina@gmail.com>
2022-01-23 15:43:00 +0200

    added option to ignore website certificate errors

--- tests/oksana/books_app/conftest.py
+++ tests/oksana/books_app/conftest.py
@@ -10,6 +10,7 @@ def session() -> WebDriver:
     chromeOptions = Options()
     chromeOptions.add_argument('--headless')
     chromeOptions.add_argument("--disable-extensions")
+    chromeOptions.add_argument("--ignore-certificate-errors")
     chromeOptions.add_argument("window-size=1920,1080")
     session = webdriver.Chrome(options=chromeOptions)
     yield session
