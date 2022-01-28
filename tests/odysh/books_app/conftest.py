cf0becb26240ab339798c6168e47c16a3f637dbf
SOFTSERVE\odyshlevyi <oleksandrdyshlevyi@interbrew.net>
2022-01-28 11:40:22 +0200

    Added option to ignore sertification errors

--- tests/odysh/books_app/conftest.py
+++ tests/odysh/books_app/conftest.py
@@ -7,7 +7,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="session")
 def driver():
-    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), chrome_options=options)
     driver.maximize_window()
     yield driver
     driver.quit()
