52998e6e5197a18f4be3548d641c33845c5ee68d
Anton Kaiafiuk <akaiafiuk@gmail.com>
2022-02-09 14:50:05 +0200

    test controllers using mocker

--- tests/akaiafiuk/minesweeper_tests/controllers_test.py
+++ tests/akaiafiuk/minesweeper_tests/controllers_test.py
@@ -1,16 +1,15 @@
-from unittest import mock
-from unittest.mock import patch
 from akaiafiuk.mine_sweeper.controllers import next_move_user, next_move_automatic
+from akaiafiuk.mine_sweeper import controllers
 from akaiafiuk.mine_sweeper import Field
 
 
-@patch('akaiafiuk.mine_sweeper.controllers.randint', mock.MagicMock(return_value=2))
-def test_next_move_automatic():
+def test_next_move_automatic(mocker):
+    mocker.patch.object(controllers, 'randint', side_effect=[3, 4])
     field = Field(10, 8, 3)
-    assert next_move_automatic(field) == (2, 2)
+    assert next_move_automatic(field) == (3, 4)
 
 
-@patch('akaiafiuk.mine_sweeper.controllers.input', mock.MagicMock(return_value='2,2'))
-def test_next_move_user():
+def test_next_move_user(mocker):
+    mocker.patch.object(controllers, 'input', return_value='3, 2')
     field = Field(10, 8, 3)
-    assert next_move_user(field) == (1, 1)
+    assert next_move_user(field) == (2, 1)
