579268a8624f3b8ec5e5c3d5bf58acc02332d115
SOFTSERVE\odyshlevyi <oleksandrdyshlevyi@interbrew.net>
2022-02-14 20:05:27 +0200

    Added function + tests for backward_string func

---
+++ tests/odysh/test_task_backward_string.py
@@ -0,0 +1,17 @@
+import pytest
+
+from odysh.backward_string import backward_string
+
+
+@pytest.mark.parametrize("string, reversed", [
+    ("val", 'lav'),
+    ("  ", '  '),
+    ("ohho", 'ohho'),
+    ("123456789", '987654321')
+])
+def test_backward_string(string, reversed):
+    assert backward_string(string) == reversed
+
+
+def test_negative_backward_string():
+    assert backward_string('ab') != 'ab'
