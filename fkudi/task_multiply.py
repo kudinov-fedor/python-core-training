a7e4e6b7aaaeafde98f003d5d1c9963b7dd91abf
fkudinov-mn <fkudinov@modeln.com>
2022-02-14 10:02:48 +0200

    add task mult

---
+++ fkudi/task_multiply.py
@@ -0,0 +1,7 @@
+"""
+Write a function that will receive 2 numbers as input and it should return the multiplication of these 2 numbers.
+"""
+
+
+def mult_two(a, b):
+    return a * b
---
+++ tests/fkudi/test_task_multiply.py
@@ -0,0 +1,14 @@
+import pytest
+
+from fkudi.task_multiply import mult_two
+
+
+@pytest.mark.parametrize("a, b, expected", [
+    (4, 3, 12),
+    (3, 2, 6),
+    (1, 0, 0),
+])
+def test_mult_two(a, b, expected):
+    assert mult_two(a, b) == expected
+
+
