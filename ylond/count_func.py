10dc28b3b444c6e3f9b879b14be817fdcba2ed19
julenka7 <londarenko.julia@gmail.com>
2022-02-15 14:12:54 +0200

    comment fixed

--- ylond/count_func.py
+++ ylond/count_func.py
@@ -1,8 +1,3 @@
 # return the number of times the value appears in the string#
-
 def count_value(a, sub):
     return a.count(sub)
-
-# res = count_value("My son has two hats and two pants", "two")
-# print(res)
-
