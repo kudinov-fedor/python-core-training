d4f25921d66a4b4a080bda72dce0c4848f14538e
julenka7 <londarenko.julia@gmail.com>
2022-02-16 12:55:43 +0200

    first word task and add tests for extend func

--- tests/ylond/test_extend_func.py
+++ tests/ylond/test_extend_func.py
@@ -6,7 +6,10 @@ from ylond.extend_func import extend_func
 @pytest.mark.parametrize("a, b, expected", [
     ([1, 2, 4], [5, 6, 7], [1, 2, 4, 5, 6, 7]),
     ([], ['cat', 'dog'], ['cat', 'dog']),
-    ([], (1, 2, 85), [1, 2, 85])])
+    ([], (1, 2, 85), [1, 2, 85]),
+    ([], {1, 'test', 85}, [1, 85, 'test']),
+    ([], {'dict': 1, 'dict1': 2}, ['dict', 'dict1']),
+    ([], range(10), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])])
 def test_extend_funct(a, b, expected):
     assert extend_func(a, b) == expected
 
