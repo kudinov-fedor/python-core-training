f9675388fcb631973e95f7fa3ed03cd1b1592164
julenka7 <londarenko.julia@gmail.com>
2022-02-16 12:57:27 +0200

    add tests

--- tests/ylond/test_first_word.py
+++ tests/ylond/test_first_word.py
@@ -8,7 +8,8 @@ from ylond.find_first_word import first_word
     ("1 2 5", "1"),
     ("-5e, exponent", "-5e,"),
     ("Hi Harry", "Hi"),
-    ("NONE exception", "NONE")
+    ("NONE exception", "NONE"),
+    ("First_word", "First_word")
 ])
 def test_first_word(text, expected):
     assert first_word(text) == expected
