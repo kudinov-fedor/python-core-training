34dd3dd45d0259b3832222571c69402213c07977
Khrystyna Kholiavinska <kkholi@softserveinc.com>
2022-02-20 19:38:28 +0200

    test_builtins

--- tests/kkhol/test_builtins.py
+++ tests/kkhol/test_builtins.py
@@ -22,3 +22,53 @@ def test_len_error():
     x = 145
     with pytest.raises(TypeError):
         len(x)
+
+
+def test_len2():
+    # verify whether r is not empty
+    r = [34, 56, 87, 23]
+    assert len(r) != 0
+
+
+def test_len3():
+    #verify whether r is not empty
+    r = [9]
+    assert len(r) != 0, 'Cannot be empty'
+
+
+def test_divide():
+    #Make sure we do not divide on 0
+    a = 23
+    b = 7
+    z = a / b
+    assert b != 0
+
+
+def test_pres():
+    c = '2 few 76 a lot'
+    d = '4'
+    assert d in c, 'c does not contain d'
+
+
+# Check is boolean value is a number
+def test_bool():
+    assert True != 1
+
+
+def test_bool1():
+    assert False != 0, 'False is actually a zero'
+
+
+def test_alike():
+    # Test whether values have similar id's
+    a = 4
+    b = 4
+    c = id(a)
+    d = id(b)
+    assert c == d
+
+
+def test_isinstance():
+    # check whether c is of a particular type
+    c = 56
+    assert isinstance(c, (int, float, str))
