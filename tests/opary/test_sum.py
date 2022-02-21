9d014524a9802e613dc15801ed7291087eedf7f8
Olha Parykulian <opary@softserveinc.com>
2022-02-21 19:58:01 +0200

    test sum updated

--- tests/opary/test_sum.py
+++ tests/opary/test_sum.py
@@ -12,24 +12,24 @@ def test_sum_true2():
     assert a + b  == 850
 
 
-def test_sum_true5():
+def test_sum_true3():
         a = 57
         b = 793
         sum=a+b
         assert sum == 850
 
 
-def test_sum_true3():
+def test_sum_true4():
     a = (193, 79, 3957)
     b = sum(a)
     print (b)
     assert b == 4229
 
 
-def test_sum_false():
+def test_sum_true5():
     a = 11
     b = 89
-    assert a + b != 100
+    assert a + b <= 100
 
 
 @pytest.mark.parametrize('a, b, expected_sum', [
