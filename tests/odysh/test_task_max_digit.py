f92c871855438779266bfbb59eda369397e2e158
SOFTSERVE\odyshlevyi <oleksandrdyshlevyi@interbrew.net>
2022-02-21 19:56:54 +0200

    Added max_digit func + added unit tests

---
+++ tests/odysh/test_task_max_digit.py
@@ -0,0 +1,20 @@
+import pytest
+
+from odysh.max_digit import max_digit
+
+@pytest.mark.parametrize("number, max", [
+    (123, 3),
+    (456, 6),
+    (654, 6),
+    (321, 3)
+])
+def test_max_digit(number, max):
+    assert max_digit(number) == max
+
+
+def test_negative_max_digit():
+    assert max_digit(123) == 2
+
+
+def test_negative_str_to_max_digit():
+    assert max_digit("123") == 3
