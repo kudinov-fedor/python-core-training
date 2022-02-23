b101e072e4777eabd9271a1a6adda813dcb8c30b
Khrystyna Kholiavinska <kkholi@softserveinc.com>
2022-02-23 14:21:46 +0200

    msg

--- kkhol/password_1.py
+++ kkhol/password_1.py
@@ -11,7 +11,7 @@ Output: A bool."""
 
 
 def is_acceptable_password(password: str) -> bool:
-    if len(password) > 6:
+    if len(password) >=6:
         return True
     else:
         return False
