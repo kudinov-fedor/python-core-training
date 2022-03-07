e67d0b0cfea6246fe66bbac5e1deafe7f9612ee9
Khrystyna Kholiavinska <kkholi@softserveinc.com>
2022-03-07 16:29:01 +0200

    tasksecond

---
+++ kkhol/Practice 03.07.py
@@ -0,0 +1,8 @@
+password = input()
+if len(password) >= 6:
+    if not password.isalnum():
+        print('Password must contain only letters or numbers')
+    else:
+        print('Good')
+elif len(password) < 6:
+    print('Password should be longer than 6 characters')
