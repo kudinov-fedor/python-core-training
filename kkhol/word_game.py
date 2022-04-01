ac27bb940c7f25c66e423e5404168d5b9cb81d52
Khrystyna Kholiavinska <kkholi@softserveinc.com>
2022-04-01 16:50:49 +0300

    edited game

--- kkhol/word_game.py
+++ kkhol/word_game.py
@@ -1,28 +1,24 @@
 word = 'apple'
+
 print('The word you are to guess is: "Fruit that is also a piece of technology". \n'
       'There are', len(word), 'letters.')
+
 used = []
-display = word
-for i in range(len(word)):
-    # replace each letter with a '_'
-    display = display[0:i] + "_" + display[i + 1:]
+
+display = '_' * len(word)
 
 print(" ".join(display))
 
-typed_letter = 0
-
-while typed_letter < len(word):
+while display != word:
     letter = input('What is your guess?')
     used.extend(letter)
-
     for i in range(len(word)):
+
         if letter and letter == word[i]:
             display = display[0:i] + letter + display[i + 1:]
-            print('Correct,', letter, 'is under index', word.index(letter))
-            typed_letter = typed_letter + 1
-        #else:
-            #print('No such letter')
-    print('You already tried:', used)
+            print(letter,' is correct,')
+
+    print('You already tried:',set(used))
 
     print(" ".join(display))
 
