8d7d47cf5d1ba7ccd99460dae31c82b13a117b5f
Anton Kaiafiuk <akaiafiuk@gmail.com>
2022-05-30 16:56:53 +0300

    added typings to forced_click_element

--- tests/akaiafiuk/test_tools_qa/utils.py
+++ tests/akaiafiuk/test_tools_qa/utils.py
@@ -1,9 +1,10 @@
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 
 
 def scroll_down(driver: WebDriver) -> None:
     driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
 
 
-def forced_click_element(driver, element) -> None:
+def forced_click_element(driver: WebDriver, element: WebElement) -> None:
     driver.execute_script('arguments[0].click();', element)
