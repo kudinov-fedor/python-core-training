d11be22101e5e1acc6a412a5cad14c8d37736a3e
Anton Kaiafiuk <akaiafiuk@gmail.com>
2022-05-30 16:45:22 +0300

    add typings

--- tests/akaiafiuk/test_tools_qa/utils.py
+++ tests/akaiafiuk/test_tools_qa/utils.py
@@ -1,6 +1,9 @@
-def scroll_down(driver):
+from selenium.webdriver.remote.webdriver import WebDriver
+
+
+def scroll_down(driver: WebDriver) -> None:
     driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
 
 
-def forced_click_element(driver, element):
+def forced_click_element(driver, element) -> None:
     driver.execute_script('arguments[0].click();', element)
