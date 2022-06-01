b8180277953f726923093c4fd7ef8f540f29de75
Anton Kaiafiuk <akaiafiuk@gmail.com>
2022-06-01 19:27:31 +0300

    fixes based on feedback

--- tests/akaiafiuk/book_store_ui/books_locators.py
+++ tests/akaiafiuk/book_store_ui/books_locators.py
@@ -2,15 +2,15 @@ from selenium.webdriver.common.by import By
 
 
 class BooksLocators:
-    PAGE_TITLE = By.CSS_SELECTOR, 'body > #app .pattern-backgound .main-header'
+    PAGE_TITLE = By.CSS_SELECTOR, '.main-header'
     SEARCH_INPUT = By.CSS_SELECTOR, '#searchBox'
     SEARCH_ICON = By.CSS_SELECTOR, '.input-group-text'
-    LOGIN_BUTTON = By.CSS_SELECTOR, '[type="button"]'
+    LOGIN_BUTTON = By.CSS_SELECTOR, '#login'
 
-    COLUMN_TITLES = By.XPATH, './/*[@class="rt-resizable-header-content"]'
-    BOOK_TITLES = By.XPATH, './/*[contains(@class, "action")]/*/a'
-    BOOK_AUTHORS = By.XPATH, './/div[3][@class="rt-td"]'
-    TABLE_ROW = By.XPATH, './/*[@class="rt-tr-group"]/child::*'
+    COLUMN_TITLES = By.XPATH, '//*[@class="rt-resizable-header-content"]'
+    BOOK_TITLES = By.XPATH, '//*[contains(@class, "action")]/*/a'
+    BOOK_AUTHORS = By.XPATH, '//*[@class="action-buttons"]/../../div[3]'
+    TABLE_ROW = By.XPATH, '//*[@class="action-buttons"]/../..'
 
     @staticmethod
     def get_book_locator_locator_by_name(name):
