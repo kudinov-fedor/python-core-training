4733d771ac87a998260b809e757edffd12db94b8
nkfortest <nazariy.kushnir@gmail.com>
2022-06-02 23:47:39 +0300

    fix review comments

--- tests/nkushnir/test_selenium_task1.py
+++ tests/nkushnir/test_selenium_task1.py
@@ -4,38 +4,45 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains as ac
 
 
-@pytest.fixture(scope="function")
+SELENIUM_HOST = 'http://127.0.0.1'
+SELENIUM_PORT = '4444'
+BUTTONS_DEMO_URL = 'https://demoqa.com/buttons'
+
+@pytest.fixture(scope="module")
 def driver():
     capabilities = {'browserName': 'firefox', 'marionette': True}
     driver = webdriver.Remote(
-        command_executor='http://127.0.0.1:4444/wd/hub',
+        command_executor=f'{SELENIUM_HOST}:{SELENIUM_PORT}/wd/hub',
         desired_capabilities=capabilities
     )
+    driver.maximize_window()
     yield driver
     driver.quit()
 
-def test_click_buttons(driver):
-    driver.get('https://demoqa.com/buttons')
-    driver.maximize_window()
 
-    double_click_btn_id = 'doubleClickBtn'
-    right_click_btn_id = 'rightClickBtn'
+def test_click_by_js(driver):
+    driver.get(BUTTONS_DEMO_URL)
     click_btn_xpath = '//button[contains(@class, "btn-primary") and text()="Click Me"]'
     simple_click_result_id = 'dynamicClickMessage'
-    double_click_result_id = 'doubleClickMessage'
-    right_click_result_id = 'rightClickMessage'
-
     simple_click_btn = driver.find_element(By.XPATH, click_btn_xpath)
     driver.execute_script("arguments[0].click()", simple_click_btn)
+    simple_cilck_result = driver.find_element(By.ID, simple_click_result_id)
+    assert simple_cilck_result.text == "You have done a dynamic click"
+
+def test_right_click(driver):
+    driver.get(BUTTONS_DEMO_URL)
+    right_click_btn_id = 'rightClickBtn'
+    right_click_result_id = 'rightClickMessage'
     right_click_btn = driver.find_element(By.ID, right_click_btn_id)
     ac(driver).context_click(right_click_btn).perform()
+    right_click_result = driver.find_element(By.ID, right_click_result_id)
+    assert right_click_result.text == "You have done a right click"
+
+def test_move_and_double_click(driver):
+    driver.get(BUTTONS_DEMO_URL)
+    double_click_btn_id = 'doubleClickBtn'
+    double_click_result_id = 'doubleClickMessage'
     double_click_btn = driver.find_element(By.ID, double_click_btn_id)
     ac(driver).move_to_element(double_click_btn).double_click().perform()
-
-    simple_cilck_result = driver.find_element(By.ID, simple_click_result_id)
     double_click_result = driver.find_element(By.ID, double_click_result_id)
-    right_click_result = driver.find_element(By.ID, right_click_result_id)
-
-    assert simple_cilck_result.text == "You have done a dynamic click"
     assert double_click_result.text == "You have done a double click"
-    assert right_click_result.text == "You have done a right click"
