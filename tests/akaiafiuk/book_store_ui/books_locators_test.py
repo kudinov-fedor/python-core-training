409b8c780a02964cb525431b9e23f8e9e9658388
Anton Kaiafiuk <akaiafiuk@gmail.com>
2022-06-02 21:27:41 +0300

    more improved then ever

--- tests/akaiafiuk/book_store_ui/books_locators_test.py
+++ tests/akaiafiuk/book_store_ui/books_locators_test.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from python_at_2021.tests.akaiafiuk.constants import HOST
 
 
 class BooksLocators:
@@ -8,11 +9,19 @@ class BooksLocators:
     LOGIN_BUTTON = By.CSS_SELECTOR, '#login'
 
     COLUMN_TITLES = By.XPATH, '//*[@class="rt-resizable-header-content"]'
-    BOOK_TITLES = By.XPATH, '//*[contains(@class, "action")]/*/a'
     TABLE_ROW = By.XPATH, '//*[@class="action-buttons"]/ancestor::*[@role="row"]'
-    BOOK_TITLE = By.XPATH, '/following::a'
-    BOOK_AUTHOR = By.XPATH, '/div[3]'
+    BOOK_TITLE = By.XPATH, './/a'
+    BOOK_AUTHOR = By.XPATH, './div[3]'
 
     @staticmethod
     def get_book_locator_locator_by_name(name):
         return f'//*[text() = "{name}"]'
+
+
+def test_locators(session):
+    session.get(HOST + '/books')
+    row = session.find_elements(*BooksLocators.TABLE_ROW)[0]
+    title = row.find_element(*BooksLocators.BOOK_TITLE)
+    author = row.find_element(*BooksLocators.BOOK_AUTHOR)
+    assert title.text == 'Git Pocket Guide'
+    assert 'Richard E.' in author.text
--- tests/akaiafiuk/conftest.py
+++ tests/akaiafiuk/conftest.py
@@ -1,6 +1,16 @@
 import pytest
+from selenium.webdriver import Chrome
+from selenium.webdriver.remote.webdriver import WebDriver
 
 
 @pytest.fixture()
 def test_list():
     return [1, 2, 3]
+
+
+@pytest.fixture
+def session() -> WebDriver:
+    driver = Chrome()
+    driver.maximize_window()
+    yield driver
+    driver.quit()
