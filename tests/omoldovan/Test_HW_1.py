b7e1bce4048d93623a2c4387a909fff00e43899d
omoldovan <alexmoldovan1027@gmail.com>
2022-06-03 18:17:41 +0300

    The homework 1 is done

--- tests/omoldovan/Test_HW_1.py
+++ tests/omoldovan/Test_HW_1.py
@@ -1,33 +1,40 @@
 import pytest
-#
-#
-# def test_sample():
-#     assert 1 == 1
-#
-#
-# def test_sample_2():
-#     pass
-#
-#
-# def test_error():
-#     res = 1 != 1
-#     print(res)
-#     assert res
 
 
-@pytest.mark.parametrize('param1, param2, res', [
-    [6, 2, 3],      # positive/positive --PASS
-    [2, 1, 1],      # positive/positive --FAIL
-    [24, -3, -8],   # positive/negative
-    [0, 6, 0]       # zero division
-
-])
-def test_zero_dev_with_param(param1, param2, res):
-    assert param1/param2 == res
+def test_abs():
+    res = abs(-5)
+    assert res == 5
 
 
-@pytest.mark.skip("Just for fun")
-def test_error():
-    res = 1 != 1
-    print(res)
-    assert res
+def test_tuple():
+    res = tuple('abc')
+    assert res == ('a', 'b', 'c')
+
+
+# noinspection PyUnreachableCode
+def test_reversed():
+    test_list = ['dasasd', 'asdsa', 1, 22]
+    test_list.reverse()
+    return test_list
+    assert test_list == [22, 1, 'asdsa', 'dasasd']
+
+
+def test_len():
+    res = 'bajada'
+    assert len(res) == 6
+
+
+# HOW TO MAKE VIA PARAMETRIZED ???
+
+# @pytest.mark.parametrize('val1, val2, total', [
+#     [11, 22, 33],  # positive numbers PASS
+#     [11, 22, 0],  # positive numbers FAIL
+#     [-13, -15, -28]  # negative numbers PASS
+# ])
+# def test_sum(val1, val2, total):
+#     assert total == sum(val1, val2)
+
+
+def test_sum():
+    a = [1, 3, 4]
+    assert 8 == sum(a)
---
+++ tests/omoldovan/Test_Lesson_1.py
@@ -0,0 +1,38 @@
+import pytest
+#
+#
+# def test_sample():
+#     assert 1 == 1
+#
+#
+# def test_sample_2():
+#     pass
+#
+#
+# def test_error():
+#     res = 1 != 1
+#     print(res)
+#     assert res
+
+
+@pytest.mark.parametrize('param1, param2, res', [
+    [6, 2, 3],      # positive/positive --PASS
+    [2, 1, 1],      # positive/positive --FAIL
+    [24, -3, -8],   # positive/negative
+    [0, 6, 0]       # zero division
+
+])
+def test_zero_dev_with_param(param1, param2, res):
+    assert param1/param2 == res
+
+
+@pytest.mark.skip("Just for fun")
+def test_error():
+    res = 1 != 1
+    print(res)
+    assert res
+
+
+def test_zero_div_error():
+    with pytest.raises(ZeroDivisionError):
+        1/5
