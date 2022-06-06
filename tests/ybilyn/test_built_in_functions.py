0f34a1eded6dc64d2feb56d16bc431d40833336a
ybilyn <ybilyn@gmail.com>
2022-06-06 21:43:00 +0300

    some fix

--- tests/ybilyn/test_built_in_functions.py
+++ tests/ybilyn/test_built_in_functions.py
@@ -8,18 +8,18 @@ Tests for type() python built-in function
 
 
 @pytest.mark.parametrize("data,res", [
-    (type(5), int),
-    (type(5.0), float),
-    (type(True), bool),
-    (type("Hello"), str),
-    (type([1, 2, 3]), list),
-    (type(("apple", "banana", "cherry")), tuple),
-    (type({"brand": "Ford", "model": "Mustang", "year": 1964}), dict),
-    (type(1j), complex),
-    (type(int(abs(-100.05))), int)
+    (5, int),
+    (5.0, float),
+    (True, bool),
+    ("Hello", str),
+    ([1, 2, 3], list),
+    (("apple", "banana", "cherry"), tuple),
+    ({"brand": "Ford", "model": "Mustang", "year": 1964}, dict),
+    (1j, complex),
+    (int(abs(-100.05)), int)
 ])
 def test_check_type_of_data(data, res):
-    assert data == res
+    assert type(data) == res
 
 
 @pytest.mark.smoke
@@ -40,7 +40,7 @@ Tests for handling ZeroDivisionError
 def test_zero_division_error():
     try:
         1 / 0
-    except Exception as err:
-        print('Handling run-time error:', err.__class__)
-        assert type(err) == ZeroDivisionError
-        assert type(err) != ArithmeticError
+    except ZeroDivisionError as err:
+        print('Handling run-time error:', type(err))
+    else:
+        raise AssertionError("Expected Error never risen")
