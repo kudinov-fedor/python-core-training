c436ed3bb6261d785116c19572f9d8ef47eb4a46
nkfortest <nazariy.kushnir@gmail.com>
2022-06-12 23:20:40 +0300

    rewrite book store test with using PO

---
+++ tests/nkushnir/pages/base_page.py
@@ -0,0 +1,87 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+
+
+def get_element(method):
+    def wrapper(self, locator, *args, **kwargs):
+        element = self.driver.find_element(*locator)
+        return method(self, element, *args, **kwargs)
+    return wrapper
+
+
+def get_element_from_parent(method):
+    def wrapper(self, parent, locator, *args, **kwargs):
+        element = parent.find_element(*locator)
+        return method(self, element, *args, **kwargs)
+    return wrapper
+
+
+def get_selector(method):
+    def wrapper(self, locator, *args, **kwargs):
+        selector = Select(self.driver.find_element(*locator))
+        return method(self, selector, *args, **kwargs)
+    return wrapper
+
+
+def get_elements(method):
+    def wrapper(self, locator, *args, **kwargs):
+        elements = self.driver.find_elements(*locator)
+        return method(self, elements, *args, **kwargs)
+    return wrapper
+
+
+class BasePage():
+    BASE_URL = 'https://demoqa.com/'
+
+    def __init__(self, driver):
+        self.driver = driver
+    
+    def open_page(self, page_path=None):
+        self.driver.get(f'{self.BASE_URL}{page_path}')
+    
+    def scroll_to_bottom(self):
+        self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+    
+    def format_xpath_locator(self, xpath, value_to_format):
+        return (By.XPATH, xpath.format(value_to_format))
+
+    @get_element
+    def enter_text(self, element, text):
+        element.clear()
+        element.send_keys(text)
+    
+    @get_element
+    def confirm_by_enter(self, element):
+        element.send_keys(Keys.ENTER)
+
+    @get_element
+    def get_text(self, element):
+        return element.text
+    
+    @get_element_from_parent
+    def get_text_from_parent(self, element):
+        return element.text
+
+    @get_element
+    def get_attr(self, element, attribute):
+        return element.get_attribute(attribute)
+    
+    @get_selector
+    def select_by_value(self, selector, value):
+        selector.select_by_value(str(value))
+    
+    @get_element
+    def is_enabled(self, element):
+        return element.is_enabled()
+    
+    @get_elements
+    def get_elements_length(self, elements):
+        return len(elements)
+    
+    @get_elements
+    def get_elements_list(self, elements):
+        return elements
+    
+    
+
---
+++ tests/nkushnir/pages/book_store_page.py
@@ -0,0 +1,58 @@
+from selenium.webdriver.common.by import By
+from tests.nkushnir.pages.base_page import BasePage
+
+
+class BookStorePage(BasePage):
+    PAGE_PATH = 'books'
+    COLUMNS = {"image": 1, "title": 2, "author": 3, "publisher": 4}
+    SEARCH_BOX = (By.ID, 'searchBox')
+    SEARCH_BUTTON = (By.ID, 'basic-addon2')
+    PAGES_SELECT = (By.XPATH, '//select[@aria-label="rows per page"]')
+    PREV_BUTTON = (By.XPATH, '//div[@class="-previous"]//button')
+    NEXT_BUTTON = (By.XPATH, '//div[@class="-next"]//button')
+    CURRENT_PAGE_INPUT = (By.XPATH, '//span[@class="-pageInfo"]//input')
+    TOTAL_PAGES_LABEL = (By.CSS_SELECTOR, '.-totalPages')
+    BOOKS_TABLE = (By.XPATH, '//div[@class="rt-table"]')
+    TABLE_ROW = (By.XPATH, '//div[contains(@class, "rt-tr-group")]')
+    TABLE_ROW_EMPTY = (By.XPATH, '//div[contains(@class, "rt-tr -padRow")]')
+    TABLE_ROW_DATA = (By.XPATH, '//div[contains(@class, "rt-tr -odd") or contains(@class, "rt-tr -even")]')
+    CELL_XPATH = '//div[@class="rt-td"][{}]'
+
+
+    def open(self):
+        self.open_page(self.PAGE_PATH)
+
+    def select_table_rows_number(self, rows_number):
+        self.select_by_value(self.PAGES_SELECT, rows_number)
+    
+    def get_current_page_number(self):
+        return int(self.get_attr(self.CURRENT_PAGE_INPUT, 'value'))
+    
+    def get_total_pages_number(self):
+        return int(self.get_text(self.TOTAL_PAGES_LABEL))
+    
+    def next_page_is_enabled(self):
+        return self.is_enabled(self.NEXT_BUTTON)
+    
+    def get_table_rows_number(self):
+        return self.get_elements_length(self.TABLE_ROW)
+    
+    def search_book_by_value(self, value_to_enter):
+        self.enter_text(self.SEARCH_BOX, value_to_enter)
+        self.confirm_by_enter(self.SEARCH_BOX)
+    
+    def get_table_rows_with_data_number(self):
+        return self.get_elements_length(self.TABLE_ROW_DATA)
+    
+    def get_values_from_column(self, column):
+        column_values = []
+        for row in self.get_elements_list(self.TABLE_ROW_DATA):
+            col_locator = self.format_xpath_locator(self.CELL_XPATH, self.COLUMNS[column.lower()])
+            cell_text = self.get_text_from_parent(row, col_locator)
+            column_values.append(cell_text)
+        return column_values
+    
+    def table_is_filtered_by_column_value(self, filter_value, column):
+        column_values = self.get_values_from_column(column)
+        return all(filter_value.lower() in cell_text.lower() for cell_text in column_values)
+    
