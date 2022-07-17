b31ab69ed04c7bd971b78bc059b5dcc60f40b450
Oleh Sokolov <Oleh.Sokolov-external@sema4.com>
2022-07-17 22:27:25 +0300

    fixed code

--- osokolov/guess_word.py
+++ osokolov/guess_word.py
@@ -1,20 +1,21 @@
 import re
 
 
-def guess(word, hint):
+def guess(word: str, hint: str):
     print('Welcome!')
     guessing_word = ['_ '] * len(word)
     print(f'Guessed word is the {hint}: {"".join(guessing_word)}')
-    print('If you want ot quit the game input q')
+    print('If you want ot quit the game input word: quit')
     while True:
         print("".join(guessing_word))
         answer = input('Your try: ').lower()
         if "".join(guessing_word) == word:
             print(f'You guess!\nIt is: {word}')
             break
-        elif answer == 'q':
+        elif answer == 'quit':
             break
-        elif len(answer) > 1 or re.search(r'[^a-z]', answer) is not None:
+        elif len(answer) > 1 or re.search(r'[a-z]', answer) is None:
+            print('Input correct data')
             continue
         elif answer not in word:
             print('This letter is not in the word')
@@ -26,3 +27,4 @@ def guess(word, hint):
             continue
         else:
             break
+
