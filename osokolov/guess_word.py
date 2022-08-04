17485fe5bf5dbd305ee995ff7e9cff62f57f3de6
Oleh Sokolov <Oleh.Sokolov-external@sema4.com>
2022-08-04 18:06:28 +0300

    fixed code

--- osokolov/guess_word.py
+++ osokolov/guess_word.py
@@ -2,12 +2,22 @@ from string import ascii_lowercase
 
 
 class GameModel:
+    def __init__(self):
+        self.tries = []
 
-    def check_win(self, word: str, letters: list):
-        return all([i in letters for i in word])
+    def check_win(self, word: str):
+        return all([i in self.tries for i in word])
 
 
 class GameView:
+
+    def start_game_message(self, hint):
+        print('Welcome!')
+        print(f'Guess the word: f{hint}  ')
+        print('If you want ot quit the game input word: quit')
+
+    def warning_message(self):
+        print('Input correct data')
 
     def get_user_answer(self):
         return input('Your try: ')
@@ -17,12 +27,30 @@ class GameView:
 
 
 class GameController:
-    def __init__(self):
+    def __init__(self, word, hint):
+        self.word = word.lower()
+        self.hint = hint
         self.view = GameView()
         self.model = GameModel()
-        self.answer = self.view.get_user_answer()
 
-    def validate_user_answer(self, answer):
+    def start(self):
+        self.view.start_game_message(self.hint)
+        while True:
+            answer = self.view.get_user_answer()
+            if answer == 'quit':
+                break
+            if self.validate_user_answer(answer):
+                self.model.tries.append(answer)
+            else:
+                self.view.warning_message()
+                continue
+            self.view.show_game_result(self.word, self.model.tries)
+            if self.model.check_win(self.word):
+                break
+        print('You guess!')
+
+    @classmethod
+    def validate_user_answer(cls, answer):
         try:
             return len(answer) == 1 and answer in ascii_lowercase
         except TypeError:
@@ -30,28 +58,4 @@ class GameController:
 
 
 def guess(word: str, hint: str):
-    word = word.lower()
-    tries = []
-    print('Welcome!')
-    print(f'Guess the word: f{hint}  ')
-    print('If you want ot quit the game input word: quit')
-    while True:
-        controller = GameController()
-        # user answer
-        answer = controller.answer
-        # validation
-        if answer == 'quit':
-            break
-        if controller.validate_user_answer(answer):
-            tries.append(answer)  # save user data
-        else:
-            print('Input correct data')
-            continue
-        # show current state
-        controller.view.show_game_result(word, tries)
-        # check win
-        if controller.model.check_win(word, tries):
-            break
-
-    print('You guess!')
-
+    GameController(word, hint).start()
