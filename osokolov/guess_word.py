630e152d2292e41782b2cd1afb5344d238f1ce0f
Oleg <olegasokol@gmail.com>
2022-08-09 13:32:19 +0000

    Merged in Creat_classes (pull request #100)
    Creat classes
    
    * created classes
    
    * fixed code
    
    * fixed code
    
    * fixed code
    
    
    Approved-by: Федор Кудинов

--- osokolov/guess_word.py
+++ osokolov/guess_word.py
@@ -1,82 +1,65 @@
 from string import ascii_lowercase
 
-#
-# class GameModel:
-#     def __init__(self, word, hint):
-#         self.word = word
-#         self.hint = hint
-#         self.tries = []
-#
-#     def save_latter(self, letter):
-#         self.tries.append(letter)
-#
-#     def check_win(self) -> bool:
-#         pass
-#
-#
-# class GameView():
-#     def start_game(self, data: GameModel):
-#         print('Welcome!')
-#         print(f'Guessing word is the {data.word}. It has {len(data.hint)} letters')
-#         print('If you want ot quit the game input word: quit')
-#
-#     def get_guessing_word(self, word: GameModel):
-#         guessing_word = ['_ '] * len(word)
-#         print(guessing_word)
-#
-#     def end_game(self):
-#         print('You guess the word !')
-#         print('Goodbye !')
-#
-#
-# class GameController:
-#
-#     def recive_letter(self):
-#         #input
-#
-#         pass
+
+class GameModel:
+    def __init__(self, word, hint):
+        self.word = word
+        self.hint = hint
+        self.tries = []
+
+    def check_win(self):
+        return all([i in self.tries for i in self.word])
 
 
-def get_user_answer():
-    return input('Your try: ')
+class GameView:
+
+    def start_game_message(self, model: GameModel):
+        print('Welcome!')
+        print(f'Guess the word: f{model.hint}  ')
+        print('If you want ot quit the game input word: quit')
+
+    def warning_message(self):
+        print('Input correct data')
+
+    def show_game_result(self, model: GameModel):
+        print(''.join([letter if letter in model.tries else "*" for letter in model.word]))
+
+    def win_message(self):
+        print('You guess!')
 
 
-def validate_user_answer(user_answer):
-    try:
-        return len(user_answer) == 1 and user_answer in ascii_lowercase
-    except TypeError:
-        return False
+class GameController:
+    def __init__(self, word, hint):
+        self.view = GameView()
+        self.model = GameModel(word, hint)
 
+    def start(self):
+        self.view.start_game_message(self.model)
+        while True:
+            answer = self.get_user_answer()
+            if answer == 'quit':
+                break
+            if self.validate_user_answer(answer):
+                self.model.tries.append(answer)
+            else:
+                self.view.warning_message()
+                continue
+            self.view.show_game_result(self.model)
+            if self.model.check_win():
+                break
+        self.view.win_message()
 
-def show_game_result(word: str, letters: list):
-    print(''.join([letter if letter in letters else "*" for letter in word]))
+    @classmethod
+    def validate_user_answer(cls, answer):
+        try:
+            return len(answer) == 1 and answer in ascii_lowercase
+        except TypeError:
+            return False
 
-
-def check_win(word: str, letters: list):
-    return all([i in letters for i in word])
+    @classmethod
+    def get_user_answer(cls):
+        return input('Your try: ')
 
 
 def guess(word: str, hint: str):
-    word = word.lower()
-    tries = []
-    print('Welcome!')
-    print(f'Guess the word: f{hint}  ')
-    print('If you want ot quit the game input word: quit')
-    while True:
-        # user answer
-        answer = get_user_answer()
-        # validation
-        if answer == 'quit':
-            break
-        if validate_user_answer(answer):
-            tries.append(answer)  # save user data
-        else:
-            print('Input correct data')
-            continue
-        # show current state
-        show_game_result(word, tries)
-        # check win
-        if check_win(word, tries):
-            break
-
-    print('You guess!')
+    GameController(word, hint).start()
