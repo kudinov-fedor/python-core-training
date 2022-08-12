4a2174953f435df2ccfb2765e6ebe60e9e093f7f
fkudinov <fkudinov@modeln.com>
2022-08-12 20:10:20 +0300

    add hw for 8 lection

---
+++ homework/homework_8/every_person.py
@@ -0,0 +1,14 @@
+
+class Person:
+    def __init__(self, first_name, last_name, birth_date, job, working_years, salary, country, city, gender='unknown'):
+        ...
+
+
+if __name__ == '__main__':
+    p1 = Person("John", "Smith", "19.09.1979", "welder", 15, 3600, "Canada", "Vancouver", "male")
+    p2 = Person("Hanna Rose", "May", "05.12.1995", "designer", 2.2, 2150, "Austria", "Vienna")
+    assert p1.name() == "John Smith", "Name"
+    assert p1.age() == 38, "Age"
+    assert p2.work() == "Is a designer", "Job"
+    assert p1.money() == "648 000", "Money"
+    assert p2.home() == "Lives in Vienna, Austria", "Home"
