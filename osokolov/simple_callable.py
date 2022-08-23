560282a440a893945c731a4d54a86ef05d3dcd50
Oleh Sokolov <Oleh.Sokolov-external@sema4.com>
2022-08-23 21:44:51 +0300

    Callable class

---
+++ osokolov/simple_callable.py
@@ -0,0 +1,18 @@
+
+class SimpleCallable:
+    def __init__(self):
+        self.call_count = 0
+
+    def __call__(self, *args, **kwargs):
+        self.call_count += 1
+
+
+if __name__ == "__main__":
+
+    a = SimpleCallable()
+
+    assert a.call_count == 0
+
+    a()
+    a()
+    assert a.call_count == 2
