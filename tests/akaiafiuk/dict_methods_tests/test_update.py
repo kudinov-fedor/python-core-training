9a280a33a822782f90cc94330418b4efdf4beaf2
Anton Kaiafiuk <akaiafiuk@gmail.com>
2022-09-09 17:36:45 +0300

    test with unhashable key

--- tests/akaiafiuk/dict_methods_tests/test_update.py
+++ tests/akaiafiuk/dict_methods_tests/test_update.py
@@ -3,7 +3,8 @@ import pytest
 UPDATE_DICT = {3: 'three', 4: 'four', 5: 'five'}
 UPDATE_LIST = [(3, 'three'), (4, 'four'), (5, 'five')]
 UPDATE_ZIP = zip([3, 4, 5], ['three', 'four', 'five'])
-BAD_TUPLE_LIST = [(3, 'three'), (4, 'four', 'five'), (5, 'five')]
+BAD_TUPLE_LIST_THREE_ELEMENTS = [(3, 'three'), (4, 'four', 'five'), (5, 'five')]
+BAD_TUPLE_LIST_UNHASHABLE = [(3, 'three'), ([4, ], 'four'), (5, 'five')]
 
 
 def test_update_with_another_dict(default_dict):
@@ -24,7 +25,7 @@ def test_update_zip(default_dict):
     assert default_dict == {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five'}
 
 
-def test_update_with_bad_list():
+def test_update_with_three_elements_tuple():
     """
     Some kind of side effect
     when update with list of tuples and one of the tuples is bad (e.g too much values),
@@ -34,5 +35,19 @@ def test_update_with_bad_list():
     some_dict = dict()
     assert len(some_dict) == 0
     with pytest.raises(ValueError):
-        some_dict.update(BAD_TUPLE_LIST)
+        some_dict.update(BAD_TUPLE_LIST_THREE_ELEMENTS)
+    assert len(some_dict) == 1
+
+
+def test_update_with_three_elements_tuple():
+    """
+    Some kind of side effect
+    when update with list of tuples and one of the tuples is bad (e.g unhashable first element),
+    update() will iterate through the list and add k: v pair
+    but return ValueError when reach bad tuple
+    """
+    some_dict = dict()
+    assert len(some_dict) == 0
+    with pytest.raises(TypeError):
+        some_dict.update(BAD_TUPLE_LIST_UNHASHABLE)
     assert len(some_dict) == 1
