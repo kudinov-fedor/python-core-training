fe83dddd964c2183dee98937ceaed5643026418c
Anton Kaiafiuk <akaiafiuk@gmail.com>
2022-09-16 11:36:48 +0300

    added some negative tests

--- tests/akaiafiuk/set_methods_tests/math_operations/test_intersection.py
+++ tests/akaiafiuk/set_methods_tests/math_operations/test_intersection.py
@@ -1,3 +1,5 @@
+import pytest
+
 x = {1, 2, 3, 4, 'a'}
 y = {3, 4, 5}
 z = ['a', 'b', 'c', 1, 2, 3, 4, 4]
@@ -7,6 +9,12 @@ def test_intersection_operator():
     """Intersect two sets using operator. Can only intersect two elements at once."""
     answer = x & y
     assert answer == {3, 4}
+
+
+def test_intersection_operator_negative():
+    """Negative test. Error is displayed when intersecting with incorrect type"""
+    with pytest.raises(TypeError):
+        x & [3, 4, 5]
 
 
 def test_intersection_method():
@@ -21,6 +29,15 @@ def test_intersection_update_operator():
     yellow_objects = {'banana', 'apple', 'chicken'}
     fruits &= yellow_objects
     assert fruits == {'banana', 'apple'}
+
+
+def test_intersection_update_operator_negative():
+    """&= can only work with same type"""
+    fruits = {'orange', 'apple', 'banana'}
+    yellow_objects = ['banana', 'apple', 'chicken']
+    with pytest.raises(TypeError):
+        fruits &= yellow_objects
+    assert fruits == {'orange', 'apple', 'banana'}
 
 
 def test_intersection_update_method():
