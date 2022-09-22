88944bcb1c25ab59ec73aeb8131c1d426c5ce7b4
Anton Kaiafiuk <akaiafiuk@gmail.com>
2022-09-22 19:10:21 +0300

    improvements based on comments from the coach

--- tests/akaiafiuk/set_methods_tests/comparison/issuperset.py
+++ tests/akaiafiuk/set_methods_tests/comparison/issuperset.py
@@ -1,33 +1,24 @@
-"""
-In this module, test functions verify is superset using issuperset() method and ">=" operator.
-Long story short: True if calling from equal or bigger set.
-Scenario #1: Returns True for equal sets
-Scenario #2: Returns True if calling from a superset
-Scenario #3: Some elements are not common - return False
-Scenario #4: Calling from a smaller set returns False
-"""
-
 import pytest
 
 
 @pytest.mark.parametrize("set_called_from, set_comparing_with, result", [
-    ({1, 2, 3}, {1, 2, 3}, True),
-    ({1, 2, 3, 4}, {1, 2, 3}, True),
-    ({1, 2, 3}, {2, 3, 4}, False),
-    ({1, 2, 3}, {1, 2, 3, 4}, False)
+    ({1, 2, 3}, {1, 2, 3}, True),     # Scenario #1: Returns True for equal sets
+    ({1, 2, 3, 4}, {1, 2, 3}, True),  # Scenario #2: Returns True if calling from a superset
+    ({1, 2, 3}, {2, 3, 4}, False),    # Scenario #3: Some elements are not common - return False
+    ({1, 2, 3}, {1, 2, 3, 4}, False)  # Scenario #4: Calling from a smaller set returns False
 ])
 def test_issubset_method(set_called_from, set_comparing_with, result):
-    """Checking issuperset() method"""
+    """Test function verify is superset using issuperset() method."""
     assert set_called_from.issuperset(set_comparing_with) == result
 
 
 @pytest.mark.parametrize("set_called_from, set_comparing_with, expected_result", [
-    ({1, 2, 3}, {1, 2, 3}, True),
-    ({1, 2, 3, 4}, {1, 2, 3}, True),
-    ({1, 2, 3}, {2, 3, 4}, False),
-    ({1, 2, 3}, {1, 2, 3, 4}, False)
+    ({1, 2, 3}, {1, 2, 3}, True),     # Scenario #1: Returns True for equal sets
+    ({1, 2, 3, 4}, {1, 2, 3}, True),  # Scenario #2: Returns True if calling from a superset
+    ({1, 2, 3}, {2, 3, 4}, False),    # Scenario #3: Some elements are not common - return False
+    ({1, 2, 3}, {1, 2, 3, 4}, False)  # Scenario #4: Calling from a smaller set returns False
 ])
 def test_issubset_operator(set_called_from, set_comparing_with, expected_result):
-    """Checking ">=" operator"""
+    """Test function verify is superset using ">=" operator."""
     actual_result = set_called_from >= set_comparing_with
     assert actual_result == expected_result
