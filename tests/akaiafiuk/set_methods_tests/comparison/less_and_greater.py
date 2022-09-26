d6ad4c13058d01ae6254e2cf6d6ec27b18d4bfc2
Anton Kaiafiuk <akaiafiuk@gmail.com>
2022-09-26 20:23:06 +0300

    fixed incorrect scenario for > and <

--- tests/akaiafiuk/set_methods_tests/comparison/less_and_greater.py
+++ tests/akaiafiuk/set_methods_tests/comparison/less_and_greater.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.mark.parametrize("set_called_from, set_comparing_with, expected_result", [
     ({1, 2, 3, 4}, {1, 2, 3}, True),   # Scenario #1: Returns True if calling from a superset
     ({1, 2, 3}, {1, 2, 3}, False),     # Scenario #2: Returns False for equal sets
-    ({1, 2, 3}, {1, 2, 3}, False),     # Scenario #3: Returns False if there are uncommon elements
+    ({1, 2, 3, 4}, {1, 2, 5}, False),     # Scenario #3: Returns False if there are uncommon elements
 ])
 def test_more_then(set_called_from, set_comparing_with, expected_result):
     """Basic tests for more then ">" operator"""
@@ -15,7 +15,7 @@ def test_more_then(set_called_from, set_comparing_with, expected_result):
 @pytest.mark.parametrize("set_called_from, set_comparing_with, expected_result", [
     ({1, 2, 3}, {1, 2, 3, 4}, True),   # Scenario #1: Returns True if calling from a subset
     ({1, 2, 3}, {1, 2, 3}, False),     # Scenario #2: Returns False for equal sets
-    ({1, 2, 3}, {1, 2, 3}, False),     # Scenario #3: Returns False if there are uncommon elements
+    ({1, 2, 3, 4}, {1, 2, 5}, False),     # Scenario #3: Returns False if there are uncommon elements
 ])
 def test_less_then(set_called_from, set_comparing_with, expected_result):
     """Basic tests for less then "<" operator"""
