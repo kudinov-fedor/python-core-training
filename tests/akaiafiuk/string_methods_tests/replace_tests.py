3cf5c0741355e304d6e7876fe0b107e6bb625adb
Anton Kaiafiuk <akaiafiuk@gmail.com>
2022-12-03 14:27:43 +0200

    improved tests

--- tests/akaiafiuk/string_methods_tests/replace_tests.py
+++ tests/akaiafiuk/string_methods_tests/replace_tests.py
@@ -1,21 +1,16 @@
-import pytest
+TEXT = "one one was a race horse, two two was one too."
 
 
-@pytest.fixture()
-def text():
-    return "one one was a race horse, two two was one too."
-
-
-def test_replace_required_arguments(text):
+def test_replace_required_arguments():
     """
     Two arguments are required.
     Old text as a first argument, new text as a second argument.
     """
-    assert text.replace("one", "three") == "three three was a race horse, two two was three too."
+    assert TEXT.replace("one", "three") == "three three was a race horse, two two was three too."
 
 
-def test_replace_with_occurrences_number(text):
+def test_replace_with_occurrences_number():
     """Can provide number of occurrences as a third argument.
     Default value is -1, which would remove all occurrences"""
-    assert text.replace("one", "three", -1) == "three three was a race horse, two two was three too."
-    assert text.replace("one", "three", 2) == "three three was a race horse, two two was one too."
+    assert TEXT.replace("one", "three", -1) == "three three was a race horse, two two was three too."
+    assert TEXT.replace("one", "three", 2) == "three three was a race horse, two two was one too."
--- tests/akaiafiuk/string_methods_tests/strip_tests.py
+++ tests/akaiafiuk/string_methods_tests/strip_tests.py
@@ -1,11 +1,10 @@
 def test_strip_no_args():
     """Strip with no args returns a copy of the string with leading and trailing whitespace removed."""
     some_string = "    Hello There    "
-    assert some_string.strip() == "Hello There"  # A new string is returned
-    assert some_string == "    Hello There    "  # Original string is not changed
+    assert some_string.strip() == "Hello There"
 
 
 def test_strip_with_args():
     """If chars is given and not None, remove characters in chars instead."""
-    some_string = "xxx    Hello xy There    yyy"
+    some_string = "xyyxyxx    Hello xy There    yxyxyxyxyyxxyyyx"
     assert some_string.strip("xy") == "    Hello xy There    "
