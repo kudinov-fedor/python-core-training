d144454e71c50aa950fe918c7f3e900370b26f5c
Anton Kaiafiuk <akaiafiuk@gmail.com>
2022-12-12 19:32:10 +0200

    separate, simpler tests

--- tests/akaiafiuk/string_methods_tests/split_tests.py
+++ tests/akaiafiuk/string_methods_tests/split_tests.py
@@ -1,24 +1,16 @@
 import pytest
 
-TEST_STRING = "You know I'm born to lose, and gambling's for fools"
 
-ER_1 = ['You', 'know', "I'm", 'born', 'to', 'lose,', 'and', "gambling's", 'for', 'fools']
-ER_2 = ["You know I'm born to lose", "and gambling's for fools"]
-ER_3 = ['You', 'know', "I'm born to lose, and gambling's for fools"]
-
-
-@pytest.mark.parametrize('argument, result', [
-    (None, ER_1),       # No arguments. Split using ' ' separator by default
-    (", ", ER_2),       # ', ' as a separator
-])
-def test_split(argument, result):
-    """
-    Return a list of the words in the string, using sep as the delimiter string.
-    Can be used without arguments or with sep
-    """
-    assert TEST_STRING.split(argument) == result
+def test_split_no_args():
+    """Split with no arguments splits using space as a separator."""
+    assert "Hello World".split() == ['Hello', 'World']
 
 
-def test_split_with_args():
-    """Can be used with and sep and maxsplit arguments"""
-    TEST_STRING.split(' ', 2) == ER_3
+def test_split_separator():
+    """Split with no arguments splits using space as a separator."""
+    assert "Hello, World".split(', ') == ['Hello', 'World']
+
+
+def test_split_max_split():
+    """max_split second arg is used to specify number of splits. Len of list will be max_split + 1"""
+    assert "Hello World !".split(' ', 1) == ['Hello', 'World !']
