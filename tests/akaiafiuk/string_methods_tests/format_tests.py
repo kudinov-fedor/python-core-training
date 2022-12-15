0aadc36027380b96530493f1ffcf552782c3d292
Anton Kaiafiuk <akaiafiuk@gmail.com>
2022-12-15 19:57:09 +0200

    rsplit and format tests

---
+++ tests/akaiafiuk/string_methods_tests/format_tests.py
@@ -0,0 +1,31 @@
+import pytest
+
+
+def test_format_using_indexes():
+    """format string using named indexes"""
+    test_string = "My name is {name}. I live in {city}".format(name="Anton", city="Kyiv")
+    assert test_string == "My name is Anton. I live in Kyiv"
+
+
+def test_format_using_numbered_indexes():
+    """format string using numbered indexes"""
+    test_string = "My name is {0}. I live in {1}".format("Anton", "Kyiv")
+    assert test_string == "My name is Anton. I live in Kyiv"
+
+
+def test_format_using_empty_placeholders():
+    """format string using empty placeholders"""
+    test_string = "My name is {}. I live in {}".format("Anton", "Kyiv")
+    assert test_string == "My name is Anton. I live in Kyiv"
+
+
+def test_format_too_many_values():
+    """format string using empty placeholders"""
+    test_string = "My name is {}. I live in {}".format("Anton", "Kyiv", "outside range")
+    assert test_string == "My name is Anton. I live in Kyiv"
+
+
+def test_format_index_not_exist():
+    """format string using empty placeholders"""
+    with pytest.raises(IndexError, match="Replacement index 4 out of range for positional args tuple"):
+        "My name is {1}. I live in {4}".format("Anton", "Kyiv", "outside range")
