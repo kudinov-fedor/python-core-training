617b75302ce93a76c6b57714c4d9288e9e80cab8
Anton Kaiafiuk <akaiafiuk@gmail.com>
2023-01-17 20:42:13 +0200

    additional tests for __format__, __repr__ and __str__

--- tests/akaiafiuk/string_methods_tests/advanced_formatting_tests/format_custom_object.py
+++ tests/akaiafiuk/string_methods_tests/advanced_formatting_tests/format_custom_object.py
@@ -8,6 +8,12 @@ class ObjectWithFormatDefined:
             return '101010'
         return '42'
 
+    def __str__(self):
+        return 'str'
+
+    def __repr__(self):
+        return 'repr'
+
 
 def test_formatting_bin():
     assert "{:bin}".format(ObjectWithFormatDefined()) == '101010'
@@ -15,3 +21,11 @@ def test_formatting_bin():
 
 def test_formatting_other():
     assert "{}".format(ObjectWithFormatDefined()) == '42'
+
+
+def test_format_str():
+    assert '{0!s}'.format(ObjectWithFormatDefined()) == "str"
+
+
+def test_format_str():
+    assert '{0!r}'.format(ObjectWithFormatDefined()) == "repr"
