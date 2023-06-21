12db8e84585c47a299beb62ed8c3bbbc2c726847
Khrystyna Tkachyk <tkachykhr@gmail.com>
2023-06-21 14:51:59 +0300

    Updated commit files

--- tests/khrtkachyk/hw_1_khr_operations_2_0.py
+++ tests/khrtkachyk/hw_1_khr_operations_2_0.py
@@ -2,11 +2,10 @@ import pytest
 
 
 @pytest.mark.parametrize(["par1", "res"], [
-    (2 - (5 * 3) ^ 2, -15),
-    (2 - (5 * 3) ^ 3, -15),
-    (0, -15),
-    (2 - (5 * 0) ^ 2, -15)
+    (2 - (5 * 3) ** 2, 2 - (5 * 3) ** 2),
+    (2 - (5 * 3) ** 3, 2 - (5 * 3) ** 2),
+    (0, 2 - (5 * 3) ** 2),
+    (2 - (5 * 0) ** 2, 2 - (5 * 3) ** 2)
 ])
-def test_xor(par1, res):
-    res = 2 - (5 * 3) ^ 2
+def test_operators(par1, res):
     assert par1 == res
