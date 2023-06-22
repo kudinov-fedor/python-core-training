84a75901e9ae5d6553ca15415120b20a26eac043
Olha Popova <gurtovayaolenka@gmail.com>
2023-06-23 00:03:36 +0300

    fix for PR comments

--- tests/olpopova/homework/homework01/first_test_part2.py
+++ tests/olpopova/homework/homework01/first_test_part2.py
@@ -2,23 +2,17 @@ import pytest
 
 
 @pytest.mark.parametrize(['num1', 'num2', 'res'], [
-    # ('abc', 2, 'abcabc'),
-    # ((2, ), 5, 22222),
-    (2, 5, 22222),
+    ('abc', 2, 'abcabc'),
+    ((2, ), 5, (2, 2, 2, 2, 2)),
+    (2, 5, 10),
     ([4, 5], 2, [4, 5, 4, 5])
 ])
 def test_multiplicaion(num1, num2, res):
-    return num1 * num2
+    assert num1 * num2 == res
 
 
-# @pytest.mark.parametrize(['num1', 'num2', 'res'], [
-#     ((1, 4), (2, 6), (1, 4, 2, 6)),
-#     ([4, 5], [3, 9], [4, 5, 3, 9]),
-# ])
-# def sum_operation(num1, num2, res):
 def sum_operation(num1, num2):
     return num1 + num2
-# assert num1 + num2 == res
 
 
 actual_list = sum_operation([4, 5], [3, 9])
