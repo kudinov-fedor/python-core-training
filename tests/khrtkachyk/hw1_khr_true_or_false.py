bfc4a404324aa5f76712d5723cb915673d7bfb01
Khrystyna Tkachyk <tkachykhr@gmail.com>
2023-06-22 10:21:36 +0300

    true or false 1.0

--- tests/khrtkachyk/hw1_khr_true_or_false.py
+++ tests/khrtkachyk/hw1_khr_true_or_false.py
@@ -1,3 +1,15 @@
 import pytest
 
 
+@pytest.mark.parametrize(["par1", "res"], [
+    ((lambda: len([False, ]) > 0), True),
+    ((lambda: len({1, "abc"}) > 0), True),
+    ((lambda: bool({})), False),
+    ((lambda: len([False, ]) == 0), False),
+    ((lambda: not (1, 2, 3)), False),
+    ((lambda: "it is true" if [None] else "it is false"), "it is true"),
+    ((lambda: True and False), False),
+    ((lambda: True or False), True)
+])
+def test_true_false(par1, res):
+    assert par1() is res
