2f0642887d7598c2821d2777f00da2fbdfe53ebc
Olha Popova <gurtovayaolenka@gmail.com>
2023-06-24 00:38:01 +0300

    small fixes

--- tests/olpopova/homework/homework02/task2.py
+++ tests/olpopova/homework/homework02/task2.py
@@ -1,7 +1,8 @@
 """
-***** Task2 *****
+***** Task2 *********************************************************
 You should write a function that will receive a positive integer and return: "Fizz" if the number is divisible
 by 3 (3, 6, 9 ...) otherwise convert the given number to a string (2 -> "2").
+*********************************************************************
 """
 
 
@@ -11,3 +12,10 @@ def checkio(num: int) -> str:
     else:
         res = str(num)
     return res
+
+
+# These "asserts" are used for self-checking
+assert checkio(3) == "Fizz"
+assert checkio(7) == "7"
+assert checkio(27) == "Fizz"
+assert checkio(0) == "Fizz"
