213ea0afa6d3b77ebf9e6af6a8d5ed9a00bff477
Andriy Havryshkevych <gavandre@gmail.com>
2023-06-23 12:15:45 +0300

    renamed test namees, added new lines between tests, also verified the code with flake8 lib

--- tests/ahavryshkevych/test_hw2_comparison.py
+++ tests/ahavryshkevych/test_hw2_comparison.py
@@ -1,37 +1,40 @@
 import pytest
 
 
-def test_compare_01():
+def test_compare_tuples():
     res = ("d", "o", "g") > ("d", "a", "y")
     assert res is True
 
 
-def test_compare_02():
+def test_compare_booleans():
     res = (True > False)
     assert res is True
+
 
 @pytest.mark.parametrize(["par1", "par2", "res"], [
     (["d", "o", "g"], ["d", "a", "y"], True),
     (["dog"], ["day"], True)
 ])
-
-def test_compare_03(par1, par2, res):
+def test_compare_lists(par1, par2, res):
     result = par1 > par2
     assert result is res
+
 
 @pytest.mark.parametrize(["par3", "par4", "res4"], [
     ("a", "abc", True),
     ("a", ["a", "b", "c"], True),
     ("z", ["a", "b", "c"], False)
 ])
-def test_compare_04(par3, par4, res4):
+def test_compare_string_existence(par3, par4, res4):
     result = par3 in par4
     assert result is res4
 
-def test_compare_05():
+
+def test_compare_str_sorting():
     result = sorted(["banana", "ball", "cat"])
     assert result[0] == "ball"
 
-def test_compare_06():
+
+def test_compare_str_sorting_by_lenght():
     result = sorted(["banana", "ball", "cat"], key=len)
     assert result[0] == "cat"
