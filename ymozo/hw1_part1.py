301ceb20e9cabc2b2ac2b2dcdf9e2e540b04eaff
Yurii Mozola <yurii.mozola@spirent.com>
2023-06-23 13:32:37 +0300

    resolved_comments

--- ymozo/hw1_part1.py
+++ ymozo/hw1_part1.py
@@ -3,12 +3,12 @@ import pytest
 
 @pytest.mark.parametrize(["par1", "par2", "res"], [
     (["d", "o", "g"], ["d", "a", "y"], True),
-    (["d", "o", "g"], ["d", "a", "y"], True),
     (["d", "a", "g"], ["d", "o", "y"], False),
     (["a", "b", "c"], ["z", "x", "w"], False),
     ([1, 2], [1, 3], False),
     (["dog"], ["day"], True),
     ([True], [False], True),
+    (True, False, True)
 ])
 def test_compare_string(par1, par2, res):
     result = par1 >= par2
@@ -26,14 +26,25 @@ def test_check_equality(par1, par2, res):
     assert result is res
 
 
-def test_check_contains():
-    a = "a" in ["a", "b", "c"]
-    assert a is True
+@pytest.mark.parametrize(["par1", "par2", "res"], [
+    ("a", ["a", "b", "c"], True),
+    ("a", ["abc"], False),
+    ("ab", "abc", True),
+    ("ab", ["a", "b", "c"], False)
+])
+def test_check_contains_in(par1, par2, res):
+    result = par1 in par2
+    assert result is res
 
 
-def test_check_contains_1():
-    a = "z" in ["a", "b", "c"]
-    assert a is False
+@pytest.mark.parametrize(["par1", "par2", "res"], [
+    ("z", ["a", "b", "c"], True),
+    ("z", "abc", True),
+    ("z", ("a", "b", "c"), True),
+])
+def test_check_contains_not_in(par1, par2, res):
+    result = par1 not in par2
+    assert result is res
 
 
 def test_implicit_comparison():
@@ -50,4 +61,3 @@ def test_check_reverse():
     a = sorted(["banana", "ball", "cat"], reverse=True, key=len)
     res = ["banana", "ball", "cat"]
     assert a == res
-
