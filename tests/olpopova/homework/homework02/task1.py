35e77eb008c1a5f845c00a04705b216932346396
Olha Popova <gurtovayaolenka@gmail.com>
2023-06-23 21:13:48 +0300

    backward string

---
+++ tests/olpopova/homework/homework02/task1.py
@@ -0,0 +1,24 @@
+"""
+****** Task1 ******
+Backward String
+*******************
+"""
+
+
+def backward_string(val: str) -> str:
+    chars_list = []
+    new_str = ''
+    for char in val:
+        chars_list.append(char)
+    for char in chars_list.__reversed__():
+        new_str += char
+    return new_str
+
+
+# These "asserts" are used for self-checking
+assert backward_string("val") == "lav"
+assert backward_string("") == ""
+assert backward_string("ohho") == "ohho"
+assert backward_string("123456789") == "987654321"
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
