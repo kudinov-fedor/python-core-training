e89424aea28eb27e5590c67ccbebc63e0e00cd9f
Olha Popova <gurtovayaolenka@gmail.com>
2023-06-23 23:27:31 +0300

    password verification. more complex

--- tests/olpopova/homework/homework02/task6.py
+++ tests/olpopova/homework/homework02/task6.py
@@ -1,10 +1,17 @@
 """
-***** Task6 *****
+***** Task6 **********************************************************
 In this mission you need to create a password verification function.
-*****************
+**********************************************************************
 The verification conditions are:
     the length should be bigger than 6;
     should contain at least one digit.
+**********************************************************************
+4th edition:
+
+The verification conditions are:
+    the length should be bigger than 6;
+    should contain at least one digit, but it cannot consist of just digits;
+    if the password is longer than 9 - previous rule (about one digit), is not required.
 """
 
 
@@ -22,3 +29,28 @@ assert is_acceptable_password("muchlonger") == False
 assert is_acceptable_password("ashort") == False
 assert is_acceptable_password("muchlonger5") == True
 assert is_acceptable_password("sh5") == False
+
+
+def is_acceptable_password_4th_edition(password: str) -> bool:
+    result = False
+    if 6 < len(password) < 9:
+        result = password.isalnum() and not password.isalpha() and not password.isdecimal()
+    elif len(password) > 9:
+        result = password.isalnum() or " " in password
+    return result
+
+
+print("Example:")
+print(is_acceptable_password_4th_edition("short"))
+
+# These "asserts" are used for self-checking
+assert is_acceptable_password_4th_edition("short") == False
+assert is_acceptable_password_4th_edition("short54") == True
+assert is_acceptable_password_4th_edition("muchlonger") == True
+assert is_acceptable_password_4th_edition("ashort") == False
+assert is_acceptable_password_4th_edition("notshort") == False
+assert is_acceptable_password_4th_edition("muchlonger5") == True
+assert is_acceptable_password_4th_edition("sh5") == False
+assert is_acceptable_password_4th_edition("1234567") == False
+assert is_acceptable_password_4th_edition("12345678910") == True
+assert is_acceptable_password_4th_edition('this is password') == True
