456cb99750bd0c42a543b83449fc413dea57f993
Olha Popova <gurtovayaolenka@gmail.com>
2023-06-24 00:41:53 +0300

    small fixes

--- tests/olpopova/homework/homework02/task6.py
+++ tests/olpopova/homework/homework02/task6.py
@@ -1,7 +1,7 @@
 """
 ***** Task6 **********************************************************
 In this mission you need to create a password verification function.
-**********************************************************************
+
 The verification conditions are:
     the length should be bigger than 6;
     should contain at least one digit.
@@ -20,6 +20,7 @@ The verification conditions are:
     should contain at least one digit, but it cannot consist of just digits;
     having numbers or containing just numbers does not apply to the password longer than 9;
     a string should not contain the word "password" in any case.
+**********************************************************************
 """
 
 
