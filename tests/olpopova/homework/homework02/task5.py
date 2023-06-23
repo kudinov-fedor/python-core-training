74ddc37aca7212c10e87ef301ca56e76f61c5666
Olha Popova <gurtovayaolenka@gmail.com>
2023-06-23 22:25:39 +0300

    Fizz, Fizz Buzz, Buzz, str(integer)

---
+++ tests/olpopova/homework/homework02/task5.py
@@ -0,0 +1,32 @@
+"""
+***** Task5 *****
+"Fizz buzz" is a word game we will use to teach the robots about division. Let's learn computers.
+
+You should write a function that will receive a positive integer and return:
+"Fizz Buzz" if the number is divisible by 3 and by 5;
+"Fizz" if the number is divisible by 3;
+"Buzz" if the number is divisible by 5;
+The number as a string for other cases.
+"""
+
+
+def checkio(num: int) -> str:
+    if num % 3 == 0 and num % 5 == 0:
+        res = 'Fizz Buzz'
+    elif num % 3 == 0:
+        res = 'Fizz'
+    elif num % 5 == 0:
+        res = 'Buzz'
+    else:
+        res = str(num)
+    return res
+
+
+print("Example:")
+print(checkio(15))
+
+# These "asserts" are used for self-checking
+assert checkio(15) == "Fizz Buzz"
+assert checkio(6) == "Fizz"
+assert checkio(10) == "Buzz"
+assert checkio(7) == "7"
