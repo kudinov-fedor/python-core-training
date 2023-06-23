7b640259084c1418775e70126db56b482f55e69c
Olha Popova <gurtovayaolenka@gmail.com>
2023-06-24 00:40:26 +0300

    small fixes

--- tests/olpopova/homework/homework02/task5.py
+++ tests/olpopova/homework/homework02/task5.py
@@ -1,5 +1,5 @@
 """
-***** Task5 *****
+***** Task5 *********************************************************
 "Fizz buzz" is a word game we will use to teach the robots about division. Let's learn computers.
 
 You should write a function that will receive a positive integer and return:
@@ -7,6 +7,7 @@ You should write a function that will receive a positive integer and return:
 "Fizz" if the number is divisible by 3;
 "Buzz" if the number is divisible by 5;
 The number as a string for other cases.
+*********************************************************************
 """
 
 
@@ -21,9 +22,6 @@ def checkio(num: int) -> str:
         res = str(num)
     return res
 
-
-print("Example:")
-print(checkio(15))
 
 # These "asserts" are used for self-checking
 assert checkio(15) == "Fizz Buzz"
