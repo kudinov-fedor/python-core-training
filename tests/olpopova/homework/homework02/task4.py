83efaf404cf8c7b1d8a91b6d28b3976292d64735
Olha Popova <gurtovayaolenka@gmail.com>
2023-06-24 00:39:21 +0300

    small fixes

--- tests/olpopova/homework/homework02/task4.py
+++ tests/olpopova/homework/homework02/task4.py
@@ -1,15 +1,13 @@
 """
-***** Task4 *****
+***** Task4 *********************************************************
 You have a non-negative integer. Try to find out how many digits it has.
+*********************************************************************
 """
 
 
 def number_length(value: int) -> int:
     return len(str(value))
 
-
-print("Example:")
-print(number_length(10))
 
 # These "asserts" are used for self-checking
 assert number_length(10) == 2
