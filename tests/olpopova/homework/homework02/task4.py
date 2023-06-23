02f613165978425519cf4751d5564a77df0e328c
Olha Popova <gurtovayaolenka@gmail.com>
2023-06-23 22:14:01 +0300

    how many digits has integer

---
+++ tests/olpopova/homework/homework02/task4.py
@@ -0,0 +1,18 @@
+"""
+***** Task4 *****
+You have a non-negative integer. Try to find out how many digits it has.
+"""
+
+
+def number_length(value: int) -> int:
+    return len(str(value))
+
+
+print("Example:")
+print(number_length(10))
+
+# These "asserts" are used for self-checking
+assert number_length(10) == 2
+assert number_length(0) == 1
+assert number_length(4) == 1
+assert number_length(44) == 2
