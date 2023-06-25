3f403e988ef0b0175f8a5e7c1223f04a22475d2f
Andriy Havryshkevych <gavandre@gmail.com>
2023-06-25 22:58:28 +0300

    refactored file names

--- tests/ahavryshkevych/test_hw3_acceptable_pwd_3.py
+++ tests/ahavryshkevych/test_hw3_acceptable_pwd_3.py
@@ -1,10 +1,11 @@
+# Taken from mission Acceptable Password II
+
 # Taken from mission Acceptable Password I
 def is_acceptable_password(password: str) -> str:
     pwd_lenght = len(password) > 6
     pwd_digit = any(char.isdigit() for char in password)
-    return pwd_lenght and pwd_digit
-# def is_acceptable_password(password: str) -> bool:
-    # return len(password) > 6 and any(element.isdigit() for element in password)
+    all_digits_check = all(char.isdigit() for char in password)
+    return pwd_lenght and pwd_digit and all_digits_check is not True
 
 
 print("Example:")
@@ -16,5 +17,6 @@ assert is_acceptable_password("muchlonger") == False
 assert is_acceptable_password("ashort") == False
 assert is_acceptable_password("muchlonger5") == True
 assert is_acceptable_password("sh5") == False
+assert is_acceptable_password("1234567") == False
 
 print("The mission is done! Click 'Check Solution' to earn rewards!")
