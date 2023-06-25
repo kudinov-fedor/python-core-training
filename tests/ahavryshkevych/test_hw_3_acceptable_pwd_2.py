ad65c337817ccda868956481140b2f54f15c252c
Andriy Havryshkevych <gavandre@gmail.com>
2023-06-25 14:08:58 +0300

    the third excercise

---
+++ tests/ahavryshkevych/test_hw_3_acceptable_pwd_2.py
@@ -0,0 +1,17 @@
+# Taken from mission Acceptable Password I
+
+def is_acceptable_password(password: str) -> bool:
+    return len(password) > 6 and any(element.isdigit() for element in password)
+
+
+print("Example:")
+print(is_acceptable_password("short"))
+
+# These "asserts" are used for self-checking
+assert is_acceptable_password("short") == False
+assert is_acceptable_password("muchlonger") == False
+assert is_acceptable_password("ashort") == False
+assert is_acceptable_password("muchlonger5") == True
+assert is_acceptable_password("sh5") == False
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
