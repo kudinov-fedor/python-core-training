f66a15d7378596427c29e79e29d391925173668f
Khrystyna Tkachyk <tkachykhr@gmail.com>
2023-06-25 22:14:16 +0300

    changes_v3.0

--- khrtkachyk/checkio_tasks/task_10_Acceptable_Pass_v_IV.py
+++ khrtkachyk/checkio_tasks/task_10_Acceptable_Pass_v_IV.py
@@ -11,21 +11,24 @@ Output: A logic value (bool).
 
 def is_acceptable_password(password: str) -> bool:
     valid_length = 6 < len(password) <= 9
-    not_too_short = len(password) <= 6
+    too_short = len(password) <= 6
     at_least_one_digit = any(i.isdigit() for i in password)
+    all_digits = all(i.isdigit() for i in password)
     not_only_digits = any(i.isalpha() for i in password)
     is_long_password = len(password) > 9
 
-    just_valid_length = valid_length and at_least_one_digit and not_only_digits
-    valid_short = not not_too_short and at_least_one_digit and not_only_digits
-    valid_long = is_long_password and not_only_digits
+    valid_short = not too_short and at_least_one_digit and not_only_digits
+    valid_long = is_long_password and (not_only_digits or all_digits)
+    just_valid_length = valid_length and not_only_digits and at_least_one_digit
 
-    pass_is_valid = valid_long or valid_short or just_valid_length
+    pass_is_valid = just_valid_length or valid_long or valid_short
     return pass_is_valid
 
 
 if __name__ == '__main__':
     print("Example:")
+    print(is_acceptable_password("sh5"))
+    print(is_acceptable_password("1234567"))
     print(is_acceptable_password("short"))
     print(is_acceptable_password("short54"))
     print(is_acceptable_password("notshort"))
