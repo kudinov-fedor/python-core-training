07ceb09c098329dbdf8f66f332c4950f3e73642f
Andriy Havryshkevych <gavandre@gmail.com>
2023-06-25 22:43:29 +0300

    updated

--- tests/ahavryshkevych/test_hw_3_acceptable_pwd_2.py
+++ tests/ahavryshkevych/test_hw_3_acceptable_pwd_2.py
@@ -11,7 +11,7 @@ print("Example:")
 print(is_acceptable_password("short"))
 
 # These "asserts" are used for self-checking
-assert is_acceptable_password("short") == False
+assert is_acceptable_password("short") == Fal
 assert is_acceptable_password("muchlonger") == False
 assert is_acceptable_password("ashort") == False
 assert is_acceptable_password("muchlonger5") == True
