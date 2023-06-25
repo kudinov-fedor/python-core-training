284abcf80bf7323236d3f37a7b6e9258d18c864c
Alesia Hancharyk <alesiahanc@gmail.com>
2023-06-25 22:29:23 +0200

    fixed last comment

--- tests/alesiahanc/hw2_checkio_task2.py
+++ tests/alesiahanc/hw2_checkio_task2.py
@@ -7,9 +7,7 @@ The text consists of numbers, spaces and letters from the English alphabet.
 
 def sum_numbers(text: str) -> int:
     elements = text.split(" ")
-    digits = [x for x in elements if x.isdigit()]
-    result = list(map(int, digits))
-    return sum(result)
+    return sum(int(x) for x in elements if x.isdigit())
 
 
 if __name__ == '__main__':
