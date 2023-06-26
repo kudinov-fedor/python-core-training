bdefb5dcb8d8ca85aa353cf73fee4331e4b7dc49
okindrat <kindolenka@gmail.com>
2023-06-26 15:01:04 +0300

    my first commit

--- okindrat/__init__.py
+++ okindrat/__init__.py
@@ -1,7 +1,17 @@
-flake8 YOUR_NAME_HERE tests/okindrat
+flake8 okindrat tests/okindrat
 
 flake8 okindrat tests/okindrat
 flake8 tests/okindrat
 flake8 tests/okindrat\ "__init__.py"
 
 git status
+git checkout origin/main  -b okindrat/my_first_commi
+
+git commit -m “my first commit”
+
+git push origin okindrat/my_first_commit
+
+git checkout origin/main  -b okindrat/my_first_commit
+
+pytest tests/okindrat
+flake8 okindrat tests/okindrat
