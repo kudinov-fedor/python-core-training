8515c48d6b66660d5a6f1901c4a46fd39b51d0f5
Yurii Leshchuk <ylesh@softserveinc.com>
2023-06-26 14:44:31 +0300

    added hw2_task2

---
+++ tests/ylesh/homework_2/task_is_even.py
@@ -0,0 +1,12 @@
+"""
+Check if the given number is even or not. Your function should return True if the number is even, and False if the
+number is odd.
+"""
+
+
+def is_even(num: int) -> bool:
+    return num % 2 == 0
+
+
+print(is_even(2))
+print(is_even(3))
