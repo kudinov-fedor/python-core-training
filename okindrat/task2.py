bef0930306faaa08962ce1a71ca964dd517d5734
okindrat <kindolenka@gmail.com>
2023-06-26 15:52:11 +0300

    task1 from checkio -solved

---
+++ okindrat/task2.py
@@ -0,0 +1,18 @@
+#you need to create a password verification function.The verification condition is:
+# the length should be bigger than 6.
+def is_acceptable_password(password: str) -> bool:
+    if len(password) > 6:
+        return True
+    else:
+        return False
+
+
+print("Example:")
+print(is_acceptable_password("short"))
+
+# These "asserts" are used for self-checking
+assert is_acceptable_password("short") == False
+assert is_acceptable_password("muchlonger") == True
+assert is_acceptable_password("ashort") == False
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
