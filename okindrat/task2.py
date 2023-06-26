0e55c401f459bbcd0ff128db6a5c25c3cde1fc81
okindrat <kindolenka@gmail.com>
2023-06-26 15:55:26 +0300

    task2 from checkio -solved

--- okindrat/task2.py
+++ okindrat/task2.py
@@ -1,5 +1,5 @@
 #you need to create a password verification function.The verification condition is:
-# the length should be bigger than 6.
+# the length should be bigger than 6
 def is_acceptable_password(password: str) -> bool:
     if len(password) > 6:
         return True
