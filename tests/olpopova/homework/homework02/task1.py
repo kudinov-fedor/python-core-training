1cfae7712082c6ad12d92a0a8590dec15018bd94
Olha Popova <gurtovayaolenka@gmail.com>
2023-06-26 16:53:14 +0300

    small fix

--- tests/olpopova/homework/homework02/task1.py
+++ tests/olpopova/homework/homework02/task1.py
@@ -6,13 +6,7 @@ Backward String
 
 
 def backward_string(val: str) -> str:
-    chars_list = []
-    new_str = ''
-    for char in val:
-        chars_list.append(char)
-    for char in chars_list.__reversed__():
-        new_str += char
-    return new_str
+    return ''.join(reversed(val))
 
 
 # These "asserts" are used for self-checking
@@ -20,3 +14,14 @@ assert backward_string("val") == "lav"
 assert backward_string("") == ""
 assert backward_string("ohho") == "ohho"
 assert backward_string("123456789") == "987654321"
+
+
+def backward_string2(val: str) -> str:
+    return val[::-1]
+
+
+# These "asserts" are used for self-checking
+assert backward_string2("val") == "lav"
+assert backward_string2("") == ""
+assert backward_string2("ohho") == "ohho"
+assert backward_string2("123456789") == "987654321"
