922979d98efbbcad323f691dc1f3cba76a0b53c3
Olha Popova <gurtovayaolenka@gmail.com>
2023-06-27 13:58:58 +0300

    small fix

--- tests/olpopova/homework/homework02/task4.py
+++ tests/olpopova/homework/homework02/task4.py
@@ -10,41 +10,41 @@ def number_length(value: int) -> int:
     return len(str(value))
 
 
-# These "asserts" are used for self-checking
-assert number_length(10) == 2
-assert number_length(0) == 1
-assert number_length(4) == 1
-assert number_length(44) == 2
+def number_length_with_log_10(value: int) -> int:
+    # edge cases
+    if value == 0:
+        return 1
+
+    # intermediate values
+    log_10 = log10(value)
+    value_threshold = log_10 - floor(log_10) < 0.5
+    long_float = value > len('a' * 14)
+
+    # final steps
+    char_length_gt_15 = ceil(log_10)
+    char_length_lt_15 = round(log_10) + 1 if value_threshold else ceil(log_10)
+
+    return char_length_gt_15 if long_float else char_length_lt_15
 
 
-def number_length2(value: int) -> int:
-    return 1 if value == 0 else (ceil(log10(value)) if log10(value) > 1.5 else ceil(log10(value)) + 1)
-
-
-# These "asserts" are used for self-checking
-assert number_length2(10) == 2
-assert number_length2(0) == 1
-assert number_length2(56890) == 5
-assert number_length2(44) == 2
-assert number_length2(87860404) == 8
-assert number_length2(8786040432) == 10
-assert number_length2(999999999999999) == 15
-
-
-def x(value: int) -> int:
+def number_length_with_div_mod(value: int) -> int:
     qty = 1
-    if value != 0:
-        while divmod(value, 10)[0] != 0:
-            value /= 10
-            qty += 1
+
+    # edge cases
+    if value == 0:
+        return qty
+    elif value > len('9' * 16):
+        return number_length(value)
+
+    # final steps
+    while divmod(value, 10)[0] != 0:
+        value /= 10
+        qty += 1
+
     return qty
 
 
 # These "asserts" are used for self-checking
-assert x(10) == 2
-assert x(0) == 1
-assert x(56890) == 5
-assert x(44) == 2
-assert x(87860404) == 8
-assert x(8786040432) == 10
-assert x(999999999999999) == 15
+for i in range(1, 50):
+    val = int("9" * i)
+    assert number_length(val) == number_length_with_log_10(val) == number_length_with_div_mod(val)
