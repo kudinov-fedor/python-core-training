227a9484ad792905151cd688b367f863199f68f2
Khrystyna Tkachyk <tkachykhr@gmail.com>
2023-06-28 21:38:21 +0300

    khrtkachyk/hw3_introspection_v_2.0

--- tests/khrtkachyk/test_hw3_introspection.py
+++ tests/khrtkachyk/test_hw3_introspection.py
@@ -1,30 +1,81 @@
 import pytest
 
+# 1. Tests for "test_check_ident_equal" function
+a = lambda a: a
+b = lambda b: b
 
-@pytest.mark.parametrize(["par", "par0", "res0"], [
-    ((1, 2, 3), (1, 2, 3), True),
-    (id((1, 2, 3)), id((1, 2, 3)), True),
-    ([1, 2, 3], [1, 2, 3], True),
-    (id([1, 2, 3]), id([1, 2, 3]), False)
+
+@pytest.mark.parametrize("a", [
+    [1, 2, 3],
+    (1, 2, 3),
+    ([1, 2], [3, 4]),
+    (lambda: ([1, 2], [3, 4]))
 ])
-def test_equality(par, par0, res0):
-    assert (par == par0) is res0
+def test_check_ident_equal(a):
+    """
+    Create two references to one object
+    Check that two references' objects are identical and equal
+    """
+    b = a
+    assert a is b
+    assert a == b
 
 
+# 2. Tests for "test_callable" function
+c = lambda c: c
+
+
+def new_func():
+    return c
+
+
+@pytest.mark.parametrize(["par", "res"], [
+    (lambda: True, False),
+    (lambda: [1, 2, 3], False),
+    (lambda: (bool, int), False),
+    (lambda: c, True),
+    (lambda: new_func(), True)
+])
+def test_callable(par, res):
+    """
+    Check whether a specific parameter is callable
+    """
+    assert callable(par()) is res
+
+
+# 3. Tests for "test_identity" function
 @pytest.mark.parametrize(["par1", "par2", "res1"], [
-    (lambda: (1, 2, 3), lambda: (1, 2, 3), True),
     (lambda: ([1, 2], [3, 4]), lambda: ([1, 2], [3, 4]), False)
 ])
 def test_identity(par1, par2, res1):
+    """
+    Create two lambda-functions with the same expression
+    Check that two lambda-functions are not identical
+    """
     assert (par1() is par2()) is res1
 
 
-@pytest.mark.parametrize(["n", "res"], [
-    (3, "<class 'generator'>")
+# 4. Tests for "test_type_class_checks" function
+class myClass:
+    try:
+        5 / 0
+    except ZeroDivisionError as error:
+        raised_error = error
+
+myclass_instance = myClass()
+
+
+@pytest.mark.parametrize(["par3", "par4", "res2"], [
+    ((lambda: True), bool, False),
+    (3.2, float, True),
+    ("Hi", (float, str, list, tuple, dict, repr(str)), True),
+    ("Hello", (float, list, tuple, dict), False),
+    (myclass_instance, myClass, True),
+    (myclass_instance, (int, dict, str), False),
+    (myClass.raised_error, ZeroDivisionError, True),
 ])
-def test_yield_numbers(n, res):
-    start_num = 0
-    while start_num < n:
-        yield start_num
-        start_num += 1
-    assert (test_yield_numbers(n)).__class__ == res
+def test_type_class_checks(par3, par4, res2):
+    """
+    Check whether an object is of specific class
+    """
+    assert (isinstance(par3, par4)) is res2
