7399b871f6051c8ff4af9efc3244810cbe3d5066
Yurii Mozola <yurii.mozola@spirent.com>
2023-06-28 21:42:41 +0300

    added_checkio_goes_right_after

---
+++ ymozo/hw_2/checkio_goes_right_fter.py
@@ -0,0 +1,27 @@
+def goes_after(word: str, first: str, second: str) -> bool:
+    if first in word and second in word:
+        first = word.index(first)
+        second = word.index(second)
+        return second - first == 1
+    return False
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(goes_after("world", "w", "o"))
+
+    # These "asserts" are used for self-checking
+    assert goes_after("world", "w", "o") == True
+    assert goes_after("world", "w", "r") == False
+    assert goes_after("world", "l", "o") == False
+    assert goes_after("list", "l", "o") == False
+    assert goes_after("", "l", "o") == False
+    assert goes_after("list", "l", "l") == False
+    assert goes_after("world", "d", "w") == False
+    assert goes_after('cable', 'l', 'b') == False
+    assert goes_after('cable', 'a', 'b') == True
+    assert goes_after('copyrightable', 'o', 'p') == True
+    assert goes_after('copyrightable', 'o', 'a') == False
+    assert goes_after('copyrightable', 'o', 'o') == False
+
+    print("The mission is done! Click 'Check Solution' to earn rewards!")
