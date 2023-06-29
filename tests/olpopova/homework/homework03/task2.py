4807b818af9cfc326925090b78328a3e1d2f4529
Olha Popova <gurtovayaolenka@gmail.com>
2023-06-29 13:50:25 +0300

    small fixes

--- tests/olpopova/homework/homework03/task2.py
+++ tests/olpopova/homework/homework03/task2.py
@@ -5,15 +5,20 @@ Max Digit
 You have a number and you need to determine which digit in this number is the biggest.
 *********************************************************************
 """
+import pytest
 
 
 def max_digit(value: int) -> int:
     return int(max(str(value)))
 
 
-assert max_digit(0) == 0
-assert max_digit(52) == 5
-assert max_digit(634) == 6
-assert max_digit(1) == 1
-assert max_digit(1000) == 1
-assert max_digit(3579) == 9
+@pytest.mark.parametrize(['value', 'expected'], [
+    (0, 0),
+    (52, 5),
+    (634, 6),
+    (1, 1),
+    (10000, 1),
+    (3579, 9)
+])
+def test_max_digit(value, expected):
+    assert max_digit(value) == expected
