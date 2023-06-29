19ba3350c5acb83dd57461c700a276beb5f5b9cd
Khrystyna Tkachyk <tkachykhr@gmail.com>
2023-06-30 00:21:04 +0300

    updated solutions v.3_0

--- khrtkachyk/checkio_tasks/task13_Goes_right_after.py
+++ khrtkachyk/checkio_tasks/task13_Goes_right_after.py
@@ -11,12 +11,31 @@ Output: A logic value (bool).
 
 
 def goes_after(word: str, first: str, second: str) -> bool:
-    for i in range(0, len(word)-1):
-        return True if (word[i] == first and word[i + 1] == second) else False
+    for i, ch in enumerate(word[:-1]):
+        if word[i] == first and word[i+1] == second:
+            return True
     return False
+
+
+def goes_after_2_0(word2: str, first2: str, second2: str) -> bool:
+    splitted = word2.split(first2, 1)
+    if splitted[-1].startswith(second2):
+        return True
+    else:
+        return False
+
+
+def goes_after_3_0(word3: str, first3: str, second3: str) -> bool:
+    substr_to_find = first3 + second3
+    res = substr_to_find in word3
+    return res
 
 
 if __name__ == '__main__':
     print("Example:")
     print(goes_after("world", "w", "o"))
     print(goes_after("", "w", "o"))
+    print(goes_after_2_0("world", "w", "o"))
+    print(goes_after_2_0("", "w", "o"))
+    print(goes_after_3_0("world", "w", "o"))
+    print(goes_after_3_0("", "w", "o"))
--- tests/khrtkachyk/Checkio_tests/test_task_13_Goes_right_after.py
+++ tests/khrtkachyk/Checkio_tests/test_task_13_Goes_right_after.py
@@ -1,5 +1,7 @@
 import pytest
 from checkio_tasks.task13_Goes_right_after import goes_after
+from checkio_tasks.task13_Goes_right_after import goes_after_2_0
+from checkio_tasks.task13_Goes_right_after import goes_after_3_0
 
 """
 In a given string you need to check if one symbol goes right after another. If so - return True, otherwise - False.
@@ -11,9 +13,10 @@ Input: Three arguments. The first one is a given string (str), second is a symbo
  and the third is a symbol (str) that should go after the first one.
 Output: A logic value (bool).
 """
+# @pytest.mark.parametrize("func", [goes_after, goes_after_2_0, goes_after_3_0])
 
 
-@pytest.mark.parametrize(["word", "first", "second", "res"], [
+@pytest.mark.parametrize("word, first, second, res", [
     ("world", "w", "o", True),
     ("world", "w", "r", False),
     ("world", "l", "o", False),
@@ -24,3 +27,29 @@ Output: A logic value (bool).
 ])
 def test_goes_after(word, first, second, res):
     assert goes_after(word, first, second) is res
+
+
+@pytest.mark.parametrize("word2, first2, second2, res2", [
+    ("world", "w", "o", True),
+    ("world", "w", "r", False),
+    ("world", "l", "o", False),
+    ("list", "l", "o", False),
+    ("", "l", "o", False),
+    ("list", "l", "l", False),
+    ("world", "d", "w", False)
+])
+def test_goes_after_2_0(word2, first2, second2, res2):
+    assert goes_after_2_0(word2, first2, second2) is res2
+
+
+@pytest.mark.parametrize("word3, first3, second3, res3", [
+    ("world", "w", "o", True),
+    ("world", "w", "r", False),
+    ("world", "l", "o", False),
+    ("list", "l", "o", False),
+    ("", "l", "o", False),
+    ("list", "l", "l", False),
+    ("world", "d", "w", False)
+])
+def test_goes_after_3_0(word3, first3, second3, res3):
+    assert goes_after_3_0(word3, first3, second3) is res3
