478be64d83931b836e6e29c8181f9718fa24b91d
Olha Popova <gurtovayaolenka@gmail.com>
2023-06-29 23:15:00 +0300

    three words in line

---
+++ tests/olpopova/homework/homework03/three_words.py
@@ -0,0 +1,41 @@
+"""
+**********************************************************************
+Three Words
+
+Let's teach the Robots to distinguish words and numbers.
+You are given a string with words and numbers separated by whitespaces (one space). The words contains only letters.
+You should check if the string contains three words in succession. For example, the string "start 5 one two three 7 end"
+contains three words in succession.
+**********************************************************************
+"""
+import pytest
+
+
+def checkio(words: str) -> bool:
+    # edge cases
+    words_list = words.split(" ")
+    list_size = len(words_list)
+    if list_size < 3:
+        return False
+
+    # final steps
+    for i in range(0, list_size - 1):
+        if i <= list_size - 3:
+            is_all_alpha = all([words_list[i].isalpha(), words_list[i + 1].isalpha(), words_list[i + 2].isalpha()])
+
+            if not is_all_alpha:
+                continue
+
+        return is_all_alpha
+
+
+@pytest.mark.parametrize(['words', 'expected'], [
+    ("Hello World hello", True),
+    ("He is 123 man", False),
+    ("1 2 bla 3 4", False),
+    ("bla 1 bla bla bla", True),
+    ("Hi", False),
+    ('one two 3 four five 6 seven eight 9 ten eleven 12', False)
+])
+def test_checkio(words, expected):
+    assert checkio("Hello World hello") is True
