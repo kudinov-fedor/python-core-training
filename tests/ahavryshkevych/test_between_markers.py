640acc5c720cb29106ac81abfa4cdcb9218c7f95
Andriy Havryshkevych <gavandre@gmail.com>
2023-06-29 11:55:24 +0300

    second task

---
+++ tests/ahavryshkevych/test_between_markers.py
@@ -0,0 +1,13 @@
+import pytest
+
+from ahavryshkevych.task_between_markers import between_markers
+
+
+@pytest.mark.parametrize(["par1", "par2", "par3", "res"], [
+    ("What is >apple<", ">", "<", "apple"),
+    ("What is [apple]", "[", "]", "apple"),
+    ("What is ><", ">", "<", ""),
+    ("[an apple]", "[", "]", "an apple")
+])
+def test_between_markers(par1, par2, par3, res):
+    assert between_markers(par1, par2, parcd..3) == res
