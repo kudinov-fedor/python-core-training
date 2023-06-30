7ad4289f63108cb7108a146265015a16c0752804
Andriy Havryshkevych <gavandre@gmail.com>
2023-06-30 15:18:08 +0300

    updates

--- ahavryshkevych/task_goes_right_after.py
+++ ahavryshkevych/task_goes_right_after.py
@@ -9,12 +9,18 @@ second is a symbol (str) that should go first, and the third is a symbol (str) t
 def goes_after(word: str, first: str, second: str) -> bool:
     both_present = first in word and second in word
     non_equality_check = first != second
-    i, j = word.find(first), word.find(second)
 
-    if non_equality_check and both_present:
-        for i, j in zip(word, word[1:]):
-            # I Wanted to return this expression, as you have taught, buy for some reason it returns False
-            # I have to figure out why this happens
-            if i == first and j == second:
-                return True
+    if not(non_equality_check and both_present):
+        return False
+    for i, j in zip(word, word[1:]):
+        if i == first and j == second:
+            return True
     return False
+
+
+def goes_after2(word: str, first: str, second: str):
+    return str(first+second) in word
+
+
+def goes_after3(word: str, first: str, second: str):
+    return any([ (i,j) for i, j in zip(word, word[1:]) if i == first and j == second])
