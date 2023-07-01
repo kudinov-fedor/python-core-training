adcb3f1fda659fcc85c3af13b1e8f63c08800bc2
Olha Popova <gurtovayaolenka@gmail.com>
2023-07-01 20:04:50 +0300

    fix for function

--- tests/olpopova/homework/homework04/beginning_zeros.py
+++ tests/olpopova/homework/homework04/beginning_zeros.py
@@ -9,19 +9,7 @@ given string.
 
 
 def beginning_zeros(a: str) -> int:
-    length = 0
-
-    # edge case
-    if a[0] != '0':
-        return length
-
-    for i in a:
-        if i == '0':
-            length += 1
-        else:
-            break
-
-    return length
+    return len(a) - len(a.lstrip('0'))
 
 
 # These "asserts" are used for self-checking
