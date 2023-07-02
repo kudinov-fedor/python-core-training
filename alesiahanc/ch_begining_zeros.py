67daeaa4e7b792e44b56ec5cdf7515aefc8f4339
Alesia Hancharyk <alesiahanc@gmail.com>
2023-07-02 19:22:49 +0200

    added solution with for loop

--- alesiahanc/ch_begining_zeros.py
+++ alesiahanc/ch_begining_zeros.py
@@ -5,5 +5,17 @@ You need to find how many zero digits ("0") are at the beginning of the given st
 
 
 def beginning_zeros(text_number: str) -> int:
-    # your code here
-    return len(text_number) - len(text_number.lstrip('0'))  # lstrip delete all the leading characters mentioned in its argument.
+    # saved to remember lstrip method
+    # return len(text_number) - len(text_number.lstrip('0'))  # lstrip delete all the leading characters mentioned in its argument.
+
+    # Solution with for
+    count_zero = 0
+    for char in text_number:
+        if char != "0":
+            break
+        count_zero += 1
+    return count_zero
+
+
+if __name__ == '__main__':
+    beginning_zeros("00")
