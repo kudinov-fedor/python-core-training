0394b23a0823c491551e77870c9f39ee4bf359f8
Olha Popova <gurtovayaolenka@gmail.com>
2023-07-07 14:27:16 +0300

    refactoring

--- olpopova/homework/homework04/speech_module.py
+++ olpopova/homework/homework04/speech_module.py
@@ -18,40 +18,41 @@ HUNDRED = "hundred"
 
 def checkio(num: int) -> str:
     length = len(str(num))
-    if length > 1:
-        first = int(str(num)[0])
-        second = int(str(num)[1])
 
     if length == 1:
         robot_speech = FIRST_TEN[num - 1]
 
     elif length == 2:
-        robot_speech = find_speech_for_double_num(num, first, second)
+        first_int = int(str(num)[0])
+        second_int = int(str(num)[1])
+        robot_speech = find_speech_for_double_num(num, first_int, second_int)
 
     else:
-        third = int(str(num)[2])
-        robot_speech = find_speech_for_triple_num(num, first, second, third)
+        first_int = int(str(num)[0])
+        second_int = int(str(num)[1])
+        third_int = int(str(num)[2])
+        robot_speech = find_speech_for_triple_num(num, first_int, second_int, third_int)
 
     return robot_speech
 
 
-def find_speech_for_double_num(num: int, first: int, second: int) -> str:
+def find_speech_for_double_num(num: int, first_int: int, second_int: int) -> str:
     is_num_before_twenty = num < 20
     is_zero_div_by_10 = num % 10 == 0
     num_after_twenty_condition = OTHER_TENS[num // 10 - 2] if is_zero_div_by_10 else \
-        OTHER_TENS[first - 2] + " " + FIRST_TEN[second - 1]
+        OTHER_TENS[first_int - 2] + " " + FIRST_TEN[second_int - 1]
 
     return SECOND_TEN[num % 10] if is_num_before_twenty else num_after_twenty_condition
 
 
-def find_speech_for_triple_num(num: int, first: int, second: int, third: int):
-    robot_speech = FIRST_TEN[first - 1] + " " + HUNDRED
+def find_speech_for_triple_num(num: int, first_int: int, second_int: int, third_int: int):
+    robot_speech = FIRST_TEN[first_int - 1] + " " + HUNDRED
 
     if num % 100 == 0:
         return robot_speech
-    elif second == 0:
-        robot_speech += " " + FIRST_TEN[third - 1]
+    elif second_int == 0:
+        robot_speech += " " + FIRST_TEN[third_int - 1]
     else:
-        robot_speech += " " + find_speech_for_double_num(num % 100, second, third)
+        robot_speech += " " + find_speech_for_double_num(num % 100, second_int, third_int)
 
     return robot_speech
