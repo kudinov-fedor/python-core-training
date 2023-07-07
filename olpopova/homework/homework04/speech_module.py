e4540546e38f2b3401e2e2b686ab45a7c2da4ac6
Olha Popova <gurtovayaolenka@gmail.com>
2023-07-07 14:17:02 +0300

    refactoring

--- olpopova/homework/homework04/speech_module.py
+++ olpopova/homework/homework04/speech_module.py
@@ -11,28 +11,8 @@ spaces instead one.
 """
 
 FIRST_TEN = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-SECOND_TEN = [
-    "ten",
-    "eleven",
-    "twelve",
-    "thirteen",
-    "fourteen",
-    "fifteen",
-    "sixteen",
-    "seventeen",
-    "eighteen",
-    "nineteen",
-]
-OTHER_TENS = [
-    "twenty",
-    "thirty",
-    "forty",
-    "fifty",
-    "sixty",
-    "seventy",
-    "eighty",
-    "ninety",
-]
+SECOND_TEN = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+OTHER_TENS = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 HUNDRED = "hundred"
 
 
@@ -42,7 +22,7 @@ def checkio(num: int) -> str:
         first = int(str(num)[0])
         second = int(str(num)[1])
 
-    if 0 < num < 10:
+    if length == 1:
         robot_speech = FIRST_TEN[num - 1]
 
     elif length == 2:
@@ -50,28 +30,28 @@ def checkio(num: int) -> str:
 
     else:
         third = int(str(num)[2])
-        robot_speech =  FIRST_TEN[first - 1] + " " + HUNDRED
-        if num % 100 == 0:
-            return robot_speech
-        elif str(second) + str(third) == '10':
-            robot_speech += " " + SECOND_TEN[0]
-        elif num % 10 == 0:
-            robot_speech += " " + OTHER_TENS[second - 2]
-        elif second == 0:
-            robot_speech += " " + FIRST_TEN[third - 1]
-        elif second == 1:
-            robot_speech += " " + SECOND_TEN[int(str(num)[1:]) % 10]
-        else:
-            robot_speech += " " + OTHER_TENS[second - 2] + " " + FIRST_TEN[third - 1]
+        robot_speech = find_speech_for_triple_num(num, first, second, third)
 
     return robot_speech
 
 
 def find_speech_for_double_num(num: int, first: int, second: int) -> str:
-
-    is_num_before_twenty = 10 <= num < 20
+    is_num_before_twenty = num < 20
     is_zero_div_by_10 = num % 10 == 0
     num_after_twenty_condition = OTHER_TENS[num // 10 - 2] if is_zero_div_by_10 else \
         OTHER_TENS[first - 2] + " " + FIRST_TEN[second - 1]
 
     return SECOND_TEN[num % 10] if is_num_before_twenty else num_after_twenty_condition
+
+
+def find_speech_for_triple_num(num: int, first: int, second: int, third: int):
+    robot_speech = FIRST_TEN[first - 1] + " " + HUNDRED
+
+    if num % 100 == 0:
+        return robot_speech
+    elif second == 0:
+        robot_speech += " " + FIRST_TEN[third - 1]
+    else:
+        robot_speech += " " + find_speech_for_double_num(num % 100, second, third)
+
+    return robot_speech
