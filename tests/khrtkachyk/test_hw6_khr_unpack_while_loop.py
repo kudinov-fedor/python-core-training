d7a34a8a1a04ab8fcbcda392bedfa1ee26c134d6
Khrystyna Tkachyk <tkachykhr@gmail.com>
2023-07-09 16:41:02 +0300

    another changes

--- tests/khrtkachyk/test_hw6_khr_unpack_while_loop.py
+++ tests/khrtkachyk/test_hw6_khr_unpack_while_loop.py
@@ -3,18 +3,14 @@ from typing import List, Any
 
 def unpack_while_loop(lst: List[Any]):
     initial_queue = [lst]
-    resulting = []
     while initial_queue:
         dequeue = initial_queue.pop(0)
         if isinstance(dequeue, list):
-            for i in dequeue:
-                initial_queue.insert(0, i)
+            initial_queue = dequeue + initial_queue
         else:
-            resulting.insert(0, dequeue)
-    yield resulting
+            yield dequeue
 
 
 def test_unpack_while_loop():
     target = [123, ["234", None], [[1], [23], [[123], 123, ["sdf", True]]]]
-    for value in unpack_while_loop(lst=target):
-        assert value == [123, "234", None, 1, 23, 123, 123, "sdf", True]
+    assert list(unpack_while_loop(target)) == [123, "234", None, 1, 23, 123, 123, "sdf", True]
