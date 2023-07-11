2bdf26663e5b25ce934681672e36ccfe2d502c2b
Olha Popova <gurtovayaolenka@gmail.com>
2023-07-11 22:21:42 +0300

    4th homework. Collections

---
+++ olpopova/homework/homework04/collections.py
@@ -0,0 +1,33 @@
+
+def unpack_dict_to_list(collection):
+    return list(collection)
+
+
+def unpack_dict_to_tuple(collection):
+    return tuple(collection)
+
+
+# multiple assignment
+def multiple_assignment(collection):
+    a, b, c, d = collection
+    return "First will be {3}, then - {1}, then - {2} and in the end - {0}".format(d, b, c, a)
+
+
+def mult_assignment_tail(collection):
+    a, *tail = collection
+    return a, tail
+
+
+def mult_assignment_head(collection):
+    *head, d = collection
+    return head, d
+
+
+def sort(data, sort_params, reverse):
+    kwargs = {'key': sort_params,
+              'reverse': reverse}
+    return sorted(data, **kwargs)
+
+
+def filter_data(collection, filter_name):
+    return sorted(list(map(lambda i: i[filter_name], collection)))
---
+++ tests/olpopova/homework/homework04/test_collections.py
@@ -0,0 +1,70 @@
+from olpopova.homework.homework04.collections import *
+import pytest
+
+
+data = [{"age": 16, "name": "John", "sex": "M"},
+        {"age": 34, "name": "Marry", "sex": "F"},
+        {"age": 25, "name": "Mathew", "sex": "M"}]
+
+
+@pytest.mark.parametrize(['data', 'sort_params', 'reverse', 'expected'], [
+    (data, lambda i: i["age"], False, [{"age": 16, "name": "John", "sex": "M"},
+                                       {"age": 25, "name": "Mathew", "sex": "M"},
+                                       {"age": 34, "name": "Marry", "sex": "F"}]),
+    (data, lambda i: i["age"], True, [{"age": 34, "name": "Marry", "sex": "F"},
+                                      {"age": 25, "name": "Mathew", "sex": "M"},
+                                      {"age": 16, "name": "John", "sex": "M"}]),
+    (data, lambda i: i["sex"] == "M", False, [{"age": 34, "name": "Marry", "sex": "F"},
+                                              {"age": 16, "name": "John", "sex": "M"},
+                                              {"age": 25, "name": "Mathew", "sex": "M"}]),
+    (data, lambda i: i["sex"] == "M", True, [{"age": 16, "name": "John", "sex": "M"},
+                                             {"age": 25, "name": "Mathew", "sex": "M"},
+                                             {"age": 34, "name": "Marry", "sex": "F"}]),
+    (data, lambda i: (i["sex"] == "M", i["age"]), False, [{"age": 34, "name": "Marry", "sex": "F"},
+                                                          {"age": 16, "name": "John", "sex": "M"},
+                                                          {"age": 25, "name": "Mathew", "sex": "M"}]),
+    (data, lambda i: (i["sex"] == "M", -i["age"]), False, [{"age": 34, "name": "Marry", "sex": "F"},
+                                                           {"age": 25, "name": "Mathew", "sex": "M"},
+                                                           {"age": 16, "name": "John", "sex": "M"}]),
+    (data, lambda i: i["name"], False, [{"age": 16, "name": "John", "sex": "M"},
+                                        {"age": 34, "name": "Marry", "sex": "F"},
+                                        {"age": 25, "name": "Mathew", "sex": "M"}])
+])
+def test_sort(data, sort_params, reverse, expected):
+    assert sort(data, sort_params, reverse) == expected
+
+
+@pytest.mark.parametrize(['data', 'filter_name', 'expected'], [
+    (data, "name", ["John", "Marry", "Mathew"]),
+    (data, "age", [16, 25, 34])
+])
+def test_filter(data, filter_name, expected):
+    assert filter_data(data, filter_name) == expected
+
+
+@pytest.mark.parametrize(['collection', 'expected'], [
+    ({'key1': 'param', 'key2': 123, 'key3': None}, ['key1', 'key2', 'key3']),
+    ({(123, 456): 'kjdgg', (123, 654): 'kjdjd', ('jdjf', 'rrrr'): 1235}, [(123, 456), (123, 654), ('jdjf', 'rrrr')])
+])
+def test_unpack_dict(collection, expected):
+    assert unpack_dict_to_list(collection) == expected
+
+
+@pytest.mark.parametrize(['collection', 'expected'], [
+    ({'key1': 'param', 'key2': 123, 'key3': None}, ('key1', 'key2', 'key3')),
+    ({(123, 456): 'kjdgg', (123, 654): 'kjdjd', ('jdjf', 'rrrr'): 1235}, ((123, 456), (123, 654), ('jdjf', 'rrrr')))
+])
+def test_unpack_dict(collection, expected):
+    assert unpack_dict_to_tuple(collection) == expected
+
+
+def test_mult_assignment(collection="flgo"):
+    assert multiple_assignment(collection) == "First will be f, then - l, then - g and in the end - o"
+
+
+def test_mult_assignment_tail(collection="flgo"):
+    assert mult_assignment_tail(collection) == ('f', ['l', 'g', 'o'])
+
+
+def test_mult_assignment_tail(collection="flgo"):
+    assert mult_assignment_head(collection) == (['f', 'l', 'g'], 'o')
