c73dc2c7c538d5382df4daf487d346298f6cdcf4
Khrystyna Tkachyk <tkachykhr@gmail.com>
2023-07-12 12:13:59 +0300

    updated

--- khrtkachyk/khr_hw7_global_local_scope.py
+++ khrtkachyk/khr_hw7_global_local_scope.py
@@ -1,18 +1,18 @@
 def create_global_variable():
     global lst
     lst = [1, 2, 3, 4, 5]
-    return sum(lst)
 
 
 def use_global_variable():
-    global lst
     lst.append(6)
+
+
+def return_sum():
     return sum(lst)
 
 
 def create_global_variable_2_0(key, value):
     globals()[key] = value
-    return key, value
 
 
 def use_global_variable_2_0(key):
@@ -21,8 +21,10 @@ def use_global_variable_2_0(key):
 
 if __name__ == "__main__":
     create_global_variable()
-    print(f'Created global variable: {lst}; Sum of list items = {create_global_variable()}')
+    print(f'Created global variable: {lst}')
+    print(f'Sum of list items = {return_sum()}')
     use_global_variable()
-    print(f'Modified global variable: {lst}; Sum of list items when reusing global lst = {use_global_variable()}')
-    print(f'Key-value pair: {create_global_variable_2_0("age", 30)}')
+    print(f'Modified global variable: {lst}')
+    print(f'Sum of list items = {return_sum()}')
+    create_global_variable_2_0("age", 30)
     print(f'Getting value from created global variable: {use_global_variable_2_0("age")}')
