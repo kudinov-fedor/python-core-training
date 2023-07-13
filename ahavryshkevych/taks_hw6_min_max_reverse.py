c7a64dd72e6ae97f187949fc79bc5c873b44a147
Andriy Havryshkevych <gavandre@gmail.com>
2023-07-13 13:04:41 +0300

    updated with latest comments

--- ahavryshkevych/taks_hw6_min_max_reverse.py
+++ ahavryshkevych/taks_hw6_min_max_reverse.py
@@ -2,11 +2,8 @@ def own_max(*args, key=None) -> int:
     result = args[0]
     key = key or (lambda i: i)
     for num in args:
-        if key is not None:
-            if key(num) > key(result):
+        if key(num) > key(result):
                 result = num
-        elif num > result:
-            result = num
     return result
 
 
@@ -14,11 +11,8 @@ def own_min(*args, key=None) -> int:
     result = args[0]
     key = key or (lambda i: i)
     for num in args:
-        if key is not None:
-            if key(num) < key(result):
+        if key(num) < key(result):
                 result = num
-        elif num < result:
-            result = num
     return result
 
 
@@ -26,22 +20,11 @@ def own_sort(*args, reverse=False, key=None) -> list:
     my_list = list(args)
     sorted_list = []
     rounds = len(args)
-
-    if key is not None:
-        while len(sorted_list) < rounds:
-            if reverse:
-                x = own_max(*my_list, key=key)
-            else:
-                x = own_min(*my_list, key=key)
-            sorted_list.append(x)
-            my_list.remove(x)
-    else:
-        while len(sorted_list) < rounds:
-            if reverse:
-                x = own_max(*my_list)
-            else:
-                x = own_min(*my_list)
-            sorted_list.append(x)
-            my_list.remove(x)
-
+    while len(sorted_list) < rounds:
+        if reverse:
+            x = own_max(*my_list, key=key)
+        else:
+            x = own_min(*my_list, key=key)
+        sorted_list.append(x)
+        my_list.remove(x)
     return sorted_list
