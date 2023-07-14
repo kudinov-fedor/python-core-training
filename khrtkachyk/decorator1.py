d90e0f60e64eaafc6c1d5f45009c463994c260bd
Khrystyna Tkachyk <tkachykhr@gmail.com>
2023-07-14 15:33:34 +0300

    updated according to comments

--- khrtkachyk/decorator1.py
+++ khrtkachyk/decorator1.py
@@ -1,3 +1,4 @@
+import pytest
 """
 Create decorator which would remember answers,
 and if answer was given, do not make a call to a function
@@ -5,13 +6,13 @@ and if answer was given, do not make a call to a function
 
 
 def my_decor(original_func):
-    stored_values = {}
+    cached = {}
 
-    def wrapper(*values):
-        if values in stored_values:
-            print("Do not make call to original function")
-        elif values not in stored_values:
-            res = original_func(*values)
+    def wrapper(n: int):
+        if n in cached:
+            return original_func
+        elif n not in cached:
+            res = original_func(n)
             return res
     return wrapper
 
@@ -27,6 +28,19 @@ def fibo(n: int):
     return fibo(n - 1) + fibo(n - 2)
 
 
-if __name__ == "__main__":
-    for i in range(10):
-        print(fibo(i))
+def test_decorator(given_range, res):
+    ints = []
+    for i in range(given_range):
+        ints.append(fibo(i))
+    assert ints == res
+
+
+parametrized_decor = pytest.mark.parametrize("given_range, res", [
+    (10, [0, 1, 2, 3, 5, 8, 13, 21, 34, 55]),
+    (1, [0]),
+    (3, [0, 1, 2]),
+    (12, [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]),
+    (0, [])
+])
+
+test_decorator = parametrized_decor(test_decorator)
