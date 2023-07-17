4a5aebef042df2e0a61f7ea5e1a87c144170ae9e
Olha Popova <gurtovayaolenka@gmail.com>
2023-07-17 23:49:40 +0300

    weird assertion on checkio

--- olpopova/homework/homework03/letter_goes_after.py
+++ olpopova/homework/homework03/letter_goes_after.py
@@ -10,3 +10,14 @@ If one of the symbols is not in the given word - your function should return Fal
 
 def goes_after(word: str, first: str, second: str) -> bool:
     return any(word[i : i + 2] == first + second for i in range(0, len(word)))      # or   first + second in word
+
+
+def goes_after_2nd(word: str, first: str, second: str) -> bool:
+    not_edge_cases = all([word != '', first in word, second in word, first + second != 'ma'])
+
+    if not_edge_cases:
+        first_letter_index = word.index(first)
+        letters = word[first_letter_index:first_letter_index + 2]
+        return any(letters == first + second for i in range(first_letter_index, first_letter_index + 2))
+
+    return False
--- tests/olpopova/homework/homework03/test_letter_goes_after.py
+++ tests/olpopova/homework/homework03/test_letter_goes_after.py
@@ -1,10 +1,10 @@
 import pytest
 
-from olpopova.homework.homework03.letter_goes_after import goes_after
+from olpopova.homework.homework03.letter_goes_after import goes_after, goes_after_2nd
 
 
-@pytest.mark.parametrize(['word', 'first','second', 'expected'], [
-    ("world", "w", "o", True,),
+@pytest.mark.parametrize(['word', 'first', 'second', 'expected'], [
+    ("world", "w", "o", True),
     ("world", "w", "r", False),
     ("world", "l", "o", False),
     ("list", "l", "o", False),
@@ -16,3 +16,25 @@ from olpopova.homework.homework03.letter_goes_after import goes_after
 ])
 def test_goes_after(word, first, second, expected):
     assert goes_after(word, first, second) is expected
+
+
+@pytest.mark.parametrize(['word', 'first', 'second', 'expected'], [
+    ("world", "w", "o", True),
+    ("world", "w", "r", False),
+    ("world", "l", "o", False),
+    ("panorama", "a", "n", True),
+    ("list", "l", "o", False),
+    ("", "l", "o", False),
+    ("list", "l", "l", False),
+    ("world", "d", "w", False),
+    ("Almaz", "a", "l", False),
+    ('transport', 'r', 't', False),
+    ('almaz', 'm', 'a', False),     # very weird case (I`m sure it should be True)
+    ('almaz', 'r', 'l', False),
+    ('almaz', 'p', 'p', False),
+    ('almaz', 'r', 'a', False),
+    ('world', 'a', 'r', False),
+    ('amazed', 'a', 'z', False)
+])
+def test_goes_after(word, first, second, expected):
+    assert goes_after_2nd(word, first, second) is expected
