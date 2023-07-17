732ebd31a7c5c74859e18eea754abc68c82e2ff6
Olha Popova <gurtovayaolenka@gmail.com>
2023-07-17 21:59:30 +0300

    fixed PR comments

--- olpopova/homework/homework05/dicts.py
+++ olpopova/homework/homework05/dicts.py
@@ -1,9 +1,20 @@
 from collections import defaultdict
+from typing import List, Dict
 
 
-def filter_by_property(data):
-    by_sex = defaultdict(list)  # if key is not present, it will be created with value = list()
+def group_by(data: List[dict], key: str) -> Dict[str, list]:
+    """
+    group items from list into dict
+    where key is a value from parameter provided as key parameter
+    Usage example:
+    data = [{"a": "1", "b": 1}, {"a": "1", "b": 2}, {"a": "2", "b":3}]
+    assert group_by(data, "a") == {
+        "1": [{"a": "1", "b": 1}, {"a": "1", "b": 2}],
+        "2": [{"a": "2", "b":3}]
+    }
+    """
+    by_key = defaultdict(list)
     for i in data:
-        by_sex[i["sex"]].append(i)
+        by_key[i[key]].append(i)
 
-    return len(by_sex["M"]), len(by_sex["F"])
+    return by_key
--- tests/olpopova/homework/homework05/test_dicts.py
+++ tests/olpopova/homework/homework05/test_dicts.py
@@ -1,9 +1,17 @@
-from olpopova.homework.homework05.dicts import filter_by_property
+from olpopova.homework.homework05.dicts import group_by
 
 
-def test_filter_by_property():
+def test_group_by():
     data = [{"age": 16, "second_name": "Parris", "name": "John", "sex": "M"},
             {"age": 34, "second_name": "Atkinson", "name": "Marry", "sex": "F"},
             {"age": 15, "second_name": "Katon", "name": "Marry", "sex": "F"},
-            {"age": 25, "second_name": "Maton", "name": "Mathew", "sex": "M"}]
-    assert filter_by_property(data) == (2, 2)
+            {"age": 25, "second_name": "Maton", "name": "Mathew", "sex": "M"},
+            {"age": 78, "second_name": "Smith", "name": "John", "sex": "M"}
+            ]
+    expected = {"Marry": [{"age": 34, "second_name": "Atkinson", "name": "Marry", "sex": "F"},
+                          {"age": 15, "second_name": "Katon", "name": "Marry", "sex": "F"}],
+                "John": [{"age": 16, "second_name": "Parris", "name": "John", "sex": "M"},
+                         {"age": 78, "second_name": "Smith", "name": "John", "sex": "M"}],
+                "Mathew": [{"age": 25, "second_name": "Maton", "name": "Mathew", "sex": "M"}]
+                }
+    assert group_by(data, 'name') == expected
