a766d74c80f6dc05652d8985ce2572cb36eb17c2
Olha Popova <gurtovayaolenka@gmail.com>
2023-07-18 11:05:39 +0300

    fixed PR comments

--- olpopova/homework/homework03/letter_goes_after.py
+++ olpopova/homework/homework03/letter_goes_after.py
@@ -13,11 +13,4 @@ def goes_after(word: str, first: str, second: str) -> bool:
 
 
 def goes_after_2nd(word: str, first: str, second: str) -> bool:
-    not_edge_cases = all([word != '', first in word, second in word, first + second != 'ma'])
-
-    if not_edge_cases:
-        first_letter_index = word.index(first)
-        letters = word[first_letter_index:first_letter_index + 2]
-        return any(letters == first + second for i in range(first_letter_index, first_letter_index + 2))
-
-    return False
+    return first in word and word.find(second) - word.find(first) == 1
