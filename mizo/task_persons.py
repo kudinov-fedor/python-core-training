21034eb17d2eedf6b5ebee149f5249955a4e5e0e
Mariia Izio <marichka1360@gmail.com>
2023-07-19 17:38:02 +0300

    resolved comments

--- mizo/task_persons.py
+++ mizo/task_persons.py
@@ -2,7 +2,8 @@ from datetime import datetime
 
 
 class Person:
-    def __init__(self, first_name, last_name, date_of_birth, job, working_years, salary, country, city, gender='unknown'):
+    def __init__(self, first_name, last_name, date_of_birth, job, working_years, salary, country, city,
+                 gender='unknown'):
         self.first_name = first_name
         self.last_name = last_name
         self.date_of_birth = date_of_birth
@@ -13,25 +14,17 @@ class Person:
         self.city = city
         self.gender = gender
 
-        # Additional attributes
-        self.full_name = self.name()
-        self.age_person1 = self.age()
-        self.age_person2 = self.age()
-        self.home_location_person1 = self.home()
-        self.home_location_person2 = self.home()
-        self.money_formatted = "{:,}".format(self.money())
-
     def name(self):
-        return f"{self.first_name} {self.last_name}"
+        return self.first_name + " " + self.last_name
 
-    def work_person1(self):
+    def work(self):
         return self.job
 
     def money(self):
         return self.salary * self.working_years
 
     def home(self):
-        return self.city, self.country
+        return f"{self.city}, {self.country}"
 
     def age(self):
         today = datetime(2018, 1, 1)
@@ -39,33 +32,3 @@ class Person:
         if (today.month, today.day) < (self.date_of_birth.month, self.date_of_birth.day):
             age -= 1
         return age
-
-
-# Create instances of the Person class
-person1_date_of_birth = datetime(1979, 9, 19)
-person2_date_of_birth = datetime(1995, 12, 5)
-person1 = Person('John', 'Smith', person1_date_of_birth, 'welder', 15, 3600, 'Canada', 'Vancouver', 'male')
-person2 = Person('Hanna Rose', 'May', person2_date_of_birth, 'designer', 2.2, 2150, 'Austria', 'Vienna', 'unknown')
-
-# Access additional attributes
-print(
-    f"Person('{person1.first_name}', '{person1.date_of_birth}', '{person1.job}', {person1.working_years}, {person1.salary}, '{person1.country}', '{person1.city}', '{person1.gender}') == {person1.full_name}")
-print(
-    f"Person('{person1.first_name}', '{person1.date_of_birth}', '{person1.job}', {person1.working_years}, {person1.salary}, '{person1.country}', '{person1.city}', '{person1.gender}') == {person1.age_person1}")
-print(
-    f"Person('{person1.first_name}', '{person1.date_of_birth}', '{person1.job}', {person1.working_years}, {person1.salary}, '{person1.country}', '{person1.city}', '{person1.gender}') == is a {person1.job}")
-print(
-    f"Person('{person1.first_name}', '{person1.date_of_birth}', '{person1.job}', {person1.working_years}, {person1.salary}, '{person1.country}', '{person1.city}', '{person1.gender}') == {person1.money_formatted}")
-print(
-    f"Person('{person1.first_name}', '{person1.date_of_birth}', '{person1.job}', {person1.working_years}, {person1.salary}, '{person1.country}', '{person1.city}', '{person1.gender}') == lives in {person1.home_location_person1}")
-
-print(
-    f"Person('{person2.first_name}', '{person2.date_of_birth}', '{person2.job}', {person2.working_years}, {person2.salary}, '{person2.country}', '{person2.city}' == {person2.full_name}")
-print(
-    f"Person('{person2.first_name}', '{person2.date_of_birth}', '{person2.job}', {person2.working_years}, {person2.salary}, '{person2.country}', '{person2.city}' == {person2.age_person2}")
-print(
-    f"Person('{person2.first_name}', '{person2.date_of_birth}', '{person2.job}', {person2.working_years}, {person2.salary}, '{person2.country}', '{person2.city}' == is a {person2.job}")
-print(
-    f"Person('{person2.first_name}', '{person2.date_of_birth}', '{person2.job}', {person2.working_years}, {person2.salary}, '{person2.country}', '{person2.city}' == {person2.money_formatted}")
-print(
-    f"Person('{person2.first_name}', '{person2.date_of_birth}', '{person2.job}', {person2.working_years}, {person2.salary}, '{person2.country}', '{person2.city}' == lives in {person2.home_location_person2}")
--- tests/mizo/HomeWork8/test_persons.py
+++ tests/mizo/HomeWork8/test_persons.py
@@ -1,50 +1,24 @@
-from datetime import datetime
 from mizo.task_persons import Person
+from datetime import datetime
+
+person1_date_of_birth = datetime(1979, 9, 19)
+person2_date_of_birth = datetime(1995, 12, 5)
+
+p1 = Person("John", "Smith", person1_date_of_birth, "welder", 15, 3600, "Canada", "Vancouver", "male")
+p2 = Person("Hanna Rose", "May", person2_date_of_birth, "designer", 2.2, 2150, "Austria", "Vienna")
 
 
-def test_person1_name():
-    person1_date_of_birth = datetime(1979, 9, 19)
-    person1 = Person('John', 'Smith', person1_date_of_birth, 'welder', 15, 3600, 'Canada', 'Vancouver', 'male')
-    assert person1.name() == 'John Smith'
+def test_attr_person1():
+    assert p1.name() == "John Smith", "Name"
+    assert p1.age() == 38, "Age"
+    assert p1.work() == "welder", "Job"
+    assert p1.money() == 54000, "Money"
+    assert p1.home() == "Vancouver, Canada", "Home"
 
 
-def test_person2_name():
-    person2_date_of_birth = datetime(1995, 12, 5)
-    person2 = Person('Hanna Rose', 'May', person2_date_of_birth, 'designer', 2.2, 2150, 'Austria', 'Vienna', 'unknown')
-    assert person2.name() == 'Hanna Rose May'
-
-
-def test_person1_job():
-    person1_date_of_birth = datetime(1979, 9, 19)
-    person1 = Person('John', 'Smith', person1_date_of_birth, 'welder', 15, 3600, 'Canada', 'Vancouver', 'male')
-    assert person1.job() == 'welder'
-
-
-def test_person2_job():
-    person2_date_of_birth = datetime(1995, 12, 5)
-    person2 = Person('Hanna Rose', 'May', person2_date_of_birth, 'designer', 2.2, 2150, 'Austria', 'Vienna', 'unknown')
-    assert person2.job() == 'designer'
-
-
-def test_person1_age():
-    person1_date_of_birth = datetime(1979, 9, 19)
-    person1 = Person('John', 'Smith', person1_date_of_birth, 'welder', 15, 3600, 'Canada', 'Vancouver', 'male')
-    assert person1.age() == 38
-
-
-def test_person2_age():
-    person2_date_of_birth = datetime(1995, 12, 5)
-    person2 = Person('Hanna Rose', 'May', person2_date_of_birth, 'designer', 2.2, 2150, 'Austria', 'Vienna', 'unknown')
-    assert person2.age() == 22
-
-
-def test_person1_money():
-    person1_date_of_birth = datetime(1979, 9, 19)
-    person1 = Person('John', 'Smith', person1_date_of_birth, 'welder', 15, 3600, 'Canada', 'Vancouver', 'male')
-    assert person1.money() == 54000
-
-
-def test_person2_money():
-    person2_date_of_birth = datetime(1995, 12, 5)
-    person2 = Person('Hanna Rose', 'May', person2_date_of_birth, 'designer', 2.2, 2150, 'Austria', 'Vienna', 'unknown')
-    assert person2.money() == 4730
+def test_attr_person2():
+    assert p2.name() == "Hanna Rose May", "Name"
+    assert p2.age() == 22, "Age"
+    assert p2.work() == "designer", "Job"
+    assert p2.money() == 4730, "Money"
+    assert p2.home() == "Vienna, Austria", "Home"
