cc649db4ebc5822747f700374d17cafc3a0fb393
fkudinov <fkudinov@modeln.com>
2023-07-20 10:18:42 +0300

    add tests

--- tests/olpopova/homework/homework06/test_min_max_sorted.py
+++ tests/olpopova/homework/homework06/test_min_max_sorted.py
@@ -1,4 +1,6 @@
-from olpopova.homework.homework06.min_max_sorted import min, max, sorted
+import pytest
+
+from olpopova.homework.homework06.min_max_sorted import min, max, sorted, identify_insert_index
 
 
 def test_min():
@@ -18,3 +20,18 @@ def test_sorted():
     assert sorted(-7, -4, -2, 1, 2, 3, 4, 5, 6, reverse=True) == [6, 5, 4, 3, 2, 1, -2, -4, -7]
     assert sorted(-7, -4, -2, 1, 2, 3, 4, 5, 6, key=abs) == [1, -2, 2, 3, -4, 4, 5, 6, -7]
     assert sorted(-7, -4, -2, 1, 2, 3, 4, 5, 6, key=abs, reverse=True) == [-7, 6, 5, 4, -4, 3, 2, -2, 1]
+
+
+@pytest.mark.parametrize("data, item, res", [
+    ([], 5, 0),
+    ([1], 5, 1),
+    ([6], 5, 0),
+    ([3, 4], 1, 0),
+    ([3, 6], 5, 1),
+    ([3, 4], 5, 2),
+])
+@pytest.mark.parametrize("key", [
+    (lambda i: i), None,
+])
+def test_inserts(data, item, res, key):
+    assert identify_insert_index(data, item, key=key) == res
