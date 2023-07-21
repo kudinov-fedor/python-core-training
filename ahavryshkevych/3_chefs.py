1141b5d952116b9f8648a1d4e59a4bebb43385be
Andriy Havryshkevych <gavandre@gmail.com>
2023-07-21 11:50:57 +0300

    updated

--- ahavryshkevych/3_chefs.py
+++ ahavryshkevych/3_chefs.py
@@ -1,42 +1,33 @@
 class AbstractCook:
-    def __init__(self, food_name, drink_name):
+    def __init__(self):
         self.food = 0
         self.drink = 0
-        self.food_name = food_name
-        self.drink_name = drink_name
 
     def add_food(self, food_amount, food_price):
         total_cost = food_amount * food_price
         self.food += total_cost
 
-
     def add_drink(self, drink_amount, drink_price):
         total_cost = drink_amount * drink_price
         self.drink += total_cost
-
-
 
     def total(self):
         return f"{self.food_name}: {self.food}, {self.drink_name}: {self.drink}, Total: {self.food + self.drink}"
 
 
-
-
 class JapaneseCook(AbstractCook):
-    def __init__(self):
-        super().__init__(food_name="Sushi", drink_name="Tea")
-
+    food_name = "Sushi"
+    drink_name = "Tea"
 
 
 class UkrainianCook(AbstractCook):
-    def __init__(self):
-        super().__init__(food_name="Dumplings", drink_name="Compote")
+    food_name = "Dumplings"
+    drink_name = "Compote"
 
 
 class ItalianCook(AbstractCook):
-    def __init__(self):
-        super().__init__(food_name="Pizza", drink_name="Juice")
-
+    food_name = "Pizza"
+    drink_name = "Juice"
 
 
 if __name__ == '__main__':
@@ -54,7 +45,6 @@ if __name__ == '__main__':
     client_3.add_food(2, 20)
     client_3.add_food(2, 30)
     client_3.add_drink(2, 10)
-
 
     assert client_1.total() == "Sushi: 105, Tea: 20, Total: 125", client_1.total()
     assert client_2.total() == "Dumplings: 90, Compote: 100, Total: 190"
