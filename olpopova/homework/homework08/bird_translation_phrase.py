13482c276e00c3ecd1bc601b1114451f9405392d
Olha.Popova <gurtovayaolenka@gmail.com>
2023-07-22 22:26:30 +0300

    implementation of translate functions for checkio task

---
+++ olpopova/homework/homework08/bird_translation_phrase.py
@@ -0,0 +1,59 @@
+import re
+
+
+def translate(text: str) -> str:
+    vowels = ['a', 'o', 'e', 'i', 'u', 'y']
+    translated_phrase = []
+
+    for word in text.split():
+        translated_phrase.append(word)
+
+        for index in range(0, len(word)):
+            if index != len(word):
+                triple_vowels = word[index + 1: index + 3] == word[index] * 2
+
+                if word[index] in vowels and triple_vowels:
+                    word = translated_phrase.pop().replace(word[index], '', 2)
+
+                elif word[index] not in vowels and word[index + 1] in vowels:
+                    chars_list = list(translated_phrase.pop())
+                    del chars_list[index + 1]
+                    word = ''.join(chars_list)
+
+                translated_phrase.append(word)
+            else:
+                break
+    return ' '.join(translated_phrase)
+
+
+def translate_with_table(text: str) -> str:
+    vowels = ['a', 'o', 'e', 'i', 'u', 'y']
+    translated_phrase = []
+    translation_table = {vowels[0] * 3: vowels[0],
+                         vowels[1] * 3: vowels[1],
+                         vowels[2] * 3: vowels[2],
+                         vowels[3] * 3: vowels[3],
+                         vowels[4] * 3: vowels[4],
+                         vowels[5] * 3: vowels[5],
+                         }
+    translated_with_table = custom_make_translation(text, translation_table)
+
+    for word in translated_with_table.split():
+        translated_phrase.append(word)
+
+        for index in range(0, len(word)):
+            if index != len(word):
+                if word[index] not in vowels and word[index + 1] in vowels:
+                    chars_list = list(translated_phrase.pop())
+                    del chars_list[index + 1]
+                    word = ''.join(chars_list)
+                    translated_phrase.append(word)
+            else:
+                break
+
+    return ' '.join(translated_phrase)
+
+
+def custom_make_translation(text, translation):
+    regex = re.compile('|'.join(map(re.escape, translation)))
+    return regex.sub(lambda match: translation[match.group(0)], text)
