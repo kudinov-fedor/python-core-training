26121d1af8eaef6946bc5502ab654878c6017b8b
Olha.Popova <gurtovayaolenka@gmail.com>
2023-07-26 17:23:47 +0300

    fix for PR comments

--- olpopova/homework/homework08/person.py
+++ olpopova/homework/homework08/person.py
@@ -6,7 +6,7 @@ class Person:
     def __init__(self, first_name, last_name, birth_date, job, working_years, salary, country, city, gender='unknown'):
         self.fn = first_name
         self.ln = last_name
-        self.birth = birth_date
+        self.birth = datetime.strptime(birth_date, f'%d.%m.%Y')
         self.job = job
         self.work_years = working_years
         self.salary = salary
@@ -18,15 +18,14 @@ class Person:
         return ' '.join([self.fn, self.ln])
 
     def age(self):
-        separator = self.birth[2]
-        return date.today().year - datetime.strptime(self.birth, f'%d{separator}%m{separator}%Y').year
+        return date.today().year - self.birth.year
 
     def work(self):
-        return f'{self.fn} is a {self.job}'
+        return f'Is a {self.job}'
 
     def money(self):
-        return ' '.join([f'Earned in {self.work_years} years -',
-                         '{0:,}'.format(self.salary * 12 * self.work_years).replace(',', ' ')])
+        money = '{0:,}'.format(self.salary * 12 * self.work_years).replace(',', ' ')
+        return money
 
     def home(self):
         return f'Lives in {self.city}, {self.country}'
--- tests/olpopova/homework/homework08/test_person.py
+++ tests/olpopova/homework/homework08/test_person.py
@@ -20,7 +20,7 @@ def john():
 def perceval():
     return {'first_name': 'Perceval',
             'last_name': 'Leclerk',
-            'birth_date': '6/1/1997',
+            'birth_date': '6.1.1997',
             'job': 'F1 Driver',
             'working_years': 10,
             'salary': 1500000,
@@ -34,12 +34,6 @@ def test_person(john, perceval):
     p2 = Person(**perceval)
     assert p1.name() == 'John Smith', 'Name'
     assert p1.age() == 44, 'Age'
-    assert p2.work() == 'Perceval is a F1 Driver', 'Job'
-    assert p1.money() == 'Earned in 15 years - 648 000', 'Money'
+    assert p2.work() == 'Is a F1 Driver', 'Job'
+    assert p1.money() == '648 000', 'Money'
     assert p2.home() == 'Lives in Monte-Carlo, Monaco'
-
-
-def test_person2(perceval):
-    p = Person(**perceval)
-    assert '.\n'.join([p.name(), p.work(), p.money()]) == \
-           '.\n'.join(['Perceval Leclerk', 'Perceval is a F1 Driver', 'Earned in 10 years - 180 000 000'])
