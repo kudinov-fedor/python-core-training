aa056d5218b0d99bfc0509df553a02b2ed7540e0
Olha.Popova <gurtovayaolenka@gmail.com>
2023-07-26 22:02:13 +0300

    fix for PR comments

--- tests/olpopova/homework/homework08/test_warrior.py
+++ tests/olpopova/homework/homework08/test_warrior.py
@@ -1,18 +1,15 @@
+import pytest
+
 from olpopova.homework.homework08.warrior import Warrior, Knight, fight
 
 
-def test_fight():
-    chuck = Warrior()
-    bruce = Warrior()
-    carl = Knight()
-    dave = Warrior()
-    mark = Warrior()
-
-    assert fight(chuck, bruce) == True
-    assert fight(dave, carl) == False
-    assert chuck.is_alive == True
-    assert bruce.is_alive == False
-    assert carl.is_alive == True
-    assert dave.is_alive == False
-    assert fight(carl, mark) == False
-    assert carl.is_alive == False
+@pytest.mark.parametrize(['fighter1', 'fighter2', 'expected'],[
+    (Warrior(), Warrior(), True),
+    (Warrior(), Knight(), False)
+])
+def test_fight(fighter1, fighter2, expected):
+    chuck = fighter1
+    bruce = fighter2
+    assert fight(chuck, bruce) is expected
+    assert chuck.is_alive is expected
+    assert bruce.is_alive is not expected
