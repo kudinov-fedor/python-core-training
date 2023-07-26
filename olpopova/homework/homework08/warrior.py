fe8ed477fbcbb6b9b411e7b9cd457ff4616ad885
Olha.Popova <gurtovayaolenka@gmail.com>
2023-07-26 21:56:13 +0300

    added deal_damage

--- olpopova/homework/homework08/warrior.py
+++ olpopova/homework/homework08/warrior.py
@@ -7,6 +7,12 @@ class Warrior:
     def is_alive(self):
         return self.health > 0
 
+    def deal_dmg(self, damaged_unit):
+        """
+        E.g. warrior1.deal_dmg(knight) == 'warrior attack knight'
+        """
+        return damaged_unit.health - self.attack
+
 
 class Knight(Warrior):
     attack = 7
@@ -14,8 +20,8 @@ class Knight(Warrior):
 
 def fight(unit_1, unit_2):
     while unit_1.is_alive:
-        unit_2.health = unit_2.health - unit_1.attack
+        unit_2.health = unit_1.deal_dmg(unit_2)
         if not unit_2.is_alive:
             break
-        unit_1.health = unit_1.health - unit_2.attack
+        unit_1.health = unit_2.deal_dmg(unit_1)
     return unit_1.is_alive
