6b2598867668a79c1a755b036bbc2c2d48a4cbec
Olha.Popova <gurtovayaolenka@gmail.com>
2023-07-26 22:35:22 +0300

    fix for PR comments

--- olpopova/homework/homework08/chiefs.py
+++ olpopova/homework/homework08/chiefs.py
@@ -3,30 +3,32 @@ class AbstractCook:
 
     food = 0
     drinks = 0
+    food_name = ''
+    drink_name = ''
 
     def add_food(self, food, price):
-        self.__setattr__('food', self.food + food * price)
+        self.food += food * price
 
     def add_drink(self, drinks, price):
-        self.__setattr__('drinks', self.drinks + drinks * price)
+        self.drinks += drinks * price
 
     def total_sum(self):
         return self.food + self.drinks
 
     def total(self):
-        pass
+        return f'{self.food_name}: {self.food}, {self.drink_name}: {self.drinks}, Total: {self.total_sum()}'
 
 
 class JapaneseCook(AbstractCook):
-    def total(self):
-        return f'Sushi: {self.food}, Tea: {self.drinks}, Total: {self.total_sum()}'
+    food_name = 'Sushi'
+    drink_name = 'Tea'
 
 
 class UkrainianCook(AbstractCook):
-    def total(self):
-        return f'Borsch: {self.food}, Uzvar: {self.drinks}, Total: {self.total_sum()}'
+    food_name = 'Borsch'
+    drink_name = 'Uzvar'
 
 
 class ItalianCook(AbstractCook):
-    def total(self):
-        return f'Spaghetti: {self.food}, Wine: {self.drinks}, Total: {self.total_sum()}'
+    food_name = 'Pizza'
+    drink_name = 'Juice'
--- tests/olpopova/homework/homework08/test_chiefs.py
+++ tests/olpopova/homework/homework08/test_chiefs.py
@@ -4,12 +4,12 @@ from olpopova.homework.homework08.chiefs import JapaneseCook, UkrainianCook, Ita
 
 
 @pytest.mark.parametrize(['cuisine', 'first_order', 'second_order', 'drinks', 'expected'], [
-    (JapaneseCook(), (2, 30), (3, 15), (2, 10), 'Sushi: 105, Tea: 20, Total: 125'),
-    (UkrainianCook(), (1, 40), (2, 25), (5, 20),"Borsch: 90, Uzvar: 100, Total: 190"),
-    (ItalianCook(), (2, 20), (2, 30), (2, 10), "Spaghetti: 100, Wine: 20, Total: 120")
+    (JapaneseCook, (2, 30), (3, 15), (2, 10), 'Sushi: 105, Tea: 20, Total: 125'),
+    (UkrainianCook, (1, 40), (2, 25), (5, 20), 'Borsch: 90, Uzvar: 100, Total: 190'),
+    (ItalianCook, (2, 20), (2, 30), (2, 10), 'Pizza: 100, Juice: 20, Total: 120')
 ])
 def test_receipt(cuisine, first_order, second_order, drinks, expected):
-    client = cuisine
+    client = cuisine()
     client.add_food(*first_order)
     client.add_food(*second_order)
     client.add_drink(*drinks)
