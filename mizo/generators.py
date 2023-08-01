51012abf6832b2d555ee5b4fee2152c25adf62ec
Mariia Izio <marichka1360@gmail.com>
2023-08-01 12:13:23 +0300

    retry generators

--- mizo/generators.py
+++ mizo/generators.py
@@ -2,20 +2,25 @@ a = [0, 1, 2]
 
 
 def simple_generator(iterable):
-    for item in iterable:
-        yield item
+    length = len(iterable)
+    index = 0
+    while index < length:
+        yield iterable[index]
+        index += 1
 
 
-def test_generators():
-
+def test_simple_generator():
     iterator = simple_generator(a)
     b = list(iterator)
     assert b == [0, 1, 2]
 
 
 def reversed_generator(iterable):
-    for item in reversed(iterable):
-        yield item
+    length = len(iterable)
+    index = length - 1
+    while index >= 0:
+        yield iterable[index]
+        index -= 1
 
 
 def test_reversed_generator():
@@ -25,8 +30,10 @@ def test_reversed_generator():
 
 
 def cycle_generator(iterable):
-    for item in iterable:
-        yield item
+    n = len(iterable)
+    while True:
+        for i in range(n):
+            yield iterable[i]
 
 
 def test_cycle_iterator():
@@ -34,16 +41,17 @@ def test_cycle_iterator():
     b = [next(iterator) for _ in range(14)]
     assert b == [0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1]
 
+
 def ping_pong_generator(iterable):
+    length = len(iterable)
     while True:
-        for item in iterable:
-            yield item
-        for item in reversed(iterable[1:-1]):
-            yield item
+        for i in range(length):
+            yield iterable[i]
+        for i in range(length - 2, 0, -1):
+            yield iterable[i]
+
 
 def test_ping_pong_generator():
     iterator = ping_pong_generator(a)
     b = [next(iterator) for _ in range(14)]
     assert b == [0, 1, 2, 1, 0, 1, 2, 1, 0, 1, 2, 1, 0, 1]
-
-
