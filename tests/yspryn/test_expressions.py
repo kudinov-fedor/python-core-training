d8dfc77aa02cd4dad26c1b18435fdb8210f06aa7
Yuliia Spryn <yuliaspryn@gmail.com>
2024-01-19 11:17:03 +0200

    homework 1: corrections according to comments

--- tests/yspryn/test_expressions.py
+++ tests/yspryn/test_expressions.py
@@ -7,14 +7,22 @@ def test_tuple_check():
     assert a1 != a2
     assert a1 == a3
 
-def test_create_check():
+
+def test_create_dict():
     dict_var1 = dict
     dict_var2 = {}
     dict_var3 = dict()
 
     assert dict_var1 != dict_var2
+    assert isinstance(dict_var2, dict_var1)
+    assert isinstance(dict_var2, dict)
+    assert dict_var1() == {}
+    assert dict_var1 == dict
+    assert id(dict_var1) == id(dict)
     assert dict_var2 == dict_var3
 
+
+def test_create_str():
     str_var1 = str
     str_var2 = str()
     str_var3 = ''
