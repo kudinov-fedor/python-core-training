f220e2cf73f1352868577173ef0c319d767407cc
Yuliia Spryn <yuliaspryn@gmail.com>
2024-01-23 12:46:24 +0200

    homework 2: additional code style fixes

--- tests/yspryn/test_comprehensions.py
+++ tests/yspryn/test_comprehensions.py
@@ -48,7 +48,7 @@ def test_empty_result_for_list_compr():
 
 
 def test_iteration_with_zero_elements_in_list_compr():
-    new_list = [x+1 for x in range(0)]
+    new_list = [x + 1 for x in range(0)]
     assert new_list == []
 
 
@@ -64,7 +64,7 @@ def test_convert_types_in__list_compr():
 
 
 def test_set_with_comprehensions():
-    new_set = {x**2 for x in range(2, 5)}
+    new_set = {x ** 2 for x in range(2, 5)}
     assert new_set == {16, 9, 4}
 
 
