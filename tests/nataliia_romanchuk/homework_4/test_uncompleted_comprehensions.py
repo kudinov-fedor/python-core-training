895e8c01fb247e0e6a60654f160f9df9d6d4967b
vivanatalya <vivanatalya@gmail.com>
2024-02-02 20:43:03 +0200

    [NATALIIA_ROMANCHUK/homework4] fix comments

--- tests/nataliia_romanchuk/homework_4/test_uncompleted_comprehensions.py
+++ tests/nataliia_romanchuk/homework_4/test_uncompleted_comprehensions.py
@@ -24,6 +24,8 @@ def test_comprehensions():
 
 
 """create generator object using generator expression and unpack items form it into list, set, dict, tuple"""
+
+
 def test_generator_expression_unpacking():
     generator1 = (i for i in some_list)
     list_result = list(generator1)
@@ -43,43 +45,22 @@ def test_generator_expression_unpacking():
 
 
 def test_generator_comprehensions():
-    generator_object, generator_object1, generator_object2, generator_object3 = tee((i for i in some_list), 4)
-    assert list(generator_object) == [1, 2, 3, 4, 5]
-    assert set(generator_object1) == {1, 2, 3, 4, 5}
-    assert dict((i, i * 2) for i in generator_object2) == {1: 2, 2: 4, 3: 6, 4: 8, 5: 10}
-    assert tuple(generator_objec3) == (1, 2, 3, 4, 5)
+    generator_list = (i for i in some_list)
+    assert list(generator_list) == [1, 2, 3, 4, 5]
+
+    generator_set = (i for i in some_list)
+    assert set(generator_set) == {1, 2, 3, 4, 5}
+
+    generator_dict = (i for i in some_list)
+    assert dict((i, i * 2) for i in generator_dict) == {1: 2, 2: 4, 3: 6, 4: 8, 5: 10}
+
+    generator_tuple = (i for i in some_list)
+    assert tuple(generator_tuple) == (1, 2, 3, 4, 5)
 
 
-# comprehensions with filtration and item modification
-def test_comprehensions_with_filtration_and_modification():
+def test_comprehensions_with():
     assert [i * 2 for i in some_list if i <= 3] == [2, 4, 6]
-
     generator_object = (i * 2 for i in some_list if i <= 3)
     assert list(generator_object) == [2, 4, 6]
-
     assert {i * 2 for i in some_list if i <= 3} == {2, 4, 6}
-
     assert {i: i * 2 for i in some_list if i <= 3} == {1: 2, 2: 4, 3: 6}
-
-
-# process dict in comprehensions, same as during iteration
-[some_dict[k] * 2 for k in some_dict if some_dict[k] <= 2]
-[k for k in some_dict if some_dict[k] <= 2]
-
-[i[1] * 2 for i in some_dict.items() if i[1] <= 2]
-[i[0] for i in some_dict.items() if i[1] <= 2]
-[k for k, v in some_dict.items() if v <= 2]
-[k for (k, v) in some_dict.items() if v <= 2]
-
-{i[0]: i[1] for i in some_dict.items()}
-{k: v for k, v in some_dict.items()}
-{k: v for k, v in some_dict.items() if v <= 2}
-{k * 2: v * 2 for k, v in some_dict.items() if v <= 2}
-
-# collect data
-data = [{"age": 16, "name": "John", "sex": "M"},
-        {"age": 34, "name": "Marry", "sex": "F"},
-        {"age": 25, "name": "Mathew", "sex": "M"}]
-
-ages = {i["age"] for i in data}
-names = {i["name"] for i in data}
