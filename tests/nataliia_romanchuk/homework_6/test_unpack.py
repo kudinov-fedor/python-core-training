b79c2376e9b6e26cd63f09a7ad501190718bda52
Nataliia Romanchuk <60582245+Vivanatalya@users.noreply.github.com>
2024-02-18 11:20:21 +0200

    Update test_unpack.py

--- tests/nataliia_romanchuk/homework_6/test_unpack.py
+++ tests/nataliia_romanchuk/homework_6/test_unpack.py
@@ -1,10 +1,8 @@
 from nataliia_romanchuk.homework6.unpack import unpack_recursive, unpack_while_loop
 import pytest
 
-@pytest.mark.parametrize("some_item", [[123, ["234", None], [[1], [23], [[123], 123, ["sdf", True]]]]])
-def test_unpack_while_loop(some_item):
-    assert unpack_while_loop(some_item) == [123, "234", None, 1, 23, 123, 123, "sdf", True]
-
-@pytest.mark.parametrize("some_item", [[123, ["234", None], [[1], [23], [[123], 123, ["sdf", True]]]]])
-def test_unpack_recursive(some_item):
-    assert unpack_recursive(some_item) == [123, "234", None, 1, 23, 123, 123, "sdf", True]
+@pytest.mark.parametrize("func", [unpack_while_loop, unpack_recursive])
+def test_unpack(func):
+    some_item = [123, ["234", None], [[1], [23], [[123], 123, ["sdf", True]]]]
+    assert func(some_item) == [123, "234", None, 1, 23, 123, 123, "sdf", True]
+    
