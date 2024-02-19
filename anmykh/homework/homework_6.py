10c67c3dde249c26bed7986d4737e24773393f04
“Anastasiia <amykhale@duosecurity.com>
2024-02-19 21:00:10 +0200

    tasks + tests 6

--- anmykh/homework/homework_6.py
+++ anmykh/homework/homework_6.py
@@ -1,9 +1,39 @@
 def max_function(*items, key=None):
-    minimum_value = 0
-    for item in list(items):
-        for number in item:
-            if number > minimum_value:
-                minimum_value = number
-            else:
-                pass
-        return minimum_value
+    if len(items) == 0:
+        return ValueError
+    if len(items) == 1:
+        items = tuple(items[0])
+    if len(items) == 0:
+        return ValueError
+    max_value = items[0]
+    for item in items[1:]:
+        if item > max_value:
+            max_value = item
+    return max_value
+
+
+def min_function(*items, key=None):
+    if len(items) == 0:
+        return ValueError
+    if len(items) == 1:
+        items = tuple(items[0])
+    if len(items) == 0:
+        return ValueError
+    min_value = items[0]
+    for item in items[1:]:
+        if item < min_value:
+            min_value = item
+    return min_value
+
+
+def sort_function(items):
+    length = len(items)
+    if length == 0:
+        return []
+    if type(items) is not list:
+        items = list(items)
+    for i in range(length - 1):
+        for j in range(0, length - i - 1):
+            if items[j] > items[j + 1]:
+                items[j], items[j + 1] = items[j + 1], items[j]
+    return items
--- tests/anmykh/test_homework_6.py
+++ tests/anmykh/test_homework_6.py
@@ -1,7 +1,8 @@
 import pytest
 
 import homework.homework_6.functions
-from anmykh.homework.homework_6 import max_function
+from anmykh.homework.homework_6 import max_function, min_function, sort_function
+
 
 @pytest.mark.parametrize(["param1", "param2", "output"], [
     ["a", "b", ('a', 'b')],
@@ -13,9 +14,36 @@ def test_with_required_params(param1, param2, output):
 
 
 @pytest.mark.parametrize(["param", "output"], [
-    [(-7, -4, -2, 1, 2, 3, 4, 5, 6), 6],
-    [(2, 4, 9, 11, 100), 100],
-    [(0, 0, 3, 89, 10, 11), 89]
+    [[], ValueError],
+    [[1], 1],
+    [[-1, 0, 3], 3],
+    [(""), ValueError],
+    [("a"), "a"],
+    [("abc"), "c"]
 ])
 def test_max_function(param, output):
     assert max_function(param) == output
+
+
+@pytest.mark.parametrize(["param", "output"], [
+    [[], ValueError],
+    [[1], 1],
+    [[-1, 0, 3], -1],
+    [(""), ValueError],
+    [("a"), "a"],
+    [("abc"), "a"]
+])
+def test_min_function(param, output):
+    assert min_function(param) == output
+
+
+@pytest.mark.parametrize(["param", "output"], [
+    [[], []],
+    [[1], [1]],
+    [[0, 2, 8, -1], [-1, 0, 2, 8]],
+    [(""), []],
+    [("a"), ['a']],
+    [("azf"), ['a', 'f', 'z']]
+])
+def test_sorted(param, output):
+    assert sort_function(param) == output
