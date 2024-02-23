a4c1b07d93441ecf0e0e55beed9a2d33ef1359d8
vivanatalya <vivanatalya@gmail.com>
2024-02-23 18:19:31 +0200

    [NATALIIA_ROMANCHUK/homework8] test_warior.py

--- tests/nataliia_romanchuk/homework8/test_warior.py
+++ tests/nataliia_romanchuk/homework8/test_warior.py
@@ -1,18 +1,24 @@
 from nataliia_romanchuk.homework_8.warior import Warrior, Knight, fight
 
 
-def test_warrior():
-    chuck = Warrior()
-    bruce = Warrior()
-    carl = Knight()
-    dave = Warrior()
+def test_warrior_mark_carl():
     mark = Warrior()
-
-    assert fight(chuck, bruce) == True
-    assert fight(dave, carl) == False
-    assert chuck.is_alive == True
-    assert bruce.is_alive == False
-    assert carl.is_alive == True
-    assert dave.is_alive == False
+    carl = Knight()
     assert fight(carl, mark) == False
     assert carl.is_alive == False
+
+
+def test_chuck_bruce():
+    chuck = Warrior()
+    bruce = Warrior()
+    assert fight(chuck, bruce)
+    assert chuck.is_alive
+    assert bruce.is_alive == False
+
+
+def test_dave_carl():
+    carl = Knight()
+    dave = Warrior()
+    assert fight(dave, carl) == False
+    assert carl.is_alive
+    assert dave.is_alive == False
