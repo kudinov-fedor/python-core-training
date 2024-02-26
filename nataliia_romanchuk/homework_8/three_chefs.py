2e23b1d3d8e9e51ef171a50c27f1e3ad83ca646f
vivanatalya <vivanatalya@gmail.com>
2024-02-26 12:10:56 +0200

    [NATALIIA_ROMANCHUK/homework8] added three_chefs.py

---
+++ nataliia_romanchuk/homework_8/three_chefs.py
@@ -0,0 +1,33 @@
+
+class AbstractCook:
+    food_total = 0
+    drink_total = 0
+
+    def __init__(self, food_name: str, drink_name: str):
+        self.menu = {'food': food_name, 'drink': drink_name}
+
+    def add_food(self, food_amount: int, food_price: int):
+        self.food_total += food_amount * food_price
+
+    def add_drink(self, portion_number: int, drink_price: int):
+        self.drink_total += portion_number * drink_price
+
+    def total(self):
+        total = self.food_total +self.drink_total
+        print( f"{self.menu['food']}: {self.food_total}, {self.menu['drink']}: {self.drink_total}, Total: {total}")
+        return f"{self.menu['food']}: {self.food_total}, {self.menu['drink']}: {self.drink_total}, Total: {total}"
+
+
+class JapaneseCook(AbstractCook):
+    def __init__(self):
+        super().__init__('Sushi', 'Tea')
+
+
+class UkrainianCook(AbstractCook):
+    def __init__(self):
+        super().__init__('Pampushky', 'Uzvar')
+
+
+class ItalianCook(AbstractCook):
+    def __init__(self):
+        super().__init__('Pizza', 'Juice')
