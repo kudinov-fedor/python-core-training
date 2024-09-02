76d31fa907bc111a1fa5894f22878889f40f1aba
f.kudinov <32301350+kudinov-fedor@users.noreply.github.com>
2024-09-02 14:36:54 +0300

    Update multiply.py

--- homework/homework_2/multiply.py
+++ homework/homework_2/multiply.py
@@ -1,1 +1,11 @@
+# Посилання на умову задачі:
+# https://py.checkio.org/mission/multiply-intro/share/b3793e7677e3d43418e377b8847685aa/
 
+# Початковий код:
+# https://pastebin.com/AT8i1r7n
+
+# Розв'язок задачі:
+# https://pastebin.com/5SCynEqE 
+
+# Розбір розвʼязку
+# https://youtu.be/woiTs37mh_A
