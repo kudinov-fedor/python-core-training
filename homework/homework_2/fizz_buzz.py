016b6b63fe6df5708084d84c439d43508c92ed42
f.kudinov <32301350+kudinov-fedor@users.noreply.github.com>
2024-09-02 14:38:31 +0300

    Update fizz_buzz.py

--- homework/homework_2/fizz_buzz.py
+++ homework/homework_2/fizz_buzz.py
@@ -1,1 +1,12 @@
+# Посилання на умову задачі:
+# https://py.checkio.org/mission/fizz-buzz/share/a22c7465d4ecc1c7efad0113609f5697/
 
+# Початковий Код:
+# https://pastebin.com/Eq9sNNcB
+
+# Розв'язок задачі:
+# https://pastebin.com/3z2CxM8G
+
+
+# Розбір розвʼязку
+# https://youtu.be/3zVf00eK-po
