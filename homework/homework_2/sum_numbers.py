4adcc47b267c2b85f53c3666510a687319722279
f.kudinov <32301350+kudinov-fedor@users.noreply.github.com>
2024-09-02 14:40:09 +0300

    Create sum_numbers.py

---
+++ homework/homework_2/sum_numbers.py
@@ -0,0 +1,11 @@
+# Посилання на умову задачі:
+# https://py.checkio.org/mission/sum-numbers/share/30a756ab4ecf5ed7a4d7321fe172c7a0/
+
+# Початковий Код:
+# https://pastebin.com/hk59XsX5
+
+# Розв'язок задачі:
+# https://pastebin.com/2jiSeGzj
+
+# Розбір розвʼязку:
+# https://youtu.be/iJTXzPSrByA
