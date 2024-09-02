7bd96929e6e0d311d5e1411aec13928f26178aee
f.kudinov <32301350+kudinov-fedor@users.noreply.github.com>
2024-09-02 14:41:32 +0300

    Create three_words.py

---
+++ homework/homework_2/three_words.py
@@ -0,0 +1,11 @@
+# Посилання на умову задачі:
+# https://py.checkio.org/mission/three-words/share/dd2252765d9f75b116ba2401c69cbee8/
+
+# Початковий Код:
+# https://pastebin.com/Q56KRbCm
+
+# Розв'язок задачі:
+# https://pastebin.com/rAwMt0up
+
+# Розбір розвʼязку:
+# https://youtu.be/BJ1Cx6XiyQA
