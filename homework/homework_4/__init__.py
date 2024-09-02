f74679aeea089d6e969c5a373f18f31cadec790e
f.kudinov <32301350+kudinov-fedor@users.noreply.github.com>
2024-09-02 14:19:16 +0300

    Update __init__.py

--- homework/homework_4/__init__.py
+++ homework/homework_4/__init__.py
@@ -1,3 +1,10 @@
+# In this homework you need to look through different code examples
+# Try to run them in python console, experiment 
+# Collect questions related to pieces you did not understand
+
+# Also try to write several pytest tests
+
+
 # homework covers:
 
 # containers and sequences
