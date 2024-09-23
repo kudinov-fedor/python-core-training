72b0229d23f4cc4e9a9dc41e77486555639b9a30
irepela <irepela@softserveinc.com>
2024-09-23 17:40:48 +0300

    my first commit

---
+++ tests/irepela/test_expressions.py
@@ -0,0 +1,10 @@
+def test_equality():
+    assert id(str.join) != id("".join)
+
+def test_joins():
+    assert str.join("|", "abc") == "|".join("abc")
+
+def test_split():
+    splitted = "Hello World".split(" ")
+    assert splitted[0] == "Hello"
+    assert splitted[1] == "World"
