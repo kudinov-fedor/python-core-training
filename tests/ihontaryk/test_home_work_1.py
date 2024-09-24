cf17cef986498bc295b676d449e72c6bfff7a56a
ivannaht <59062196+ivannaht@users.noreply.github.com>
2024-09-24 16:32:50 +0300

    Updated tests

--- tests/ihontaryk/test_home_work_1.py
+++ tests/ihontaryk/test_home_work_1.py
@@ -32,5 +32,32 @@ data2 = [
 
 @pytest.mark.parametrize("input1, input2", data2)
 def test_comparison(input1, input2):
-    """verify types of comparison"""
+    """verify comparison"""
     assert input1 > input2
+
+
+data3 = [
+    ([1, 2], [1, 2]),
+    ({1, 2}, {1, 2}),
+    ((1, 3), (1, 2))
+]
+
+
+@pytest.mark.parametrize("input1, input2", data3)
+def test_different_ids(input1, input2):
+    """verify different ids"""
+    assert id(input1) != id(input2)
+
+
+
+data4 = [
+    ("a", "a"),
+    (1, 1),
+    ((1, 2), (1, 2))
+]
+
+
+@pytest.mark.parametrize("input1, input2", data4)
+def test_same_ids(input1, input2):
+    """verify same ids"""
+    assert id(input1) == id(input2)
