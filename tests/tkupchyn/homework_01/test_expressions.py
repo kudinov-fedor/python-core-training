1fa481f84ce7b547201410fc0be1b64165c7eace
Coopert1 <kupchynskyi@gmail.com>
2024-09-27 12:14:02 +0300

    address commments

--- tests/tkupchyn/homework_01/test_expressions.py
+++ tests/tkupchyn/homework_01/test_expressions.py
@@ -1,23 +1,21 @@
 import pytest
 
+
 @pytest.mark.parametrize('string, expected_result', [('abc', 'a|b|c'),
-                                                     ('test', 't|e|s|t'),])
+                                                     ('test', 't|e|s|t')])
 def test_string_join(string, expected_result):
     result = str.join("|", string)
     assert result == expected_result
 
 
 @pytest.mark.parametrize('words, expected_result', [(['abc', '', 'banana', 'ball'], 'banana'),
-                                                   (['test', '123123124'], '123123124'),
-                                                    ([], ValueError),
-                                                    (['test', 'with', True], ValueError)])
-def test_longest_word(words, expected_result):
-    if not words or not all(isinstance(word, str) for word in words):
-        with pytest.raises(ValueError):
-            raise ValueError("No words provided")
-    else:
-        result = max(words, key=len)
-        assert result == expected_result
+                                                    (['test', '123123124'], '123123124')])
+def test_longest_word_positive(words, expected_result):
+    result = max(words, key=len)
+    assert result == expected_result
 
 
-
+@pytest.mark.parametrize('words', [([1, 3, "error", True])])
+def test_longest_word_negative(words):
+    with pytest.raises(TypeError,):
+        max(words, key=len)
