42c59f418f04a48f60153cb7f98316eb96b5a1ec
ivannaht <59062196+ivannaht@users.noreply.github.com>
2024-09-28 23:51:12 +0300

    [ihontaryk/home-work-2] Updated init files

--- ihontaryk/homework_2/__init__.py
+++ ihontaryk/homework_2/__init__.py
@@ -1,14 +1,5 @@
-# It is highly important to solve practical tasks from portals like 'litcode', 'codewars' etc.
-# So here I suggest 4 tasks from portal CheckIO 
-# so that you could have some taste on how solving of tasks from such kind of portals can look like.
-# Also I highly recoment to Sign Up to CheckIO and start solving tasks there.
-# As soon as you resolve task all solutions provided by other people become available, 
-# which can be treated as greate source material for self studying
-
-# First task 'Multiply' will provide all required information how task should be solved
-
 # Tasks order:
-# 1. Multiply
-# 2. Fizz Buzz
-# 3. Summ Numbers
+# 1. Fizz Buzz
+# 2. Multiply
+# 3. Sum Numbers
 # 4. Three Words
