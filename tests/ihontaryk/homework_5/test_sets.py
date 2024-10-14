1d51a62484766e1b22d4739f528f36a1371fdb80
ivannaht <59062196+ivannaht@users.noreply.github.com>
2024-10-14 23:19:12 +0300

    [ihontaryk/home-work-5] Fixed typo

--- tests/ihontaryk/homework_5/test_sets.py
+++ tests/ihontaryk/homework_5/test_sets.py
@@ -3,18 +3,18 @@ import pytest
 from ihontaryk.homework_5.sets import remove_min_max_loop
 
 
-@pytest.mark.parametrize('set1, number1, expected_resul',
+@pytest.mark.parametrize('set1, number1, expected_result',
                          [({'a', 'b'}, 1, set()),
                           ({'a', 'b', 'c'}, 1, {'b'}),
                           ({1, 2, 3, 4, 5, 6, 7}, 3, {4}),
                           ({'a', 'b', 'abc', 'abc2'}, 2, set()),
                           ({'a', 'b', 'abc', 'bcl', 'd'}, 2, {'b'})
                           ])
-def test_remove_min_max_loop_positive(set1, number1, expected_resul):
+def test_remove_min_max_loop_positive(set1, number1, expected_result):
     """
     verify positive cases for remove_min_max_loop function
     """
-    assert remove_min_max_loop(set1, number1) == expected_resul
+    assert remove_min_max_loop(set1, number1) == expected_result
 
 
 @pytest.mark.parametrize('set1, number1, error',
