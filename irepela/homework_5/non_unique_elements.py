0ec07caea38c80373f45541aed519174c6645da1
irepela <igor.repela@gmail.com>
2024-10-15 19:07:43 +0300

    Simplified code to use list comprehension

--- irepela/homework_5/non_unique_elements.py
+++ irepela/homework_5/non_unique_elements.py
@@ -8,11 +8,4 @@ def non_unique_elements(data: list[int]) -> list[int]:
         Returns:
             list[int]: list of non unique elements
     """
-    unique_list = []
-    for num in data:
-        if data.count(num) == 1:
-            unique_list.append(num)
-
-    non_unique_list = [num for num in data if num not in unique_list]
-
-    return non_unique_list
+    return [num for num in data if data.count(num) > 1]
