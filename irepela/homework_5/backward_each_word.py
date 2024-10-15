7db6a1421f0803ffd06c58f7a413aa14ef4483aa
irepela <igor.repela@gmail.com>
2024-10-15 19:22:24 +0300

    Converted to one line dict compr using list comprehension

--- irepela/homework_5/backward_each_word.py
+++ irepela/homework_5/backward_each_word.py
@@ -9,10 +9,4 @@ def backward_each_word(text: str) -> str:
             str: string with reversed words
     """
 
-    word_list = text.split(" ")
-    reversed_word_list = []
-
-    for word in word_list:
-        reversed_word_list.append(word[::-1])
-
-    return " ".join(reversed_word_list)
+    return " ".join([word[::-1] for word in text.split(" ")])
