f259cee549566f2c014748ad4cc81901fbe856fb
irepela <igor.repela@gmail.com>
2024-10-18 18:11:47 +0300

    Refactored while loop to use the same list

--- irepela/homework_6/unpack.py
+++ irepela/homework_6/unpack.py
@@ -1,9 +1,9 @@
 def unpack_while_loop(items: list) -> list:
     """
-      Flattens list with up to 3 levels of nesting
+      Flattens list using while
 
       Args:
-          items: list of items with up to 3 levels of nesting
+          items: nested list of items
 
       Returns:
           list: flattened list
@@ -13,19 +13,7 @@ def unpack_while_loop(items: list) -> list:
     while len(copied_list) > 0:
         next_item = copied_list.pop()
         if isinstance(next_item, list):
-            nested_list_1 = list(reversed(next_item))
-            while len(nested_list_1) > 0:
-                next_item = nested_list_1.pop()
-                if isinstance(next_item, list):
-                    nested_list_2 = list(reversed(next_item))
-                    while len(nested_list_2) > 0:
-                        next_item = nested_list_2.pop()
-                        if isinstance(next_item, list):
-                            unpacked.extend(next_item)
-                        else:
-                            unpacked.append(next_item)
-                else:
-                    unpacked.append(next_item)
+            copied_list.extend(reversed(next_item))
         else:
             unpacked.append(next_item)
     return unpacked
