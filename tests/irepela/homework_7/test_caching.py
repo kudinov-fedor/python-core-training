c4a04cca27fa1d5037ac21a99cc1034b06e83336
irepela <igor.repela@gmail.com>
2024-10-21 22:16:51 +0300

    Added mocker spy to test fibonache decorator

--- tests/irepela/homework_7/test_caching.py
+++ tests/irepela/homework_7/test_caching.py
@@ -1,17 +1,8 @@
-import pytest
-from irepela.homework_7.caching import fibo
+from irepela.homework_7 import caching
 
 
-@pytest.mark.parametrize("a, expected", [
-    (0, 0),
-    (1, 1),
-    (2, 2),
-    (3, 3),
-    (4, 5),
-    (6, 13),
-    (7, 21),
-    (8, 34),
-    (9, 55),
-])
-def test_chain_sum(a, expected):
-    assert fibo(a) == expected
+def test_caching(mocker):
+    spy = mocker.spy(caching, "fibo")
+    caching.fibo(5)
+    assert spy.call_count == 7
+    assert spy.spy_return == 8
