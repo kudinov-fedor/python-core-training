3fb8a4bf5b2859df291aede8cfc7cccb7b3680ed
irepela <igor.repela@gmail.com>
2024-10-23 16:13:52 +0300

    Refactored warrior methods and attributes

--- irepela/homework_8/warrior.py
+++ irepela/homework_8/warrior.py
@@ -6,13 +6,13 @@ class Warrior:
     def __init__(self):
         self.health = 50
         self.attack = 5
-        self.is_alive = True
+
+    @property
+    def is_alive(self):
+        return self.health > 0
 
     def hit(self, damage):
-        if self.health > 0:
-            self.health = self.health - damage
-        else:
-            self.is_alive = False
+        self.health = self.health - damage
 
     def _str__(self):
         return f"Warrior ({self.health}, {self.attack}, {self.is_alive})"
@@ -24,15 +24,14 @@ class Knight(Warrior):
         super().__init__()
         self.attack = 7
 
-    def _str__(self):
+    def _str_(self):
         return f"Knight ({self.health}, {self.attack}, {self.is_alive})"
 
 
 def fight(unit_1: Warrior, unit_2: Warrior) -> bool:
     while unit_1.is_alive and unit_2.is_alive:
         # unit 2 is hit first
-        if unit_1.is_alive:
-            unit_2.hit(unit_1.attack)
+        unit_2.hit(unit_1.attack)
         if unit_2.is_alive:
             unit_1.hit(unit_2.attack)
 
--- tests/irepela/homework_8/test_warrior.py
+++ tests/irepela/homework_8/test_warrior.py
@@ -1,19 +1,18 @@
 from irepela.homework_8.warrior import Warrior, Knight, fight
 
 
-chuck = Warrior()
-bruce = Warrior()
-carl = Knight()
-dave = Warrior()
-mark = Warrior()
-
-
 def test_fight():
+    chuck = Warrior()
+    bruce = Warrior()
+    carl = Knight()
+    dave = Warrior()
+    mark = Warrior()
+
     assert fight(chuck, bruce) is True
-    assert chuck.is_alive is True
-    assert bruce.is_alive is False
+    assert chuck.is_alive
+    assert not bruce.is_alive
     assert fight(dave, carl) is False
-    assert carl.is_alive is True
-    assert dave.is_alive is False
+    assert carl.is_alive
+    assert not dave.is_alive
     assert fight(carl, mark) is False
-    assert carl.is_alive is False
+    assert not carl.is_alive
