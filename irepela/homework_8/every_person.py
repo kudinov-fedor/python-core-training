76e9083d6029284c5c98e8d4704df39dbbd9ede6
irepela <igor.repela@gmail.com>
2024-10-23 13:59:03 +0300

    Refactored every_person methods

--- irepela/homework_8/every_person.py
+++ irepela/homework_8/every_person.py
@@ -1,15 +1,23 @@
 # Details:  https://py.checkio.org/mission/every-person-is-unique/share/1c6fd4b663f7e40fa15b699f17efab40/
 from datetime import datetime
-from math import floor
+
+
+DATE_FORMAT = "%d.%m.%Y"
+CURRENT_DATE = datetime.strptime("01.01.2018", DATE_FORMAT)
+WORK_PREFIX = {
+    "male": "He is a",
+    "female": "She is a",
+    "unknown": "Is a"
+}
 
 
 class Person:
 
     def __init__(self, first_name, last_name, birth_date, job, working_years, salary, country, city,
-                 gender='unknown'):
+                 gender="unknown"):
         self.first_name = first_name
         self.last_name = last_name
-        self.birth_date = birth_date
+        self.birth_date = datetime.strptime(birth_date, DATE_FORMAT)
         self.job = job
         self.working_years = working_years
         self.salary = salary
@@ -21,19 +29,13 @@ class Person:
         return f"{self.first_name} {self.last_name}"
 
     def age(self):
-        current_date = datetime.strptime("01.01.2018", "%d.%m.%Y")
-        birth_date = datetime.strptime(self.birth_date, "%d.%m.%Y")
-        age = floor((current_date - birth_date).days / 365)
+        # Decrease by 1 if year is not full(bool converts to 0 or 1)
+        year_not_full = (CURRENT_DATE.month, CURRENT_DATE.day) < (self.birth_date.month, self.birth_date.day)
+        age = CURRENT_DATE.year - self.birth_date.year - year_not_full
         return age
 
     def work(self):
-        if self.gender == "male":
-            prefix = "He is a"
-        elif self.gender == "female":
-            prefix = "She is a"
-        else:
-            prefix = "Is a"
-        return f"{prefix} {self.job}"
+        return f"{WORK_PREFIX[self.gender]} {self.job}"
 
     def money(self):
         calculated_salary = self.working_years * self.salary * 12
