b286e086afb178d53f1f3a7b625cdae0dc3f73c6
Coopert1 <kupchynskyi@gmail.com>
2024-10-27 11:10:19 +0200

    add initial solution

--- tkupchyn/homework_06/min_max_sorted.py
+++ tkupchyn/homework_06/min_max_sorted.py
@@ -76,13 +76,3 @@ assert custom_sorted(-7, -4, -2, 1, 2, 3, 4, 5, 6) == [-7, -4, -2, 1, 2, 3, 4, 5
 assert custom_sorted(-7, -4, -2, 1, 2, 3, 4, 5, 6, reverse=True) == [6, 5, 4, 3, 2, 1, -2, -4, -7]
 assert custom_sorted(-7, -4, -2, 1, 2, 3, 4, 5, 6, key=abs) == [1, -2, 2, 3, -4, 4, 5, 6, -7]
 assert custom_sorted(-7, -4, -2, 1, 2, 3, 4, 5, 6, key=abs, reverse=True) == [-7, 6, 5, -4, 4, 3, -2, 2, 1]
-
-# can receive 1 positional parameter, which is a collection of items
-# assert custom_min([-7, -4, -2, 1, 2.4, 3, 4, 5, 6]) == -7
-# assert custom_max([-7, -4, -2, 1, 2, 3, 4, 5, 6]) == 6
-# assert custom_min([-7, -4, -2, 1, 2, 3, 4, 5, 6], key=abs) == 1
-# assert custom_max([-7, -4, -2, 1, 2, 3, 4, 5, 6]) == 6
-# assert custom_sorted([-7, -4, -2, 1, 2, 3, 4, 5, 6]) == [-7, -4, -2, 1, 2, 3, 4, 5, 6]
-# assert custom_sorted([-7, -4, -2, 1, 2, 3, 4, 5, 6], reverse=True) == [6, 5, 4, 3, 2, 1, -2, -4, -7]
-# assert custom_sorted([-7, -4, -2, 1, 2, 3, 4, 5, 6], key=abs) == [1, -2, 2, 3, -4, 4, 5, 6, -7]
-# assert custom_sorted([-7, -4, -2, 1, 2, 3, 4, 5, 6], key=abs, reverse=True) == [-7, 6, 5, -4, 4, 3, -2, 2, 1]
