2004d73c426536653b8a6ed74dd14e4b9fa63db0
ivannaht <59062196+ivannaht@users.noreply.github.com>
2024-10-31 10:50:38 +0200

    [ihontaryk/home-work-8] Fixed tests for warrior

--- tests/ihontaryk/homework_8/test_warrior.py
+++ tests/ihontaryk/homework_8/test_warrior.py
@@ -1,40 +1,43 @@
 import pytest
 from ihontaryk.homework_8.warrior import Warrior, Knight, fight
 
-chuck = Warrior()
-bruce = Warrior()
-carl = Knight()
-dave = Warrior()
-mark = Warrior()
+
+@pytest.fixture(scope='module')
+def players():
+    chuck = Warrior()
+    bruce = Warrior()
+    carl = Knight()
+    dave = Warrior()
+    mark = Warrior()
+
+    return {'chuck': chuck, 'bruce': bruce, 'carl': carl, 'dave': dave, 'mark': mark}
+
 
 @pytest.mark.parametrize('p1, p2, expected_result',
-                         [(chuck, bruce, True)])
-
-def test_fight_first(p1, p2, expected_result):
+                         [('chuck', 'bruce', True)])
+def test_fight_first(players, p1, p2, expected_result):
     """
     verify fight function for first fight
     """
 
-    assert fight(p1, p2) == expected_result
+    assert fight(players[p1], players[p2]) == expected_result
 
 
 @pytest.mark.parametrize('p1, p2, expected_result',
-                         [(dave, carl, False)])
-
-def test_fight_second(p1, p2, expected_result):
+                         [('dave', 'carl', False)])
+def test_fight_second(players, p1, p2, expected_result):
     """
     verify fight function for second fight
     """
 
-    assert fight(p1, p2) == expected_result
+    assert fight(players[p1], players[p2]) == expected_result
 
 
 @pytest.mark.parametrize('p1, p2, expected_result',
-                         [(carl, mark, False)])
-
-def test_fight_third(p1, p2, expected_result):
+                         [('carl', 'mark', False)])
+def test_fight_third(players, p1, p2, expected_result):
     """
     verify fight function for third fight
     """
 
-    assert fight(p1, p2) == expected_result
+    assert fight(players[p1], players[p2]) == expected_result
