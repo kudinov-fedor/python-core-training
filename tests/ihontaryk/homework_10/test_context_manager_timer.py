496743f764d8bfa23875c05bf028d357a2634c6a
ivannaht <59062196+ivannaht@users.noreply.github.com>
2024-11-07 12:36:47 +0200

    [ihontaryk/home-work-10] Added tasks and tests for Timer

---
+++ tests/ihontaryk/homework_10/test_context_manager_timer.py
@@ -0,0 +1,20 @@
+import pytest
+
+import time
+
+from ihontaryk.homework_10.context_manager_timer import Timer
+
+
+@pytest.mark.parametrize('delay, test_time, expected_result',
+                         [(1, 1, True),
+                          (5, 6, False)
+                          ])
+def test_timer(delay, test_time, expected_result):
+    """
+    verify timer contex manager
+    """
+
+    with Timer() as timer:
+        time.sleep(delay)
+
+    assert (timer.total >= test_time) is expected_result
