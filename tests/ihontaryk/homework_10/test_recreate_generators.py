06cc562793a9580b1ad27c57b61c197ce54a3c87
ivannaht <59062196+ivannaht@users.noreply.github.com>
2024-11-08 11:52:58 +0200

    [ihontaryk/home-work-10] Added tests for custom generators

---
+++ tests/ihontaryk/homework_10/test_recreate_generators.py
@@ -0,0 +1,34 @@
+import pytest
+
+from ihontaryk.homework_10.recreate_generators import (custom_zip, custom_enumerate,
+                                                       custom_filter, custom_map)
+
+
+@pytest.mark.parametrize('test_iterable1, test_iterable2, test_iterable3, expected_result',
+                         [(["a", "b", "c"], ["x", "y", "z"], [1, 2, 4],
+                           [("a", "x", 1), ("b", "y", 2), ("c", "z", 4)]),
+                          (["a", "b", "c"], [0, 3, 6], ["x", "y", "z"],
+                           [("a", 0, "x"), ("b", 3, "y"), ("c", 6, "z")])
+                          ])
+def test_custom_zip(test_iterable1, test_iterable2, test_iterable3, expected_result):
+    """
+    verify custom_zip function
+    """
+
+    generator = custom_zip(test_iterable1, test_iterable2, test_iterable3)
+    assert list(generator) == expected_result
+
+
+@pytest.mark.parametrize('test_iterable, start, expected_result',
+                         [(["a", "b", "c"], 0,
+                           [(0, "a"), (1, "b"), (2, "c")]),
+                          (["a", "b", "c"], 2,
+                           [(2, "a"), (3, "b"), (4, "c")]),
+                          ])
+def test_custom_enumerate(test_iterable, start, expected_result):
+    """
+    verify custom_enumerate function
+    """
+
+    generator = custom_enumerate(test_iterable, start=start)
+    assert list(generator) == expected_result
