32b6d83f62c6a3cadcc92065e138bd13e79a60a8
ivannaht <59062196+ivannaht@users.noreply.github.com>
2024-11-09 20:23:19 +0200

    [ihontaryk/home-work-8] Fixed tests for warrior

--- tests/ihontaryk/homework_8/test_warrior.py
+++ tests/ihontaryk/homework_8/test_warrior.py
@@ -1,4 +1,5 @@
 import pytest
+
 from ihontaryk.homework_8.warrior import Warrior, Knight, fight
 
 
@@ -14,30 +15,13 @@ def players():
 
 
 @pytest.mark.parametrize('p1, p2, expected_result',
-                         [('chuck', 'bruce', True)])
-def test_fight_first(players, p1, p2, expected_result):
+                         [('chuck', 'bruce', True),
+                          ('dave', 'carl', False),
+                          ('carl', 'mark', False)
+                          ])
+def test_fight(players, p1, p2, expected_result):
     """
-    verify fight function for first fight
-    """
-
-    assert fight(players[p1], players[p2]) == expected_result
-
-
-@pytest.mark.parametrize('p1, p2, expected_result',
-                         [('dave', 'carl', False)])
-def test_fight_second(players, p1, p2, expected_result):
-    """
-    verify fight function for second fight
-    """
-
-    assert fight(players[p1], players[p2]) == expected_result
-
-
-@pytest.mark.parametrize('p1, p2, expected_result',
-                         [('carl', 'mark', False)])
-def test_fight_third(players, p1, p2, expected_result):
-    """
-    verify fight function for third fight
+    verify fight function
     """
 
     assert fight(players[p1], players[p2]) == expected_result
