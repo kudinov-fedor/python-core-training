50a23e625aa73c1331c763cb52163600304e61ad
ivannaht <59062196+ivannaht@users.noreply.github.com>
2024-11-09 17:27:33 +0200

    [ihontaryk/home-work-10] Fixed catcher

--- tests/ihontaryk/homework_10/test_context_manager_try_except.py
+++ tests/ihontaryk/homework_10/test_context_manager_try_except.py
@@ -1,22 +1,45 @@
 import pytest
 
-from ihontaryk.homework_10.context_manager_try_except import Catcher, divide_numbers
+from ihontaryk.homework_10.context_manager_try_except import Catcher
 
 
-@pytest.mark.parametrize('arguments',
-                         [(1, 0, False),
-                          (-1, 0, ZeroDivisionError, False),
-                          (-1, 0, (TypeError, ZeroDivisionError), False),
-                          (1, 1, None, True),
-                          ])
-def test_catcher(arguments):
+@pytest.mark.parametrize('num1, num2, expected_result',
+                         [(10, 2, 5), (0, 2, 0)])
+def test_catcher_positive(num1, num2, expected_result):
     """
-    verify catcher context manager
+    verify catcher context manager positive scenarios
     """
 
-    num1, num2, *errors, expected_result = arguments
-
-    with Catcher(*errors):
-        result = divide_numbers(num1, num2)
+    with Catcher():
+        result = num1 / num2
 
         assert result == expected_result
+
+
+@pytest.mark.parametrize('num1, num2',
+                         [(1, 0), (1, '1')])
+def test_catcher_negative1(num1, num2):
+    """
+    verify catcher context manager negative scenarios
+    """
+
+    with Catcher():
+        result = num1 / num2
+
+        assert result
+
+
+@pytest.mark.parametrize('num1, num2, errors',
+                         [(-1, 0, ZeroDivisionError),
+                          (1, '1', TypeError),
+                          (-1, 0, (TypeError, ZeroDivisionError))
+                          ])
+def test_catcher_negative2(num1, num2, errors):
+    """
+    verify catcher context manager negative scenarios
+    """
+
+    with Catcher(errors):
+        result = num1 / num2
+
+        assert result
